c550087781f8eac648191770f34d11d369fe9577
Adam Avramov <adam@hack.bg>
2021-04-03 17:45:09 +0300

    feat: add taskmaster

--- js/index.js
+++ js/index.js
@@ -2,10 +2,11 @@ import SecretNetwork from './SecretNetwork.js'
 import say, { sayer, muted } from './say.js'
 import { loadJSON, loadSchemas } from './schema.js'
 import table from './table.js'
+import taskmaster from './taskmaster.js'
 
 export {
   SecretNetwork,
   say, sayer, muted,
   loadJSON, loadSchemas,
-  table
+  table, taskmaster
 }
---
+++ js/taskmaster.js
@@ -0,0 +1,62 @@
+import { backOff } from "exponential-backoff"
+import tabulate from './table.js'
+
+export default function taskmaster ({
+  say    = console.debug,
+
+  header = [],
+  table  = tabulate(header),
+  output,
+
+  agent,
+  afterEach = async (t1, description, reports=[]) => {
+    const t2 = new Date()
+    say(`⏱️  took ${t2-t1}msec`)
+    if (agent && reports.length > 0) {
+      const txs = await Promise.all(reports.map(getTx))
+      const totalGasUsed = txs.map(x=>Number(x.gas_used)).reduce((x,y)=>x+y, 0)
+      const t3 = new Date()
+      say(`⛽ cost ${totalGasUsed} gas`)
+      say(`🔍 gas check took ${t3-t2}msec`)
+      table.push([t1.toISOString(), description, t2-t1, totalGasUsed, t3-t2])
+    } else {
+      table.push([t1.toISOString(), description, t2-t1])
+    }
+  }
+
+}={}) {
+
+  return Object.assign(task, { done, parallel })
+
+  async function done () {
+    if (output) await T.write(output)
+  }
+
+  async function parallel (description, ...tasks) { // TODO subtotal?
+    return await task(description, () => Promise.all(tasks.map(x=>Promise.resolve(x))))
+  }
+
+  async function task (description, operation = () => {}) {
+    const t1      = new Date()
+    say(`\n${description}`)
+    const reports = []
+    const report  = tx => { txs.push(tx); return tx }
+    const result  = await Promise.resolve(operation(report))
+    await afterEach(t1, description, reports)
+    return result
+  }
+
+  // TODO decouple this
+  async function getTx (tx) {
+    return backOff(async ()=>{
+      try {
+        return await agent.API.restClient.get(`/txs/${tx}`)
+      } catch (e) {
+        console.warn(e.message)
+        console.debug('waiting for transaction info...')
+        throw e
+      }
+    })
+  }
+
+}
