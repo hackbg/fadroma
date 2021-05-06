9492d8a44dfd875fb83a4c776d3003f2e7162504
Adam Avramov <adam@hack.bg>
2021-05-06 13:12:53 +0300

    feat(ops): support both parallel and sequential builds
    BREAKING: upload method arg "conn" renamed to "network"

--- js/SecretNetwork/ops.js
+++ js/SecretNetwork/ops.js
@@ -10,18 +10,33 @@ export async function build (CONTRACTS, options = {}) {
   const { task      = taskmaster()
         , builder   = new SecretNetwork.Builder()
         , workspace
-        , outputDir = resolve(workspace, 'artifacts') } = options
+        , outputDir = resolve(workspace, 'artifacts')
+        , parallel  = true } = options
 
   // pull build container
   await pull('enigmampc/secret-contract-optimizer:latest')
 
   // build all contracts
   const binaries = {}
-  await task.parallel('build project',
-    ...Object.entries(CONTRACTS).map(([name, {crate}])=>
-      task(`build ${name}`, async report => {
-        binaries[name] = await builder.build({outputDir, workspace, crate})
-      })))
+  if (parallel) {
+    await task.parallel('build project',
+      ...Object.entries(CONTRACTS).map(([name, {crate}])=>
+        task(`build ${name}`, async report => {
+          binaries[name] = await builder.build({outputDir, workspace, crate})
+        })))
+  } else {
+    for (const [name, {crate}] of Object.entries(CONTRACTS)) {
+      await task(`build ${name}`, async report => {
+        const buildOutput = resolve(outputDir, `${crate}@HEAD.wasm`)
+        if (existsSync(buildOutput)) {
+          console.info(`${buildOutput} exists. Delete it to rebuild that contract.`)
+          binaries[name] = buildOutput
+        } else {
+          binaries[name] = await builder.build({outputDir, workspace, crate})
+        }
+      })
+    }
+  }
 
   return binaries
 }
@@ -32,9 +47,9 @@ export async function upload (CONTRACTS, options = {}) {
         } = options
 
   let { builder
-      , conn = builder ? null : await SecretNetwork.localnet({stateBase}) } = options
-  if (typeof conn === 'string') conn = await SecretNetwork[conn]({stateBase})
-  if (!builder) builder = conn.builder
+      , network = builder ? null : await SecretNetwork.localnet({stateBase}) } = options
+  if (typeof network === 'string') network = await SecretNetwork[network]({stateBase})
+  if (!builder) builder = network.builder
 
   const receipts = {}
   for (let contract of Object.keys(CONTRACTS)) {
