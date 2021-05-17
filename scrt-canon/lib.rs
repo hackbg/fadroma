b439e8c50500418fb96e6abdaa117adf715e14be
Adam Avramov <adam@hack.bg>
2021-05-17 13:09:26 +0300

    feat: add canonize/humanize shorthands

--- scrt-canon/lib.rs
+++ scrt-canon/lib.rs
@@ -1,6 +1,12 @@
 //! `HumanAddr`<->`CanonicalAddr` conversion
 
-use cosmwasm_std::{Api, CanonicalAddr, HumanAddr, StdResult};
+use cosmwasm_std::{Api, CanonicalAddr, HumanAddr, StdResult, Storage, Querier, Extern};
+
+pub fn humanize <S: Storage, A: Api, Q: Querier> (
+    deps: &Extern<S, A, Q>, addr: &CanonicalAddr
+) -> StdResult<HumanAddr> {
+    deps.api.human_addr(addr)?
+}
 
 pub trait Humanize<T> {
     fn humanize <A: Api> (&self, api: &A) -> StdResult<T>;
@@ -9,6 +15,12 @@ impl<T: Humanize<U>, U> Humanize<Vec<U>> for Vec<T> {
     fn humanize <A: Api> (&self, api: &A) -> StdResult<Vec<U>> {
         self.iter().map(|x|x.humanize(api)).collect()
     }
+}
+
+pub fn canonize <S: Storage, A: Api, Q: Querier> (
+    deps: &mut Extern<S, A, Q>, addr: &HumanAddr
+) -> StdResult<CanonicalAddr> {
+    deps.api.canon_addr(addr)?
 }
 
 pub trait Canonize<T> {
