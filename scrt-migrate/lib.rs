63856f716cfbc73b2f6e5f474b460c60be7a123c
Adam Avramov <adam@hack.bg>
2021-05-18 18:13:52 +0300

    feat(scrt-migrate): finalize contract interface

--- scrt-migrate/lib.rs
+++ scrt-migrate/lib.rs
@@ -1,3 +1,50 @@
-mod types; pub use types::*;
-mod checks; pub use checks::*;
-mod storage; pub use storage::*;
+pub mod types;
+mod storage;
+mod checks;
+
+use cosmwasm_std::{Extern, Storage, Api, Querier, HumanAddr, StdResult};
+use fadroma_scrt_addr::{Humanize, canonize};
+
+pub fn get_status <S: Storage, A: Api, Q: Querier> (
+    deps: &Extern<S, A, Q>
+) -> StdResult<types::ContractStatus<HumanAddr>> {
+    storage::load(&deps.storage)?.humanize(&deps.api)
+}
+pub fn is_operational <S: Storage, A: Api, Q: Querier> (
+    deps: &Extern<S, A, Q>
+) -> StdResult<()> {
+    checks::is_operational(&get_status(deps)?)
+}
+pub fn can_set_status <S: Storage, A: Api, Q: Querier>  (
+    deps: &Extern<S, A, Q>,
+    to_level: &types::ContractStatusLevel
+) -> StdResult<()> {
+    checks::can_set_status(&get_status(deps)?, to_level)
+}
+pub fn set_status <S: Storage, A: Api, Q: Querier> (
+    deps: &mut Extern<S, A, Q>,
+    level: types::ContractStatusLevel,
+    reason: String,
+    new_address: Option<HumanAddr>
+) -> StdResult<()> {
+    storage::save(&mut deps.storage, &types::ContractStatus { level, reason, new_address: match new_address {
+        Some(new_address) => Some(canonize(&deps.api, &new_address)?),
+        None => None
+    } })
+}
+
+#[macro_export] macro_rules! with_status {
+    ($deps: ident, match $msg:ident { $($rest:tt)* }) => {
+        if let HandleMsg::SetStatus { level, reason, new_address } = $msg {
+            can_set_status(&$deps, &level)?;
+            set_status($deps, level, reason, new_address)?;
+            Ok(HandleResponse::default())
+        } else {
+            is_operational(&$deps)?;
+            match $msg {
+                HandleMsg::SetStatus { .. } => unreachable!(),
+                $($rest)*
+            }
+        }
+    }
+}
--- scrt-migrate/storage.rs
+++ scrt-migrate/storage.rs
@@ -3,7 +3,7 @@ use crate::{types::{ContractStatus, ContractStatusLevel}};
 
 pub const PREFIX: &[u8] = b"fadroma_migration_state";
 
-pub fn load (storage: &impl Storage) -> StdResult<ContractStatus<HumanAddr>> {
+pub fn load (storage: &impl Storage) -> StdResult<ContractStatus<CanonicalAddr>> {
     match cosmwasm_utils::storage::load(storage, PREFIX)? {
         Some(status) => status,
         None => Ok(ContractStatus::default())
@@ -11,10 +11,4 @@ pub fn load (storage: &impl Storage) -> StdResult<ContractStatus<HumanAddr>> {
 }
 pub fn save (storage: &mut impl Storage, status: &ContractStatus<CanonicalAddr>) -> StdResult<()> {
     cosmwasm_utils::storage::save(storage, PREFIX, status)
-}
-pub fn is_operational (storage: &impl Storage) -> StdResult<()> {
-    crate::checks::is_operational(&load(storage)?)
-}
-pub fn can_set_status (storage: &impl Storage, level: &ContractStatusLevel) -> StdResult<()> {
-    crate::checks::can_set_status(&load(storage)?, level)
 }
