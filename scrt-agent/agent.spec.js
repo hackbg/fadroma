a33ea337ad0797c868a6d593497ef9cc28970fd8
Tibor Hudik <tibor@barrage.net>
2021-06-24 16:13:33 +0200

    Added sending tests for agent; removing contract interaction tests - those will be under contract tests

--- scrt-agent/agent.spec.js
+++ scrt-agent/agent.spec.js
@@ -9,6 +9,8 @@ const log = debug('out');
 const context = {};
 
 const mnemonic = 'canoe argue shrimp bundle drip neglect odor ribbon method spice stick pilot produce actual recycle deposit year crawl praise royal enlist option scene spy';
+const mnemonic1 = 'bounce orphan vicious end identify universe excess miss random bench coconut curious chuckle fitness clean space damp bicycle legend quick hood sphere blur thing';
+const mnemonic2 = 'element dial search ticket feed lounge gasp wide uphold reflect hand lunch primary swamp wage vessel riot modify dinosaur laundry segment purpose secret asthma';
 const keypair = EnigmaUtils.GenerateNewKeyPairFromSeed(Bip39.decode(mnemonic));
 
 describe('SecretNetworkAgent', function () {
@@ -68,11 +70,33 @@ describe('SecretNetworkAgent', function () {
     assert.strictEqual(balance, 0);
   });
 
-  it('can send SCRT', async function () { });
+  it('can send SCRT', async function () {
+    this.timeout(120000);
+    await context.admin.send(context.agent.address, 10000000000);
+    const balance = await context.agent.balance;
+    assert.strictEqual(balance, '10000000000');
+  });
 
-  it('can send SCRT to many addresses in one go', async function () { });
+  it('can send SCRT to many addresses in one go', async function () {
+    this.timeout(120000);
+    const agent1 = await SecretNetworkAgent.create({
+      mnemonic1,
+      network: context.network,
+    });
+    const agent2 = await SecretNetworkAgent.create({
+      mnemonic2,
+      network: context.network,
+    });
 
-  it('can interact with contracts', async function () { });
+    await context.agent.sendMany([
+      [agent1.address, '100'],
+      [agent2.address, '100'],
+    ]);
 
-  it('can deploy contracts', async function () {});
+    const balance2 = await agent1.balance;
+    assert.strictEqual(balance2, '100');
+
+    const balance3 = await agent2.balance;
+    assert.strictEqual(balance3, '100');
+  });
 });
