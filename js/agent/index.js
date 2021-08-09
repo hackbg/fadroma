167bf0bde249e44f168b77d3aef97cf42fbecdfd
Adam Avramov <adam@hack.bg>
2021-08-09 22:47:42 +0300

    chore: cleanup

--- js/agent/index.js
+++ js/agent/index.js
@@ -2,9 +2,4 @@ import Scrt from './scrt_network.js'
 import ScrtAgentCLI from './scrt_agent_secretcli.js'
 import ScrtAgentJS from './scrt_agent_secretjs.js'
 import * as ScrtGas from './scrt_gas.js'
-
-export {
-  Scrt,
-  ScrtAgentJS, ScrtAgentCLI,
-  ScrtGas
-}
+export { Scrt, ScrtAgentJS, ScrtAgentCLI, ScrtGas }
