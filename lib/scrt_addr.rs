72e6dd5158fc0ec54d502902f01295cf54a73551
Adam Avramov <adam.b.avramov@gmail.com>
2021-10-26 17:12:07 +0200

    feature(scrt-addr): humanize/canonize for pointer types

--- lib/scrt_addr.rs
+++ lib/scrt_addr.rs
@@ -6,8 +6,44 @@ pub trait Humanize<T> {
     fn humanize (&self, api: &impl Api) -> StdResult<T>;
 }
 
+impl Humanize<HumanAddr> for CanonicalAddr {
+    fn humanize (&self, api: &impl Api) -> StdResult<HumanAddr> {
+        humanize_maybe_empty(api, self)
+    }
+}
+
+impl Humanize<HumanAddr> for &CanonicalAddr {
+    fn humanize (&self, api: &impl Api) -> StdResult<HumanAddr> {
+        humanize_maybe_empty(api, self)
+    }
+}
+
+impl<T: Humanize<U>, U> Humanize<Vec<U>> for Vec<T> {
+    fn humanize (&self, api: &impl Api) -> StdResult<Vec<U>> {
+        self.iter().map(|x|x.humanize(api)).collect()
+    }
+}
+
 pub trait Canonize<T> {
     fn canonize (&self, api: &impl Api) -> StdResult<T>;
+}
+
+impl Canonize<CanonicalAddr> for HumanAddr {
+    fn canonize (&self, api: &impl Api) -> StdResult<CanonicalAddr> {
+        canonize_maybe_empty(api, self)
+    }
+}
+
+impl Canonize<CanonicalAddr> for &HumanAddr {
+    fn canonize (&self, api: &impl Api) -> StdResult<CanonicalAddr> {
+        canonize_maybe_empty(api, self)
+    }
+}
+
+impl<T: Canonize<U>, U> Canonize<Vec<U>> for Vec<T> {
+    fn canonize (&self, api: &impl Api) -> StdResult<Vec<U>> {
+        self.iter().map(|x|x.canonize(api)).collect()
+    }
 }
 
 /// Attempting to canonicalize an empty address will fail. 
@@ -34,28 +70,4 @@ pub fn humanize_maybe_empty(api: &impl Api, addr: &CanonicalAddr) -> StdResult<H
             api.human_address(addr)?
         }
     )
-}
-
-impl Humanize<HumanAddr> for CanonicalAddr {
-    fn humanize (&self, api: &impl Api) -> StdResult<HumanAddr> {
-        humanize_maybe_empty(api, self)
-    }
-}
-
-impl<T: Humanize<U>, U> Humanize<Vec<U>> for Vec<T> {
-    fn humanize (&self, api: &impl Api) -> StdResult<Vec<U>> {
-        self.iter().map(|x|x.humanize(api)).collect()
-    }
-}
-
-impl Canonize<CanonicalAddr> for HumanAddr {
-    fn canonize (&self, api: &impl Api) -> StdResult<CanonicalAddr> {
-        canonize_maybe_empty(api, self)
-    }
-}
-
-impl<T: Canonize<U>, U> Canonize<Vec<U>> for Vec<T> {
-    fn canonize (&self, api: &impl Api) -> StdResult<Vec<U>> {
-        self.iter().map(|x|x.canonize(api)).collect()
-    }
 }
