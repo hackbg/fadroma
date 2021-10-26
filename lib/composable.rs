75aae2b704508dfe64d0fabe20b781fadc692eb3
Adam Avramov <adam.b.avramov@gmail.com>
2021-10-26 22:42:36 +0200

    refactor(composable): split out BaseComposable - might be useful for passing things through dyn (which doesnt support added generics)

--- lib/composable.rs
+++ lib/composable.rs
@@ -2,9 +2,31 @@ use crate::scrt::{Extern, Storage, Api, Querier, StdResult, StdError, to_vec, fr
 use crate::scrt_addr::{Humanize, Canonize};
 use crate::scrt_storage::concat;
 use serde::{Serialize, de::DeserializeOwned};
+use std::cell::RefCell;
 
-pub trait Composable<S, A, Q> {
-    fn storage (self) -> S;
+pub trait BaseComposable<S, A, Q> {
+    fn storage     (&self)     -> &S;
+    fn storage_mut (&mut self) -> &mut S;
+    fn api         (&self)     -> &A;
+    fn querier     (&self)     -> &Q;
+}
+
+impl<S: Storage, A: Api, Q: Querier> BaseComposable<S, A, Q> for Extern<S, A, Q> {
+    fn storage (&self) -> &S {
+        &self.storage
+    }
+    fn storage_mut (&mut self) -> &mut S {
+        &mut self.storage
+    }
+    fn api (&self) -> &A {
+        &self.api
+    }
+    fn querier (&self) -> &Q {
+        &self.querier
+    }
+}
+
+pub trait Composable<S, A, Q>: BaseComposable<S, A, Q> {
     fn set <V: Serialize> (&mut self, key: &[u8], value: V)
         -> StdResult<()>;
     fn set_ns <V: Serialize> (&mut self, ns: &[u8], key: &[u8], value: V)
@@ -13,21 +35,13 @@ pub trait Composable<S, A, Q> {
         -> StdResult<V>;
     fn get_ns <V: DeserializeOwned> (&self, ns: &[u8], key: &[u8])
         -> StdResult<V>;
-
-    fn api (self) -> A;
     fn humanize <V: Humanize<U>, U: Canonize<V>> (&self, value: V) ->
         StdResult<U>;
     fn canonize <V: Canonize<U>, U: Humanize<V>> (&self, value: V)
         -> StdResult<U>;
-
-    fn querier (self) -> Q;
 }
 
 impl<S: Storage, A: Api, Q: Querier> Composable<S, A, Q> for Extern<S, A, Q> {
-
-    fn storage (self) -> S {
-        self.storage
-    }
     fn set <V: Serialize> (&mut self, key: &[u8], value: V) -> StdResult<()> {
         self.storage.set(key, &to_vec(&value)?);
         Ok(())
@@ -45,19 +59,10 @@ impl<S: Storage, A: Api, Q: Querier> Composable<S, A, Q> for Extern<S, A, Q> {
     fn get_ns <V: DeserializeOwned> (&self, ns: &[u8], key: &[u8]) -> StdResult<V> {
         self.get(&concat(ns, key))
     }
-
-    fn api (self) -> A {
-        self.api
-    }
     fn humanize <V: Humanize<U>, U: Canonize<V>> (&self, value: V) -> StdResult<U> {
         value.humanize(&self.api)
     }
     fn canonize <V: Canonize<U>, U: Humanize<V>> (&self, value: V) -> StdResult<U> {
         value.canonize(&self.api)
     }
-
-    fn querier (self) -> Q {
-        self.querier
-    }
-
 }
