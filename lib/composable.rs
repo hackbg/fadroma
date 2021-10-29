996c369b14b4e5af9d261156c975e6760d84814a
Adam Avramov <adam.b.avramov@gmail.com>
2021-10-29 05:10:11 +0200

    fix(composable): return None instead of Err on missing values

--- lib/composable.rs
+++ lib/composable.rs
@@ -2,7 +2,6 @@ use crate::scrt::{Extern, Storage, Api, Querier, StdResult, StdError, to_vec, fr
 use crate::scrt_addr::{Humanize, Canonize};
 use crate::scrt_storage::concat;
 use serde::{Serialize, de::DeserializeOwned};
-use std::cell::RefCell;
 
 pub trait BaseComposable<S, A, Q> {
     fn storage     (&self)     -> &S;
@@ -32,9 +31,9 @@ pub trait Composable<S, A, Q>: BaseComposable<S, A, Q> {
     fn set_ns <V: Serialize> (&mut self, ns: &[u8], key: &[u8], value: V)
         -> StdResult<()>;
     fn get <V: DeserializeOwned> (&self, key: &[u8])
-        -> StdResult<V>;
+        -> StdResult<Option<V>>;
     fn get_ns <V: DeserializeOwned> (&self, ns: &[u8], key: &[u8])
-        -> StdResult<V>;
+        -> StdResult<Option<V>>;
     fn humanize <V: Humanize<U>, U: Canonize<V>> (&self, value: V) ->
         StdResult<U>;
     fn canonize <V: Canonize<U>, U: Humanize<V>> (&self, value: V)
@@ -43,20 +42,21 @@ pub trait Composable<S, A, Q>: BaseComposable<S, A, Q> {
 
 impl<S: Storage, A: Api, Q: Querier> Composable<S, A, Q> for Extern<S, A, Q> {
     fn set <V: Serialize> (&mut self, key: &[u8], value: V) -> StdResult<()> {
-        self.storage.set(key, &to_vec(&value)?);
+        self.storage.set(key, &to_vec(&Some(value))?);
         Ok(())
     }
     fn set_ns <V: Serialize> (&mut self, ns: &[u8], key: &[u8], value: V) -> StdResult<()> {
         self.set(&concat(ns, key), value)
     }
-    fn get <V: DeserializeOwned> (&self, key: &[u8]) -> StdResult<V> {
+    fn get <V: DeserializeOwned> (&self, key: &[u8]) -> StdResult<Option<V>> {
         if let Some(data) = self.storage.get(key) {
             Ok(from_slice(&data)?)
         } else {
-            Err(StdError::generic_err(format!("{:?}: not found in storage", &key)))
+            Ok(None)
+            //Err(StdError::generic_err(format!("{:?}: not found in storage", &key)))
         }
     }
-    fn get_ns <V: DeserializeOwned> (&self, ns: &[u8], key: &[u8]) -> StdResult<V> {
+    fn get_ns <V: DeserializeOwned> (&self, ns: &[u8], key: &[u8]) -> StdResult<Option<V>> {
         self.get(&concat(ns, key))
     }
     fn humanize <V: Humanize<U>, U: Canonize<V>> (&self, value: V) -> StdResult<U> {
