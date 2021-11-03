3aa5d9aec3d36a938fd38180ad92737ebcc182fa
aakamenov <asparuh@hack.bg>
2021-11-03 16:27:06 +0200

    Add pop method and tests

--- lib/scrt_storage.rs
+++ lib/scrt_storage.rs
@@ -87,7 +87,7 @@ pub fn concat(
 /// Stores items in a way that allows for iterating over them.
 pub struct IterableStorage<T: DeserializeOwned + Serialize> {
     ns: &'static [u8],
-    index: Option<u64>,
+    len: Option<u64>,
     data: PhantomData<T>
 }
 
@@ -101,7 +101,7 @@ impl<T: DeserializeOwned + Serialize> IterableStorage<T> {
     pub fn new(ns: &'static [u8]) -> Self {
         Self {
             ns,
-            index: None,
+            len: None,
             data: PhantomData
         }
     }
@@ -113,10 +113,17 @@ impl<T: DeserializeOwned + Serialize> IterableStorage<T> {
     /// Returns the index at which the item is stored at.
     pub fn push(&mut self, storage: &mut impl Storage, value: &T) -> StdResult<u64> {
         let index = self.increment_index(storage)?;
-
         ns_save(storage, self.ns, &index.to_be_bytes(), value)?;
 
         Ok(index)
+    }
+
+    /// Removes the item at the end of the collection.
+    pub fn pop(&mut self, storage: &mut impl Storage) -> StdResult<()> {
+        let index = self.decrement_index(storage)?;
+        ns_remove(storage, self.ns, &index.to_be_bytes());
+
+        Ok(())
     }
 
     #[inline]
@@ -125,8 +132,8 @@ impl<T: DeserializeOwned + Serialize> IterableStorage<T> {
     }
 
     pub fn len(&self, storage: &impl Storage) -> StdResult<u64> {
-        if let Some(index) = self.index {
-            return Ok(index)
+        if let Some(len) = self.len {
+            return Ok(len)
         }
 
         let result: Option<u64> = ns_load(storage, self.ns, Self::KEY_INDEX)?;
@@ -135,13 +142,23 @@ impl<T: DeserializeOwned + Serialize> IterableStorage<T> {
     }
 
     fn increment_index(&mut self, storage: &mut impl Storage) -> StdResult<u64> {
-        let prev = self.len(storage)?;
-        let index = prev + 1;
+        let current = self.len(storage)?;
+        let new = current + 1;
 
-        ns_save(storage, self.ns, Self::KEY_INDEX, &index)?;
-        self.index = Some(index);
+        ns_save(storage, self.ns, Self::KEY_INDEX, &new)?;
+        self.len = Some(new);
 
-        Ok(prev)
+        Ok(current)
+    }
+
+    fn decrement_index(&mut self, storage: &mut impl Storage) -> StdResult<u64> {
+        let current = self.len(storage)?;
+        let new = current.saturating_sub(1);
+
+        ns_save(storage, self.ns, Self::KEY_INDEX, &new)?;
+        self.len = Some(new);
+
+        Ok(new)
     }
 }
 
@@ -225,3 +242,76 @@ impl<'a, T: DeserializeOwned, S: Storage> DoubleEndedIterator for StorageIterato
 }
 
 impl<'a, T: DeserializeOwned, S: Storage> ExactSizeIterator for StorageIterator<'a, T, S> { }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{mock_dependencies};
+
+    #[test]
+    fn iterable_storage_insertion() {
+        let ref mut deps = mock_dependencies(20, &[]);
+
+        let mut storage = IterableStorage::<u8>::new(b"numbers");
+        
+        for i in 0..5 {
+            storage.push(&mut deps.storage, &i).unwrap();
+        }
+
+        assert_eq!(storage.len(&deps.storage).unwrap(), 5);
+
+        storage.pop(&mut deps.storage).unwrap();
+        assert_eq!(storage.len(&deps.storage).unwrap(), 4);
+
+        // Create new to invalidate cached len
+        let mut storage = IterableStorage::<u8>::new(b"numbers");
+
+        assert_eq!(storage.len(&deps.storage).unwrap(), 4);
+
+        storage.pop(&mut deps.storage).unwrap();
+        assert_eq!(storage.len.unwrap(), 3);
+        assert_eq!(storage.len(&deps.storage).unwrap(), 3);
+
+        storage.push(&mut deps.storage, &3).unwrap();
+        assert_eq!(storage.len.unwrap(), 4);
+        assert_eq!(storage.len(&deps.storage).unwrap(), 4);
+
+        let item = storage.get_at(&deps.storage, 0).unwrap();
+        assert_eq!(item.unwrap(), 0);
+
+        let item = storage.get_at(&deps.storage, 3).unwrap();
+        assert_eq!(item.unwrap(), 3);
+
+        let item = storage.get_at(&deps.storage, 4).unwrap();
+        assert!(item.is_none());
+    }
+
+    #[test]
+    fn iterable_storage_iter() {
+        let ref mut deps = mock_dependencies(20, &[]);
+
+        let mut storage = IterableStorage::<u8>::new(b"numbers");
+        
+        for i in 1..=6 {
+            storage.push(&mut deps.storage, &i).unwrap();
+        }
+
+        let mut iter = storage.iter(&deps.storage).unwrap();
+        assert_eq!(iter.len(), 6);
+        
+        assert_eq!(iter.next().unwrap().unwrap(), 1);
+        assert_eq!(iter.next_back().unwrap().unwrap(), 6);
+        assert_eq!(iter.next_back().unwrap().unwrap(), 5);
+        assert_eq!(iter.next().unwrap().unwrap(), 2);
+        assert_eq!(iter.next().unwrap().unwrap(), 3);
+        assert_eq!(iter.next().unwrap().unwrap(), 4);
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next_back(), None);
+
+        let mut iter = storage.iter(&deps.storage).unwrap();
+        assert_eq!(iter.nth_back(4).unwrap().unwrap(), 2);
+        assert_eq!(iter.nth(0).unwrap().unwrap(), 1);
+        assert_eq!(iter.nth_back(0), None);
+        assert_eq!(iter.nth(0), None);
+    }
+}
