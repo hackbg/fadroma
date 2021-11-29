cf19c7a4fefd5cb6e23422686ff82ffa6f3d00c6
Adam A <adam@hack.bg>
2021-11-29 14:19:42 +0200

    refactor(tools): move default state base from artifacts to receipts

--- tools/ToolsConstants.ts
+++ tools/ToolsConstants.ts
@@ -2,4 +2,4 @@ import { resolve, dirname, fileURLToPath, cwd } from './ToolsSystem'
 
 export const __dirname = dirname(fileURLToPath(import.meta.url))
 
-export const defaultStateBase = resolve(cwd(), 'artifacts')
+export const defaultStateBase = resolve(cwd(), 'receipts')
