0cee63b8ced005dba1caacc3b9b55751eaa72327
Adam A <adam@hack.bg>
2021-12-14 02:05:01 +0200

    fix(crates/fadroma): reexport auth, math

--- crates/fadroma/lib.rs
+++ crates/fadroma/lib.rs
@@ -1,4 +1,5 @@
 pub use fadroma_auth as auth;
+pub use fadroma_auth::*;
 pub use fadroma_auth_proc as auth_proc;
 
 pub use fadroma_composability as composability;
@@ -11,9 +12,11 @@ pub use fadroma_derive_contract as derive_contract;
 
 pub use fadroma_killswitch as killswitch;
 
-pub use fadroma_platform_scrt as scrt;
-pub use fadroma_platform_scrt::*;
+pub use fadroma_math as math;
+pub use fadroma_math::*;
 
+pub use fadroma_platform_scrt as platform;
+pub use fadroma_platform_scrt::*;
 
 pub use fadroma_snip20_api::*;
 
