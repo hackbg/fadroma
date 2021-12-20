ffcc10580d778375d19125f7788d2f0b1f340364
Adam A <adam@hack.bg>
2021-12-20 13:38:13 +0200

    fix(admin): don't reexport generated Msg structs

--- crates/fadroma-auth/lib.rs
+++ crates/fadroma-auth/lib.rs
@@ -1,5 +1,18 @@
 pub mod admin;
-pub use admin::*;
+pub use admin::{
+    Admin,
+    load_admin,
+    save_admin,
+    load_pending_admin,
+    save_pending_admin,
+    assert_admin
+    // ...but not InitMsg, HandleMsg, or QueryMsg,
+    // because those would clash with the contract's messages
+    // in the case of a glob import (`use fadroma::*`).
+    // If you need those, `use fadroma::admin::{InitMsg, ...}`
+};
+
+pub mod multi_admin;
 
 pub mod permit;
 pub use permit::*;
