1e6af65601b6f9ef181c6e6da2ba32bf76d25ad3
aakamenov <asparuh@hack.bg>
2021-12-20 15:38:09 +0200

    Skip WASM target.

--- crates/fadroma-ensemble/lib.rs
+++ crates/fadroma-ensemble/lib.rs
@@ -1,13 +1,21 @@
+#[cfg(not(target_arch = "wasm32"))]
 mod ensemble;
+#[cfg(not(target_arch = "wasm32"))]
 mod env;
+#[cfg(not(target_arch = "wasm32"))]
 mod querier;
+#[cfg(not(target_arch = "wasm32"))]
 mod storage;
+#[cfg(not(target_arch = "wasm32"))]
 mod revertable;
+#[cfg(not(target_arch = "wasm32"))]
 mod bank;
 
 #[cfg(test)]
 mod tests;
-
+#[cfg(not(target_arch = "wasm32"))]
 pub use ensemble::*;
+#[cfg(not(target_arch = "wasm32"))]
 pub use env::*;
+#[cfg(not(target_arch = "wasm32"))]
 pub use querier::*;
