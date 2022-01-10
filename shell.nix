49295e01cf9f336e5213b510ed4d4c4aa877482f
Adam A <adam@hack.bg>
2022-01-11 01:00:30 +0200

    ci(kabinet,komandi,konzola): add gh actions pipeline for micro-libs

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 {
   arsenal ? (builtins.fetchGit {
     url = "git@github.com:hackbg/arsenal.git";
-    rev = "68dba155a4a1c8c036c7cf255728b411a92e0223";
-    ref = "refactor-1";
+    rev = "5e88ce440d433f3bdce88a1f10c54bbdda84ea54";
+    ref = "main";
   }),
-  pkgs    ? import <nixpkgs> { overlays = [ (import arsenal) ]; },
+  pkgs ? import <nixpkgs> { overlays = [ (import arsenal) ]; },
   ...
 }: pkgs.mkShell {
   name = "hackbg-fadroma-dev";
@@ -13,10 +13,9 @@
     hackbg.neovim
     hackbg.rust
     hackbg.util
-    electron_6
+    act
   ];
   shellHook = ''
     export PS1='\n\e[0;35mғᴀᴅʀᴏᴍᴀ ⬢ \w\e[0m '
-    export ELECTRON_RUNTIME='${pkgs.electron_6.out}/bin/electron'
   '';
 }
