5148048273ba7ba11e548d85d0e69dd5ee6b3fd8
Adam Avramov <adam@hack.bg>
2022-02-04 01:02:27 +0200

    docs(composability): remove old comment

--- crates/fadroma-composability/lib.rs
+++ crates/fadroma-composability/lib.rs
@@ -1,5 +1,5 @@
 pub mod composable;
-pub use composable::*; // !! clash with rust `core`
+pub use composable::*;
 
 #[cfg(not(target_arch="wasm32"))] pub mod composable_test;
 #[cfg(not(target_arch="wasm32"))] pub use composable_test::*;
