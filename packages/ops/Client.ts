f94f9fdfd174bc4c41fe38c07e6dd9d6bc704340
Adam A <adam@hack.bg>
2022-02-15 16:15:43 +0200

    feat(Client): switchAgent: pass full props

--- packages/ops/Client.ts
+++ packages/ops/Client.ts
@@ -35,9 +35,8 @@ export abstract class Client {
     this.agent.execute(this, msg)
 
   switchAgent = (agent: Agent) => new (this.constructor as ClientConstructor<typeof this>)({
+    ...this,
     agent,
-    address:  this.address,
-    codeHash: this.codeHash
   })
 
 }
