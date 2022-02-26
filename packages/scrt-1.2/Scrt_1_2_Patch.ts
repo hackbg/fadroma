1b3820a525c0ea4eb65304c56bb31875065504b7
Adam A <adam@hack.bg>
2022-02-26 22:20:46 +0200

    fix(scrt-1.2): better signing client patch

--- packages/scrt-1.2/Scrt_1_2_Patch.ts
+++ packages/scrt-1.2/Scrt_1_2_Patch.ts
@@ -3,7 +3,7 @@ import { SigningCosmWasmClient, BroadcastMode, InstantiateResult, ExecuteResult 
 export class PatchedSigningCosmWasmClient_1_2 extends SigningCosmWasmClient {
 
   submitRetries      = 10
-  resubmitDelay      = 1000
+  resultSubmitDelay  = 1000
   blockQueryInterval = 1000
   resultRetries      = 10
   resultRetryDelay   = 2000
@@ -40,7 +40,8 @@ export class PatchedSigningCosmWasmClient_1_2 extends SigningCosmWasmClient {
 
   async postTx (tx: any): Promise<any> {
 
-    // Only override for non-default broadcast modes
+    // 1. This patch only works in non-default broadcast modes (Sync or Async);
+    //    in Block mode there is no way to get the tx hash of a half-failed TX.
     if (this.restClient.broadcastMode === BroadcastMode.Block) {
       console.warn(
         '[@fadroma/scrt-1.2] Broadcast mode is set to BroadcastMode.Block, bypassing patch'
@@ -48,70 +49,86 @@ export class PatchedSigningCosmWasmClient_1_2 extends SigningCosmWasmClient {
       return super.postTx(tx)
     }
 
+    // 2. Loop until we run out of retries or a TX result is successfully obtained.
+    let id: string|null = null
     let submitRetries = this.submitRetries
     while (submitRetries--) {
 
-      // 1. Submit the transaction
+      // 3. Store the block height at which the TX was sent
       const sent = (await this.getBlock()).header.height
-      const {transactionHash: id} = await super.postTx(tx)
 
-      // 2. Poll for block height to increment
-      await this.waitForNextBlock(sent)
-
-      // 3. Start querying for the full result.
+      // 4. Submit the transaction
       try {
-        return await this.getTxResult(id)
-      } catch (error) {
-        if (error.rethrow) {
-          // 4. If the transaction resulted in an error, rethrow it so it can be decrypted
-          console.warn(`Transaction ${id} returned error:\n${error.message}`)
-          throw error
+        const result = await super.postTx(tx)
+        id = result.transactionHash
+      } catch (e) {
+        id = null
+        if (this.shouldRetry(e.message)) {
+          console.warn(`Submitting TX failed (${e.message}): ${submitRetries} retries left...`)
+          await new Promise(ok=>setTimeout(ok, this.resultSubmitDelay))
         } else {
-          // 5. If the transaction simply hasn't committed yet, query for the result again.
-          console.info(`Submit TX: ${submitRetries} retries left...`)
-          await new Promise(ok=>setTimeout(ok, this.resubmitDelay))
+          console.warn(`Submitting TX failed (${e.message}): not retrying`)
+          throw e
+        }
+      }
+
+      // 5. If we got a transaction hash, start querying for the full transaction info.
+      if (id) {
+        try {
+          return await this.getTxResult(id)
+        } catch (e) {
+          if (this.shouldRetry(e.message)) {
+            // 4. If the transaction simply hasn't committed yet,
+            //    query for the result again until we run out of retries.
+            console.info(`Getting result of TX ${id} failed (${e.message}): ${submitRetries} retries left...`)
+            await new Promise(ok=>setTimeout(ok, this.resultSubmitDelay))
+          } else {
+            // 5. If the transaction resulted in an error, rethrow it so it can be decrypted
+            //    FIXME: is this necessary now that txById is being used?
+            console.info(`Getting result of TX ${id} failed (${e.message}): not retrying`)
+            throw e
+          }
         }
       }
 
     }
+
+    throw new Error(`TX ${id} failed after ${this.submitRetries} retries.`)
 
   }
 
   async getTxResult (id: string) {
 
+    // 1. Loop until we run out of retires or we successfully get a TX result
     let resultRetries = this.resultRetries
-
     while (resultRetries--) {
 
       try {
 
-        console.debug(`[@fadroma/scrt-1.2] Requesting result of tx ${id}`)
+        // 2. Try getting the transaction by id
+        console.debug(`[@fadroma/scrt-1.2] Requesting result of TX ${id}`)
         const result = await this.restClient.txById(id)
         const {raw_log, logs = []} = result as any
 
-        if (this.shouldRethrow(raw_log)) {
-          console.warn(`[@fadroma/scrt-1.2] Transaction ${id} failed`)
+        // 3. If the raw log contains a known failure message, throw error
+        if (!this.shouldRetry(raw_log, true)) {
+          console.warn(`[@fadroma/scrt-1.2] TX ${id} failed`)
           throw new Error(raw_log)
         }
 
+        // 4. Set tx hash and logs on the tx result and return it
         Object.assign(result, { transactionHash: id, logs })
-
         return result
 
-      } catch (error) {
+      } catch (e) {
 
-        // retry only on 404, throw all other errors to decrypt them
-        if (!error.message.includes('404')) {
-          Object.assign(error, { rethrow: true })
-          throw error
+        if (this.shouldRetry(e.message)) {
+          console.info(`Getting result of TX ${id} failed (${e.message}): ${resultRetries} retries left...`)
+          await new Promise(ok=>setTimeout(ok, this.resultRetryDelay))
+        } else {
+          console.info(`Getting result of TX ${id} failed (${e.message}): not retrying`)
+          throw e
         }
-
-        if (process.env.FADROMA_PRINT_TXS) {
-          //console.warn(error.message)
-          //console.info(`Requesting result of ${id}: ${resultRetries} retries left`)
-        }
-
-        await new Promise(ok=>setTimeout(ok, this.resultRetryDelay))
 
       }
 
@@ -119,29 +136,38 @@ export class PatchedSigningCosmWasmClient_1_2 extends SigningCosmWasmClient {
 
   }
 
-  private shouldRethrow (raw_log: string): boolean {
+  private shouldRetry (message: string, isActuallyOk: boolean = false): boolean {
 
-    // out of gas fails immediately
-    if (raw_log.includes('out of gas')) {
-      console.warn('[@fadroma/scrt-1.2] Out of gas, not retrying')
+    if (message.includes('404')) {
+      console.warn('[@fadroma/scrt-1.2] Commit lag, retrying')
       return true
     }
 
-    // account sequence mismatch is retried
-    if (raw_log.includes('account sequence mismatch')) {
-      console.warn('[@fadroma/scrt-1.2] Nonce lag, retrying')
+    // out of gas fails immediately
+    if (message.includes('out of gas')) {
+      console.warn('[@fadroma/scrt-1.2] Out of gas, not retrying')
       return false
     }
 
-    // tx failures are thrown
-    if (raw_log.includes('failed')) {
-      console.warn('[@fadroma/scrt-1.2] Transaction failed, not retrying')
+    // account sequence mismatches are retried
+    if (message.includes('account sequence mismatch')) {
+      console.warn('[@fadroma/scrt-1.2] Nonce lag, retrying')
       return true
     }
 
+    // tx failures are thrown
+    if (message.includes('failed')) {
+      console.warn('[@fadroma/scrt-1.2] TX failed, not retrying')
+      return false
+    }
+
     // all other errors are retried
-    console.warn('[@fadroma/scrt-1.2] Fetching tx result failed, retrying')
-    return false
+    if (!isActuallyOk) {
+      console.warn('[@fadroma/scrt-1.2] Fetching tx result failed, retrying')
+    } else {
+      //console.info('[@fadroma/scrt-1.2] Fetching tx result succeeded')
+    }
+    return true
 
   }
 
