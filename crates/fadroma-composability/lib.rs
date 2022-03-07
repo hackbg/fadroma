f8dbe8d5419e3d8920a1e45d6b60878f937e6d00
wiz <wizoftime92@gmail.com>
2022-03-07 16:22:11 +0100

    add multilevel namespaces and remove functions

--- crates/fadroma-composability/lib.rs
+++ crates/fadroma-composability/lib.rs
@@ -1,11 +1,16 @@
 pub mod composable;
 pub use composable::*;
 
-#[cfg(not(target_arch="wasm32"))] pub mod composable_test;
-#[cfg(not(target_arch="wasm32"))] pub use composable_test::*;
+#[cfg(not(target_arch = "wasm32"))]
+pub mod composable_test;
+#[cfg(not(target_arch = "wasm32"))]
+pub use composable_test::*;
 
 pub mod dispatch;
 pub use dispatch::*;
 
 pub mod response;
 pub use response::*;
+
+mod namespace_helpers;
+use namespace_helpers::*;
