477063e3905cc01a4df71a522abe0ac4187cca3b
Wiz1991 <wizoftime92@gmail.com>
2022-04-08 15:52:20 +0200

    add contract harness macro to ensemble

--- crates/fadroma-ensemble/lib.rs
+++ crates/fadroma-ensemble/lib.rs
@@ -19,3 +19,26 @@ pub use ensemble::*;
 pub use env::*;
 #[cfg(not(target_arch = "wasm32"))]
 pub use querier::*;
+
+#[macro_export]
+macro_rules! impl_contract_harness_default {
+    ($name:ident, $contract:ident) => {
+        impl ContractHarness for $name {
+            fn init(&self, deps: &mut MockDeps, env: Env, msg: Binary) -> StdResult<InitResponse> {
+                $contract::init(deps, env, from_binary(&msg)?, $contract::DefaultImpl)
+            }
+            fn handle(
+                &self,
+                deps: &mut MockDeps,
+                env: Env,
+                msg: Binary,
+            ) -> StdResult<HandleResponse> {
+                $contract::handle(deps, env, from_binary(&msg)?, $contract::DefaultImpl)
+            }
+            fn query(&self, deps: &MockDeps, msg: Binary) -> StdResult<Binary> {
+                $contract::query(deps, from_binary(&msg)?, $contract::DefaultImpl)
+            }
+        }
+    };
+}
+
