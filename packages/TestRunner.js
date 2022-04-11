5082e042cb86c29c8448f07514b54bfc5d12efd8
Adam A <adam@hack.bg>
2022-04-11 17:39:13 +0300

    fix(deps): replace @ganesha/runspec with @hackbg/runspec

--- packages/TestRunner.js
+++ packages/TestRunner.js
@@ -1,4 +1,4 @@
-import runTest from '@ganesha/runspec'
+import runTest from '@hackbg/runspec'
 import suites from './ops/index.spec.js.md'
 import scrtSuites from './scrt/Scrt.spec.ts.md'
 runTest({...suites, ...scrtSuites}, process.argv.slice(2))
