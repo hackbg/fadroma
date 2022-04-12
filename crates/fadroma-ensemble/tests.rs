b5ac93b8efde90b968177614f846b17a7214e441
Adam A <adam@hack.bg>
2022-04-12 15:53:00 +0300

    fix(ensemble): bring back 3 boxes in ensemble tests

--- crates/fadroma-ensemble/tests.rs
+++ crates/fadroma-ensemble/tests.rs
@@ -454,7 +454,7 @@ fn auto_increment() {
         time: 7,
     });
 
-    let block_height_contract = ensemble.register(BlockHeight);
+    let block_height_contract = ensemble.register(Box::new(BlockHeight));
 
     let block_height = ensemble
         .instantiate(
@@ -514,7 +514,7 @@ fn random_increment() {
         time_range: (1, 8),
     });
 
-    let block_height_contract = ensemble.register(BlockHeight);
+    let block_height_contract = ensemble.register(Box::new(BlockHeight));
 
     let block_height = ensemble
         .instantiate(
@@ -561,7 +561,7 @@ fn random_increment() {
 fn no_auto_increment() {
     let mut ensemble = ContractEnsemble::new(50);
 
-    let block_height_contract = ensemble.register(BlockHeight);
+    let block_height_contract = ensemble.register(Box::new(BlockHeight));
 
     let block_height = ensemble
         .instantiate(
