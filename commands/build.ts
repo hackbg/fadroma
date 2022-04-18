1dc75b98c47042b9dd019babd1fc6afdf2fdfdd1
Adam A <adam@hack.bg>
2022-04-18 09:08:13 +0300

    feat(cli): build single contract from Cargo.toml

--- commands/build.ts
+++ commands/build.ts
@@ -1,39 +1,78 @@
-import { Source, Scrt_1_2, Console, bold, resolve, relative, cwd } from '../index'
+import {
+  Source, Scrt_1_2,
+  Console, bold,
+  resolve, relative, basename, dirname, cwd,
+  statSync, readFileSync,
+  TOML
+} from '../index'
+
 const console = Console('fadroma build')
-let [buildScript,...buildArgs] = process.argv.slice(2)
-buildScript = resolve(buildScript)
-const buildSetName = buildArgs.join(' ')
-console.info(bold('Build script:'), relative(cwd(), buildScript))
-console.info(bold('Build set:   '), buildSetName || bold('(none)'))
-import(resolve(buildScript)).then(({default: buildSets})=>{
-  if (buildArgs.length > 0) {
-    const buildSet = buildSets[buildSetName]
-    if (!buildSet) {
-      console.error(`No build set ${bold(buildSetName)}.`)
-      list(buildSets)
-      process.exit(1)
-    } else if (!(buildSet instanceof Function)) {
-      console.error(`Invalid build set ${bold(buildSetName)} - must be function, got: ${typeof buildSet}`)
-      process.exit(2)
-    } else {
-      const buildSources = buildSet()
-      if (!(buildSources instanceof Array)) {
-        console.error(`Invalid build set ${bold(buildSetName)} - must return Array<Source>, got: ${typeof buildSources}`)
-        process.exit(3)
+let [buildManifest,...buildArgs] = process.argv.slice(2)
+
+buildManifest = resolve(buildManifest)
+if (statSync(buildManifest).isDirectory()) {
+  buildManifest = resolve(buildManifest, 'Cargo.toml')
+}
+if (basename(buildManifest) === 'Cargo.toml') {
+  buildFromCargoToml(buildManifest, buildArgs)
+} else {
+  buildFromBuildScript(buildManifest, buildArgs)
+}
+
+function buildFromCargoToml (cargoToml, buildArgs) {
+  console.info(bold('Build manifest:'), relative(cwd(), cargoToml))
+  const workspace = dirname(cargoToml)
+  const manifest  = TOML.parse(readFileSync(cargoToml, 'utf8'))
+  const source    = new Source(workspace, manifest.package.name)
+  Scrt_1_2.getBuilder().build(source)
+    .then(artifact=>{
+      console.info('Built', artifact)
+      process.exit(0)
+    })
+    .catch(e=>{
+      console.error(`Build failed.`)
+      console.error(e)
+      process.exit(5)
+    })
+}
+
+function buildFromBuildScript (buildScript, buildArgs) {
+  const buildSetName = buildArgs.join(' ')
+  console.info(bold('Build script:'), relative(cwd(), buildScript))
+  console.info(bold('Build set:   '), buildSetName || bold('(none)'))
+  import(resolve(buildScript)).then(({default: buildSets})=>{
+    if (buildArgs.length > 0) {
+      const buildSet = buildSets[buildSetName]
+      if (!buildSet) {
+        console.error(`No build set ${bold(buildSetName)}.`)
+        list(buildSets)
+        process.exit(1)
+      } else if (!(buildSet instanceof Function)) {
+        console.error(`Invalid build set ${bold(buildSetName)} - must be function, got: ${typeof buildSet}`)
+        process.exit(2)
+      } else {
+        const buildSources = buildSet()
+        if (!(buildSources instanceof Array)) {
+          console.error(`Invalid build set ${bold(buildSetName)} - must return Array<Source>, got: ${typeof buildSources}`)
+          process.exit(3)
+        }
+        Scrt_1_2.getBuilder().buildMany(buildSources)
+          .then(()=>{
+            console.info('Build complete.')
+            process.exit(0)
+          })
+          .catch(e=>{
+            console.error(`Build failed.`)
+            console.error(e)
+            process.exit(4)
+          })
       }
-      Scrt_1_2.getBuilder().buildMany(buildSources)
-        .then(()=>console.info('Build complete.'))
-        .catch(e=>{
-          console.error(`Build failed.`)
-          console.error(e)
-          process.exit(4)
-        })
+    } else {
+      console.warn(bold('No build set specified.'))
+      list(buildSets)
     }
-  } else {
-    console.warn(bold('No build set specified.'))
-    list(buildSets)
-  }
-})
+  })
+}
 
 function list (buildSets) {
   console.log('Available build sets:')
---
+++ fixtures/empty-contract/lib.rs
@@ -0,0 +1,62 @@
+use fadroma::{
+    Storage, Api, Querier,
+    Extern, Env,
+    InitResponse, HandleResponse, Binary,
+    StdResult,
+    schemars
+};
+
+pub(crate) fn init<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>, env: Env, msg: InitMsg,
+) -> StdResult<InitResponse> {
+    Ok(InitResponse::default())
+}
+
+pub(crate) fn handle<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>, env: Env, msg: HandleMsg,
+) -> StdResult<HandleResponse> {
+    Ok(HandleResponse::default())
+}
+
+pub(crate) fn query<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>, msg: QueryMsg,
+) -> StdResult<Binary> {
+    Ok(Binary(vec![]))
+}
+
+#[derive(serde::Serialize, serde::Deserialize, schemars::JsonSchema)]
+pub enum InitMsg {}
+#[derive(serde::Serialize, serde::Deserialize, schemars::JsonSchema)]
+pub enum HandleMsg {}
+#[derive(serde::Serialize, serde::Deserialize, schemars::JsonSchema)]
+pub enum QueryMsg {}
+
+#[cfg(target_arch = "wasm32")]
+mod wasm {
+    use fadroma::platform::{
+        do_handle, do_init, do_query, ExternalApi, ExternalQuerier, ExternalStorage,
+    };
+    #[no_mangle]
+    extern "C" fn init(env_ptr: u32, msg_ptr: u32) -> u32 {
+        do_init(
+            &super::init::<ExternalStorage, ExternalApi, ExternalQuerier>,
+            env_ptr,
+            msg_ptr,
+        )
+    }
+    #[no_mangle]
+    extern "C" fn handle(env_ptr: u32, msg_ptr: u32) -> u32 {
+        do_handle(
+            &super::handle::<ExternalStorage, ExternalApi, ExternalQuerier>,
+            env_ptr,
+            msg_ptr,
+        )
+    }
+    #[no_mangle]
+    extern "C" fn query(msg_ptr: u32) -> u32 {
+        do_query(
+            &super::query::<ExternalStorage, ExternalApi, ExternalQuerier>,
+            msg_ptr,
+        )
+    }
+}
