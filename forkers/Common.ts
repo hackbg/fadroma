60bd5326140fff123bf1e46efd111cbf40529466
Adam A <adam@hack.bg>
2022-05-08 09:04:34 +0300

    chore(forkers): remove old stuff

--- forkers/Common.ts
+++
@@ -1,4 +0,0 @@
-export function isWorkerRuntime() {
-  const isWindowContext = typeof self !== "undefined" && typeof Window !== "undefined" && self instanceof Window
-  return typeof self !== "undefined" && self.postMessage && !isWindowContext ? true : false
-}
