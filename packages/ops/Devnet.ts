f5c5ade2299a40916f105f16a6b3180a40f8d51b
Adam A <adam@hack.bg>
2022-05-17 23:51:12 +0300

    fix(ops): devnet fixes

--- packages/ops/Devnet.ts
+++ packages/ops/Devnet.ts
@@ -4,9 +4,9 @@ import { basename, relative, resolve } from 'path'
 import { cwd } from 'process'
 import { existsSync, readlinkSync, symlinkSync } from 'fs'
 import { Console, bold } from '@hackbg/konzola'
-import { Path, Directory, JSONDirectory, JSONFile, mkdirp } from '@hackbg/kabinet'
+import { Path, Directory, JSONFormat, JSONFile, JSONDirectory } from '@hackbg/kabinet'
 import { freePort, waitPort } from '@hackbg/portali'
-import { Docker, DockerImage, waitUntilLogsSay } from '@hackbg/dokeres'
+import { Dokeres, DokeresImage, DokeresContainer, waitUntilLogsSay } from '@hackbg/dokeres'
 import { randomHex } from '@hackbg/toolbox'
 
 import { config } from './Config'
@@ -44,7 +44,7 @@ export abstract class Devnet {
     }
     stateRoot = stateRoot || resolve(config.projectRoot, 'receipts', this.chainId)
     this.stateRoot = new Directory(stateRoot)
-    this.nodeState = new JSONFile(stateRoot, 'node.json')
+    this.nodeState = this.stateRoot.at('node.json').as(JSONFormat)
   }
 
   /** The chain ID that will be passed to the devnet node. */
@@ -135,34 +135,12 @@ export abstract class Devnet {
   * (https://www.npmjs.com/package/dockerode) */
 export interface DockerodeDevnetOptions extends DevnetOptions {
   /** Docker image of the chain's runtime. */
-  image?: DockerImage
+  image?:       DokeresImage
   /** Init script to launch the devnet. */
-  initScript?: string
+  initScript?:  string
   /** Once this string is encountered in the log output
     * from the container, the devnet is ready to accept requests. */
   readyPhrase?: string
-  /** Handle to Dockerode or compatible (TODO mock!) */
-  docker?: {
-    getImage (): {
-      inspect (): Promise<any>
-    }
-    pull (image: any, callback: Function): void
-    modem: {
-      followProgress (
-        stream:   any,
-        callback: Function,
-        progress: Function
-      ): any
-    }
-    getContainer (id: any): {
-      id: string,
-      start (): Promise<any>
-    }
-    createContainer (options: any): {
-      id: string
-      logs (_: any, callback: Function): void
-    }
-  }
 }
 
 /** Used to reconnect between runs. */
@@ -179,17 +157,21 @@ export class DockerodeDevnet extends Devnet {
   constructor (options: DockerodeDevnetOptions = {}) {
     super(options)
     console.info('Constructing', bold('Dockerode')+'-based devnet')
-    if (options.docker) {
-      this.docker = options.docker
-    }
-    this.identities  = this.stateRoot.subdir('identities',  JSONDirectory)
+    this.identities  = this.stateRoot.subdir('identities').as(JSONFormat)
     this.image       = options.image
     this.initScript  = options.initScript
     this.readyPhrase = options.readyPhrase
   }
 
+  get dokeres (): Dokeres {
+    return this.image.dokeres
+  }
+
   /** This should point to the standard production docker image for the network. */
-  image: DockerImage
+  image: DokeresImage
+
+  /** */
+  container: DokeresContainer|null
 
   /** Mounted into devnet container in place of default init script
     * in order to add custom genesis accounts with initial balances
@@ -201,12 +183,18 @@ export class DockerodeDevnet extends Devnet {
 
   /** Gets the info for a genesis account, including the mnemonic */
   async getGenesisAccount (name: string) {
+    console.log(this.identities)
     return this.identities.load(name)
   }
 
   /** Once this phrase is encountered in the log output
     * from the container, the devnet is ready to accept requests. */
   readyPhrase: string
+
+  /** Path under which the init script is mounted in the container. */
+  get initScriptName (): string {
+    return resolve('/', basename(this.initScript))
+  }
 
   async spawn () {
     // tell the user that we have begun
@@ -224,44 +212,80 @@ export class DockerodeDevnet extends Devnet {
         console.warn(`Failed to create ${item.path}: ${e.message}`)
       }
     }
-    // create the container
-    console.info('Launching a devnet container...')
+    // run the container
+    const containerName = `${this.chainId}-${this.port}`
+    console.info('Creating and starting devnet container:', bold(containerName))
     await this.image.ensure()
-    this.container = await this.createContainer(this.getContainerOptions())
-    const shortId = this.container.id.slice(0, 8)
-    // emit any warnings
-    if (this.container.Warnings) {
-      console.warn(`Creating container ${shortId} emitted warnings:`)
-      console.info(this.container.Warnings)
-    }
-    // report progress
-    const shortPath = relative(config.projectRoot, this.nodeState.path)
-    console.info(`Created container ${bold(shortId)} (${bold(shortPath)})...`)
-    // start the container
-    await this.startContainer(this.container.id)
-    console.info(`Started container ${shortId}...`)
+    this.container = await this.image.run(containerName, {
+      env: {
+        Port:            this.port,
+        ChainID:         this.chainId,
+        GenesisAccounts: this.genesisAccounts.join(' ')
+      },
+      exposed: [
+        `${this.port}/tcp`
+      ],
+      extra: {
+        Tty:          true,
+        AttachStdin:  true,
+        AttachStdout: true,
+        AttachStderr: true,
+        Hostname:     this.chainId,
+        Domainname:   this.chainId,
+        HostConfig:   {
+          NetworkMode: 'bridge',
+          Binds: [
+            `${this.initScript}:${resolve('/', basename(this.initScript))}:ro`,
+            `${this.stateRoot.path}:/receipts/${this.chainId}:rw`
+          ],
+          PortBindings: {
+            [`${this.port}/tcp`]: [{HostPort: `${this.port}`}]
+          }
+        }
+      }
+    }, this.initScriptName, '/bin/bash')
     // update the record
     this.save()
     // wait for logs to confirm that the genesis is done
-    await waitUntilLogsSay(this.container, this.readyPhrase, undefined, this.waitSeconds)
+    await waitUntilLogsSay(
+      this.container.container,
+      this.readyPhrase,
+      false,
+      this.waitSeconds,
+      DockerodeDevnet.logFilter
+    )
     // wait for port to be open
     await this.waitPort({ host: this.host, port: Number(this.port) })
     return this
   }
 
+  /** Overridable for testing. */
   protected waitSeconds = 7
 
+  /** Filter logs when waiting for the ready phrase. */
+  static logFilter (data: string) {
+    const RE_GARBAGE = /[\x00-\x1F]/
+    return (
+      data.length > 0                            &&
+      !data.startsWith('TRACE ')                 &&
+      !data.startsWith('DEBUG ')                 &&
+      !data.startsWith('INFO ')                  &&
+      !data.startsWith('I[')                     &&
+      !data.startsWith('Storing key:')           &&
+      !RE_GARBAGE.test(data)                     &&
+      !data.startsWith('{"app_message":')        &&
+      !data.startsWith('configuration saved to') &&
+      !(data.length>1000)
+    )
+  }
+
+  /** Overridable for testing. */
   protected waitPort = waitPort
 
-  load (): DockerodeDevnetReceipt | null {
+  async load (): Promise<DockerodeDevnetReceipt> | null {
     const data = super.load()
     if (data.containerId) {
-      const id = data.containerId
-      const Warnings = null
-      const logs = () => { throw new Error(
-        '@fadroma/ops/Devnet: tried to tail logs before creating container'
-      ) }
-      this.container = { id, Warnings, logs }
+      this.container = await this.dokeres.container(data.containerId)
     } else {
       throw new Error('@fadroma/ops/Devnet: missing container id in devnet state')
     }
@@ -284,32 +308,36 @@ export class DockerodeDevnet extends Devnet {
     // get stored info about the container was supposed to be
     let id: string
     try {
-      id = this.load().containerId
+      id = (await this.load()).containerId
     } catch (e) {
       // if node state is corrupted, spawn
       console.warn(e)
       console.info(`Reading ${bold(shortPath)} failed`)
       return this.spawn()
     }
+    this.container = await this.dokeres.container(id)
+    console.log({container: this.container})
     // check if contract is running
     let running: boolean
     try {
-      running = await this.isRunning(id)
-    } catch (_e) {
+      running = await this.container.isRunning
+    } catch (e) {
       // if error when checking, RESPAWN
-      //console.info(`✋ Failed to get container ${bold(id)}`)
-      //console.info('Error was:', e)
+      console.info(`✋ Failed to get container ${bold(id)}`)
+      console.info('Error was:', e)
       console.info(`Cleaning up outdated state...`)
       await this.erase()
       console.info(`Trying to launch a new node...`)
       return this.spawn()
     }
     // if not running, RESPAWN
-    if (!running) this.startContainer(id)
+    if (!running) {
+      await this.container.start()
+    }
     // ...and try to make sure it dies when the Node process dies
     process.on('beforeExit', () => {
       if (config.devnetEphemeral) {
-        this.killContainer(id)
+        this.container.kill()
       } else {
         console.log()
         console.info(
@@ -325,7 +353,7 @@ export class DockerodeDevnet extends Devnet {
   async kill () {
     if (this.container) {
       const { id } = this.container
-      await this.killContainer(id)
+      await this.container.kill()
       console.info(
         `Stopped container`, bold(id)
       )
@@ -334,8 +362,8 @@ export class DockerodeDevnet extends Devnet {
         `Checking if there's an old node that needs to be stopped...`
       )
       try {
-        const { containerId } = this.load()
-        await this.killContainer(containerId)
+        const { containerId } = await this.load()
+        await this.container.kill()
         console.info(`Stopped container ${bold(containerId)}.`)
       } catch (_e) {
         console.info("Didn't stop any container.")
@@ -356,99 +384,30 @@ export class DockerodeDevnet extends Devnet {
       if (e.code === 'EACCES' || e.code === 'ENOTEMPTY') {
         console.warn(`Failed to delete ${path}: ${e.message}; trying cleanup container...`)
         await this.image.ensure()
-        const container = await this.createContainer(this.getCleanupContainerOptions())
+        const containerName = `${this.chainId}-${this.port}-cleanup`
+        const options = {
+          AutoRemove: true,
+          Image:      this.image.name,
+          Entrypoint: [ '/bin/rm' ],
+          Cmd:        ['-rvf', '/state',],
+          HostConfig: { Binds: [`${this.stateRoot.path}:/state:rw`] }
+          //Tty: true, AttachStdin: true, AttachStdout: true, AttachStderr: true,
+        }
+        const cleanupContainer = await this.image.run(
+          containerName,
+          { extra: options },
+          ['-rvf', '/state'],
+          '/bin/rm'
+        )
         console.info(`Starting cleanup container...`)
-        await container.start()
+        await cleanupContainer.start()
         console.info('Waiting for cleanup to finish...')
-        await container.wait()
+        await cleanupContainer.wait()
         console.info(`Deleted ${path} via cleanup container.`)
       } else {
         console.warn(`Failed to delete ${path}: ${e.message}`)
         throw e
       }
-    }
-  }
-
-  /** Used to command the container engine. */
-  protected docker: Docker = new Docker({ socketPath: config.dockerHost||'/var/run/docker.sock' })
-
-  /** The created container */
-  container: { id: string, Warnings: any, logs: Function }
-
-  private isRunning = async (id: string = this.container.id) =>
-    (await this.docker.getContainer(id).inspect()).State.Running
-
-  private createContainer = async (options: any|Promise<any>) =>
-    await this.docker.createContainer(await Promise.resolve(options))
-
-  private startContainer = async (id: string = this.container.id) =>
-    await this.docker.getContainer(id).start()
-
-  private killContainer = async (id: string = this.container.id) => {
-    const prettyId = bold(id.slice(0,8))
-    if (await this.isRunning(id)) {
-      console.info(`Stopping ${prettyId}...`)
-      await this.docker.getContainer(id).kill()
-      console.info(`Stopped ${prettyId}`)
-    }
-  }
-
-  /** What Dockerode passes to the Docker API
-    * in order to launch a devnet container. */
-  private getContainerOptions () {
-    const {
-      chainId,
-      genesisAccounts,
-      image,
-      initScript,
-      port,
-      stateRoot
-    } = this
-    const initScriptName = resolve('/', basename(initScript))
-    return {
-      Image:        image.name,
-      Name:         `${chainId}-${port}`,
-      Env:          [ `Port=${port}`
-                    , `ChainID=${chainId}`
-                    , `GenesisAccounts=${genesisAccounts.join(' ')}` ],
-      Entrypoint:   [ '/bin/bash' ],
-      Cmd:          [ initScriptName ],
-      Tty:          true,
-      AttachStdin:  true,
-      AttachStdout: true,
-      AttachStderr: true,
-      Hostname:     chainId,
-      Domainname:   chainId,
-      ExposedPorts: { [`${port}/tcp`]: {} },
-      HostConfig:   { NetworkMode: 'bridge'
-                    //, AutoRemove:   true
-                    , Binds:
-                      [ `${initScript}:${initScriptName}:ro`
-                      , `${stateRoot.path}:/receipts/${chainId}:rw` ]
-                    , PortBindings:
-                      { [`${port}/tcp`]: [{HostPort: `${port}`}] } }
-    }
-  }
-
-  /** What Dockerode passes to the Docker API
-    * in order to launch a cleanup container
-    * (for removing root-owned devnet files
-    * without escalating on the host) */
-  private getCleanupContainerOptions () {
-    const {
-      image,
-      chainId,
-      port,
-      stateRoot
-    } = this
-    return {
-      AutoRemove: true,
-      Image:      image.name,
-      Name:       `${chainId}-${port}-cleanup`,
-      Entrypoint: [ '/bin/rm' ],
-      Cmd:        ['-rvf', '/state',],
-      HostConfig: { Binds: [`${stateRoot.path}:/state:rw`] }
-      //Tty: true, AttachStdin: true, AttachStdout: true, AttachStderr: true,
     }
   }
 
