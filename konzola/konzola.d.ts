98b9d202fc4d3476a55249ffb1b0cb6ef0d6d85a
Adam A <adam@hack.bg>
2022-05-23 23:59:36 +0300

    fix(konzola): types

--- konzola/konzola.d.ts
+++ konzola/konzola.d.ts
@@ -3,22 +3,22 @@ declare module '@hackbg/konzola' {
   export { prompts } from 'prompts'
 
   export * as colors from 'colors'
-  export const bold: (string)=>string
+  export const bold: (text: string)=>string
 
   export { render } from 'prettyjson'
 
   export { table } from 'table'
 
-  export type MakeConsole = (string) => Console
+  export type MakeConsole = (prefix: string) => Console
 
   export interface Console {
-    constructor (string)
-    log   (...args: any)
-    info  (...args: any)
-    warn  (...args: any)
-    error (...args: any)
-    debug (...args: any)
-    trace (...args: any)
+    new (prefix: string): this
+    log   (...args: any): void
+    info  (...args: any): void
+    warn  (...args: any): void
+    error (...args: any): void
+    debug (...args: any): void
+    trace (...args: any): void
   }
 
   export const Console: MakeConsole
