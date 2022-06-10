2a63852911e8f07735f84bd32668f9048834deed
Adam A <adam@hack.bg>
2022-06-10 16:19:16 +0300

    fix(fadroma): fix fn paths in entrypoint macro

--- crates/fadroma/lib.rs
+++ crates/fadroma/lib.rs
@@ -93,39 +93,26 @@ pub mod prelude {
 
 #[cfg(feature = "scrt")]
 #[macro_export] macro_rules! entrypoint {
-    ($fadroma:path, $init:path, $handle:path, $query:path) => {
+    ($fadroma:path, $($init:ident)::+, $($handle:ident)::+, $($query:ident)::+) => {
         #[cfg(target_arch = "wasm32")]
         mod wasm {
-            use super::contract;
             use $fadroma::{scrt::cosmwasm_std::{
-                do_handle, do_init, do_query,
-                ExternalApi, ExternalQuerier, ExternalStorage,
+                do_init,
+                do_handle,
+                do_query,
+                ExternalStorage as EStorage,
+                ExternalApi     as EApi,
+                ExternalQuerier as EQuerier,
             }};
-            #[no_mangle]
-            extern "C" fn init(env_ptr: u32, msg_ptr: u32) -> u32 {
-                do_init(
-                    &contract::init::<ExternalStorage, ExternalApi, ExternalQuerier>,
-                    env_ptr,
-                    msg_ptr,
-                )
+            #[no_mangle] extern "C" fn init(env_ptr: u32, msg_ptr: u32) -> u32 {
+                do_init(&super::$($init)::+::<EStorage, EApi, EQuerier>, env_ptr, msg_ptr)
             }
-            #[no_mangle]
-            extern "C" fn handle(env_ptr: u32, msg_ptr: u32) -> u32 {
-                do_handle(
-                    &contract::handle::<ExternalStorage, ExternalApi, ExternalQuerier>,
-                    env_ptr,
-                    msg_ptr,
-                )
+            #[no_mangle] extern "C" fn handle(env_ptr: u32, msg_ptr: u32) -> u32 {
+                do_handle(&super::$($handle)::+::<EStorage, EApi, EQuerier>, env_ptr, msg_ptr)
             }
-            #[no_mangle]
-            extern "C" fn query(msg_ptr: u32) -> u32 {
-                do_query(
-                    &contract::query::<ExternalStorage, ExternalApi, ExternalQuerier>,
-                    msg_ptr,
-                )
+            #[no_mangle] extern "C" fn query(msg_ptr: u32) -> u32 {
+                do_query(&super::$($query)::+::<EStorage, EApi, EQuerier>, msg_ptr)
             }
-            // Other C externs like cosmwasm_vm_version_1, allocate, deallocate are available
-            // automatically because we `use cosmwasm_std`.
         }
     }
 }
