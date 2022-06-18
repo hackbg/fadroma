8b0f07b3a8b166b3b8750b8ab646107fff2fa467
Adam A <adam@hack.bg>
2022-06-18 12:20:49 +0300

    BREAKING: fix(scrt-vk): return created VK

--- packages/client-scrt/scrt-vk.ts
+++ packages/client-scrt/scrt-vk.ts
@@ -5,24 +5,15 @@ export type ViewingKey = string
 
 export class ViewingKeyClient extends Client {
 
-  create <R> (entropy = randomBytes(32).toString("hex")): Promise<R> {
-    return this.execute({
-      create_viewing_key: { entropy, padding: null }
-    }).then((tx) => {
-      console.warn('TODO decode response from create viewing key')
-      return tx as R
-      //status: JSON.parse(decode(fromHex(tx.data))).set_viewing_key.key,
-    })
+  async create (entropy = randomBytes(32).toString("hex")) {
+    const msg    = { create_viewing_key: { entropy, padding: null } }
+    let { data } = await this.execute(msg) as { data: Uint8Array|Uint8Array[] }
+    if (data instanceof Uint8Array) data = [data]
+    return data[0]
   }
 
-  set <R> (key: unknown): Promise<R> {
-    return this.execute({
-      set_viewing_key: { key }
-    }).then((tx) => {
-      //console.info(tx)
-      return tx as R
-      //status: JSON.parse(decode(fromHex(tx.data))).set_viewing_key.key,
-    })
+  async set (key: unknown) {
+    return this.execute({ set_viewing_key: { key } })
   }
 
 }
