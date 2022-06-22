b8f3a58d875cbf290334934e5da54900c1ddea90
aakamenov <asparuh@hack.bg>
2022-06-22 16:10:41 +0300

    Fix composable storage and API.

--- crates/fadroma/composability/mod.rs
+++ crates/fadroma/composability/mod.rs
@@ -17,25 +17,25 @@ pub trait BaseComposable<S, A, Q> {
 }
 
 pub trait Composable<S, A, Q>: BaseComposable<S, A, Q> {
-    fn set<Value: Serialize>(&mut self, key: &[u8], value: Value) -> UsuallyOk;
+    fn set<Value: Serialize>(&mut self, key: &[u8], value: &Value) -> UsuallyOk;
     /// Uses a single slice as namespace and it combines it with the key
-    fn set_ns<Value: Serialize>(&mut self, ns: &[u8], key: &[u8], value: Value) -> UsuallyOk;
+    fn set_ns<Value: Serialize>(&mut self, ns: &[u8], key: &[u8], value: &Value) -> UsuallyOk;
     /// Uses an array of slices which are then combined according to the following spec:
     /// https://github.com/webmaster128/key-namespacing#nesting
     fn set_multi_ns<Value: Serialize>(
         &mut self,
         ns: &[&[u8]],
         key: &[u8],
-        value: Value,
+        value: &Value,
     ) -> UsuallyOk;
 
     fn get<Value: DeserializeOwned>(&self, key: &[u8]) -> Eventually<Value>;
     fn get_ns<Value: DeserializeOwned>(&self, ns: &[u8], key: &[u8]) -> Eventually<Value>;
     fn get_multi_ns<Value: DeserializeOwned>(&self, ns: &[&[u8]], key: &[u8]) -> Eventually<Value>;
 
-    fn remove(&mut self, key: &[u8]) -> UsuallyOk;
-    fn remove_ns(&mut self, ns: &[u8], key: &[u8]) -> UsuallyOk;
-    fn remove_multi_ns(&mut self, ns: &[&[u8]], key: &[u8]) -> UsuallyOk;
+    fn remove(&mut self, key: &[u8]);
+    fn remove_ns(&mut self, ns: &[u8], key: &[u8]);
+    fn remove_multi_ns(&mut self, ns: &[&[u8]], key: &[u8]);
 
     fn humanize<Value: Humanize>(&self, value: Value) -> StdResult<Value::Output>;
     fn canonize<Value: Canonize>(&self, value: Value) -> StdResult<Value::Output>;
@@ -67,54 +67,65 @@ macro_rules! make_composable {
     ($Struct:ty) => {
         // base trait with no generic methods, in order to to support `dyn`
         impl<S: Storage, A: Api, Q: Querier> BaseComposable<S, A, Q> for $Struct {
+            #[inline]
             fn storage(&self) -> &S {
                 &self.storage
             }
+
+            #[inline]
             fn storage_mut(&mut self) -> &mut S {
                 &mut self.storage
             }
+
+            #[inline]
             fn api(&self) -> &A {
                 &self.api
             }
+
+            #[inline]
             fn querier(&self) -> &Q {
                 &self.querier
             }
         }
 
         impl<S: Storage, A: Api, Q: Querier> Composable<S, A, Q> for $Struct {
-            fn set<Value: serde::Serialize>(&mut self, key: &[u8], value: Value) -> UsuallyOk {
-                self.storage.set(key, &to_vec(&Some(value))?);
+            #[inline]
+            fn set<Value: serde::Serialize>(&mut self, key: &[u8], value: &Value) -> UsuallyOk {
+                self.storage.set(key, &to_vec(value)?);
                 Ok(())
             }
 
+            #[inline]
             fn set_ns<Value: serde::Serialize>(
                 &mut self,
                 ns: &[u8],
                 key: &[u8],
-                value: Value,
+                value: &Value,
             ) -> UsuallyOk {
                 let ns = key_prefix(ns);
                 self.set(&concat(&ns, key), value)
             }
+
+            #[inline]
             fn set_multi_ns<Value: serde::Serialize>(
                 &mut self,
                 ns: &[&[u8]],
                 key: &[u8],
-                value: Value,
+                value: &Value,
             ) -> UsuallyOk {
                 let ns = key_prefix_nested(ns);
                 self.set(&concat(&ns, key), value)
             }
 
+            #[inline]
             fn get<Value: serde::de::DeserializeOwned>(&self, key: &[u8]) -> Eventually<Value> {
-                if let Some(data) = self.storage.get(key) {
-                    Ok(from_slice(&data)?)
-                } else {
-                    Ok(None)
-                    //Err(StdError::generic_err(format!("{:?}: not found in storage", &key)))
+                match self.storage.get(key) {
+                    Some(data) => from_slice(&data),
+                    None => Ok(None)
                 }
             }
 
+            #[inline]
             fn get_ns<Value: serde::de::DeserializeOwned>(
                 &self,
                 ns: &[u8],
@@ -123,6 +134,8 @@ macro_rules! make_composable {
                 let ns = key_prefix(ns);
                 self.get(&concat(&ns, key))
             }
+
+            #[inline]
             fn get_multi_ns<Value: serde::de::DeserializeOwned>(
                 &self,
                 ns: &[&[u8]],
@@ -132,23 +145,29 @@ macro_rules! make_composable {
                 self.get(&concat(&ns, key))
             }
 
-            fn remove(&mut self, key: &[u8]) -> UsuallyOk {
+            #[inline]
+            fn remove(&mut self, key: &[u8]) {
                 self.storage.remove(key);
-                Ok(())
-            }
-            fn remove_ns(&mut self, ns: &[u8], key: &[u8]) -> UsuallyOk {
-                let ns = key_prefix(ns);
-                self.remove(&concat(&ns, key))
-            }
-            fn remove_multi_ns(&mut self, ns: &[&[u8]], key: &[u8]) -> UsuallyOk {
-                let ns = key_prefix_nested(ns);
-                self.remove(&concat(&ns, key))
             }
 
+            #[inline]
+            fn remove_ns(&mut self, ns: &[u8], key: &[u8]) {
+                let ns = key_prefix(ns);
+                self.remove(&concat(&ns, key));
+            }
+
+            #[inline]
+            fn remove_multi_ns(&mut self, ns: &[&[u8]], key: &[u8]) {
+                let ns = key_prefix_nested(ns);
+                self.remove(&concat(&ns, key));
+            }
+
+            #[inline]
             fn humanize<Value: Humanize>(&self, value: Value) -> StdResult<Value::Output> {
                 value.humanize(&self.api)
             }
 
+            #[inline]
             fn canonize<Value: Canonize>(&self, value: Value) -> StdResult<Value::Output> {
                 value.canonize(&self.api)
             }
