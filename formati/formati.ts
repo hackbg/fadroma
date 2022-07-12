713136c97248d95ba856d0e12b3c5b5079cb355c
Adam A <adam@hack.bg>
2022-07-12 17:52:08 +0300

    fix(formati): randomBuffer

--- formati/formati.ts
+++ formati/formati.ts
@@ -1,18 +1,27 @@
 import { bech32, bech32m } from 'bech32'
-import { randomBuffer } from 'secure-random'
+
+import secureRandom from 'secure-random'
+
+export { bech32, bech32m }
+
+export { toBase64, fromBase64, fromUtf8, fromHex, toHex } from '@iov/encoding'
+
+export { Sha256 } from '@iov/crypto'
+
+export const randomBytes = secureRandom.randomBuffer
 
 export const randomHex = (bytes = 1) =>
-  randomBuffer(bytes).toString("hex")
+  secureRandom.randomBuffer(bytes).toString("hex")
 
 export const randomBase64 = (bytes = 1) =>
-  randomBuffer(bytes).toString("base64")
+  secureRandom.randomBuffer(bytes).toString("base64")
 
 /** By default this generates 32 bytes - default length of canonical addr in Cosmos */
 export const randomBech32 = (prefix = 'hackbg', bytes = 32) =>
-  bech32.encode(prefix, bech32.toWords(randomBuffer(bytes)))
+  bech32.encode(prefix, bech32.toWords(secureRandom.randomBuffer(bytes)))
 
 export const randomBech32m = (prefix = 'hackbg', bytes = 32) =>
-  bech32m.encode(prefix, bech32m.toWords(randomBuffer(bytes)))
+  bech32m.encode(prefix, bech32m.toWords(secureRandom.randomBuffer(bytes)))
 
 export function pick (
   obj = {},
@@ -28,7 +37,3 @@ export function pick (
 export function required (label) {
   return () => { throw new Error(`required: ${label}`) }
 }
-
-export { bech32, bech32m, randomBuffer as randomBytes }
-export { toBase64, fromBase64, fromUtf8, fromHex, toHex } from '@iov/encoding'
-export { Sha256 } from '@iov/crypto'
