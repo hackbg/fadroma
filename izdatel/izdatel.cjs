1697ab5aeba06ef05ddd17eb0d81255e391ae635
Adam A <adam@hack.bg>
2022-07-15 02:12:03 +0300

    fix(izdatel): support dual esm/cjs build correctly

--- izdatel/izdatel.cjs
+++ izdatel/izdatel.cjs
@@ -1,39 +1,59 @@
 const { resolve, basename, relative, join, isAbsolute } = require('path')
-const { readFileSync, writeFileSync } = require('fs')
+const { existsSync, readFileSync, writeFileSync } = require('fs')
 const { execSync, execFileSync } = require('child_process')
 const process = require('process')
 module.exports = module.exports.default = izdatel
 if (require.main === module) izdatel(process.cwd(), ...process.argv.slice(2))
 function izdatel (cwd, prepareCommand = 'npm prepare', ...publishArgs) {
+
+  // Finding some files
   const $ = (...args) => join(cwd, ...args)
   const files = {
-    packageJSON:  $('package.json'),
-    tsconfigJSON: $('tsconfig.json'),
+    packageJSON:     $('package.json'),
+    tsconfigJSON:    $('tsconfig.json'),
+    tsconfigESMJSON: $('tsconfig.esm.json'),
+    tsconfigCJSJSON: $('tsconfig.cjs.json'),
   }
-  // Get output directory from tsconfig.json
-  const { compilerOptions = {} } = JSON.parse(readFileSync(files.tsconfigJSON, 'utf8'))
-  const { outDir = '.', declaration, declarationDir = outDir } = compilerOptions
+
+  // Output directory for ESM build
+  if (!existsSync(files.tsconfigESMJSON)) throw new Error('could not find tsconfig.esm.json')
+  const {
+    compilerOptions: {
+      outDir:         outDirESM         = './dist/esm',
+      declaration:    declarationESM    = true,
+      declarationDir: declarationDirESM = outDirESM
+    }
+  } = JSON.parse(readFileSync(files.tsconfigESMJSON, 'utf8'))
+
+  // Output directory for CJS build
+  if (!existsSync(files.tsconfigCJSJSON)) throw new Error('could not find tsconfig.cjs.json')
+  const {
+    compilerOptions: {
+      outDir:         outDirCJS         = './dist/cjs',
+      declaration:    declarationCJS    = true,
+      declarationDir: declarationDirCJS = outDirCJS
+    }
+  } = JSON.parse(readFileSync(files.tsconfigCJSJSON, 'utf8'))
+
   // Get original contents of package.json
   const original    = readFileSync(files.packageJSON, 'utf8')
   const packageJSON = JSON.parse(original)
+
   try {
     // Compile TS -> JS
     execSync(prepareCommand, { cwd, stdio: 'inherit' })
+    Object.assign(files, { source: packageJSON.main || 'index.ts', })
+    const replaceExtension = (x, a, b) => `${basename(x, a)}${b}`
     Object.assign(files, {
-      source:   packageJSON.main || 'index.ts',
+      esmBuild: $(outDirESM, replaceExtension(files.source, '.ts', '.esm.js')),
+      cjsBuild: $(outDirCJS, replaceExtension(files.source, '.ts', '.cjs.js')),
     })
-    Object.assign(files, {
-      typedefs: $(declarationDir, `${basename(files.source, '.ts')}.d.ts`),
-      esmBuild: $(outDir,         `${basename(files.source, '.ts')}.import.js`),
-      cjsBuild: $(outDir,         `${basename(files.source, '.ts')}.require.js`),
-    })
-    // Set types field
-    if (declaration) {
-      packageJSON.types = typedefs
-    }
-    // Set main and exports fields
+    // Set main, types, and exports fields in package.json
     if (packageJSON.type === "module") {
       packageJSON.main = files.esmBuild
+      if (declarationESM) {
+        packageJSON.types = $(declarationDirESM, replaceExtension(files.source, '.ts', '.d.ts'))
+      }
       packageJSON.exports = {
         source:  files.source,
         require: files.cjsBuild,
@@ -41,12 +61,16 @@ function izdatel (cwd, prepareCommand = 'npm prepare', ...publishArgs) {
       }
     } else {
       packageJSON.main = files.cjsBuild
+      if (declarationCJS) {
+        packageJSON.types = $(declarationDirCJS, replaceExtension(files.source, '.ts', '.d.ts'))
+      }
       packageJSON.exports = {
         source:  files.source,
-        import:  files.cjsBuild,
-        default: files.esmBuild
+        import:  files.esmBuild,
+        default: files.cjsBuild
       }
     }
+    Object.assign(files, { typedefs: packageJSON.types })
     // Set files field
     const sortedDistinct = (a=[], b=[]) => [...new Set([...a, ...b])].sort()
     packageJSON.files = sortedDistinct(
