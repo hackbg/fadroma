aefbfac3fefbc3222729ed3dda0e9f512bc2d84a
Adam A <adam@hack.bg>
2022-07-19 02:20:02 +0300

    feat(portali@1.2): class Endpoint

--- portali/portali.ts
+++ portali/portali.ts
@@ -1,3 +1,4 @@
+import * as http from 'http'
 import * as net from 'net'
 import { backOff } from "exponential-backoff"
 
@@ -74,4 +75,26 @@ export function waitPort ({
 
   })
 
+}
+
+/** API endpoint client. */
+export class Endpoint {
+  url: URL
+  constructor (url: string) {
+    this.url = new URL(url)
+  }
+  get (pathname: string = '', params: Record<string, string> = {}): Promise<any> {
+    const url = Object.assign(new URL(this.url.toString()), { pathname })
+    for (const [key, value] of Object.entries(params)) {
+      url.searchParams.set(key, value)
+    }
+    return new Promise((resolve, reject)=>{
+      this._get(url.toString(), res => {
+        let data = ''
+        res.on('data', chunk => data += chunk)
+        res.on('end', () => resolve(JSON.parse(data)))
+      }).on('error', reject)
+    })
+  }
+  _get = http.get
 }
