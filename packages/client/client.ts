16e84be8d5f213b31d58557e84954bd4a94b7069
Adam A <adam@hack.bg>
2022-07-18 09:18:25 +0300

    release: bump versions

--- packages/client/client.ts
+++ packages/client/client.ts
@@ -179,6 +179,7 @@ export interface DevnetHandle {
 }
 
 export abstract class Chain implements Spectator {
+  isSecretNetwork = false
   static Mode = ChainMode
   constructor (
     readonly id: ChainId,
