e2490fd45bf328f58a647ef2660565cbde0310ee
Adam A <adam@hack.bg>
2022-07-22 01:41:55 +0300

    fix(ci): typedoc error

--- index.ts
+++ index.ts
@@ -215,7 +215,6 @@ export const DeployOps = {
   /** Create a new deployment and add it to the command context. */
   New: async function createDeployment ({
     chain,
-    agent,
     timestamp,
     deployments = Deployments.fromConfig(chain, config.project.root),
     cmdArgs     = [],
@@ -229,6 +228,7 @@ export const DeployOps = {
   /** Add the currently active deployment to the command context. */
   Append: async function activateDeployment ({
     chain,
+    //@ts-ignore
     agent,
     deployments = Deployments.fromConfig(chain, config.project.root)
   }: ConnectedContext): Promise<DeployContext> {
