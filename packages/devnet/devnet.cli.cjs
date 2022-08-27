ea0d6e8afb11ea2c41688d07c895d6e7c04dd8e4
Adam A <adam@hack.bg>
2022-08-27 19:41:57 +0300

    FEAT(connect, devnet): expose CLI

---
+++ packages/devnet/devnet.cli.cjs
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+let dotenv
+try { dotenv = require('dotenv') } catch (e) {}
+if (dotenv) dotenv.config()
+
+let ganesha
+try { ganesha = require.resolve('@hackbg/ganesha') } catch (e) {
+  console.error(e)
+  console.error('Could not find @hackbg/ganesha. CLI not available ;d')
+  process.exit(1)
+}
+
+const entrypoint = require('path').resolve(__dirname, 'devnet.ts')
+const invocation = [process.argv[0], ganesha, entrypoint, ...process.argv.slice(2)]
+require('@hackbg/ganesha').main(invocation)
