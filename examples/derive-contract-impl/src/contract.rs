610193d45f981644458f7cb355cdb430bd046eb4
aakamenov <asparuh@hack.bg>
2023-01-25 14:14:14 +0200

    Refactor the examples and add more.

--- examples/derive-contract-impl/src/contract.rs
+++ examples/derive-contract-impl/src/contract.rs
@@ -1,65 +1,59 @@
 use fadroma::{prelude::*, derive_contract::*};
 
-mod state;
-use state::State;
+fadroma::namespace!(pub StateNs, b"state");
+pub const STATE: SingleItem<u64, StateNs> = SingleItem::new();
 
 #[contract(entry)]
 pub trait Contract {
     #[init]
     fn new(initial_value: u64) -> StdResult<Response> {
-        State::save_state(
-            deps.storage,
-            &State {
-                value: initial_value,
-            },
-        )?;
+        STATE.save(deps.storage, &initial_value)?;
+
         Ok(Response::default())
     }
 
     #[execute]
     fn add(value: u64) -> StdResult<Response> {
-        let mut state = State::load_state(deps.storage)?;
+        let mut state = STATE.load_or_default(deps.storage)?;
+        state += value;
 
-        state.value += value;
-        State::save_state(deps.storage, &state)?;
+        STATE.save(deps.storage, &state)?;
 
         Ok(Response::default())
     }
 
     #[execute]
     fn sub(value: u64) -> StdResult<Response> {
-        let mut state = State::load_state(deps.storage)?;
+        let mut state = STATE.load_or_default(deps.storage)?;
+        state -= value;
 
-        state.value -= value;
-        State::save_state(deps.storage, &state)?;
+        STATE.save(deps.storage, &state)?;
 
         Ok(Response::default())
     }
 
     #[execute]
     fn mul(value: u64) -> StdResult<Response> {
-        let mut state = State::load_state(deps.storage)?;
+        let mut state = STATE.load_or_default(deps.storage)?;
+        state *= value;
 
-        state.value *= value;
-        State::save_state(deps.storage, &state)?;
+        STATE.save(deps.storage, &state)?;
 
         Ok(Response::default())
     }
 
     #[execute]
     fn div(value: u64) -> StdResult<Response> {
-        let mut state = State::load_state(deps.storage)?;
+        let mut state = STATE.load_or_default(deps.storage)?;
+        state /= value;
 
-        state.value /= value;
-        State::save_state(deps.storage, &state)?;
+        STATE.save(deps.storage, &state)?;
 
         Ok(Response::default())
     }
 
     #[query]
     fn value() -> StdResult<u64> {
-        let state = State::load_state(deps.storage)?;
-
-        Ok(state.value)
+        STATE.load_or_default(deps.storage)
     }
 }
