f5111b15f526006af0e9b4409162d951a98e0752
aakamenov <asparuh@hack.bg>
2023-01-26 13:30:01 +0200

    Remove the DeserializeFlat macro

--- examples/derive-contract-interface/src/interface.rs
+++ examples/derive-contract-interface/src/interface.rs
@@ -1,5 +1,10 @@
 use fadroma::{prelude::*, derive_contract::*};
 
+// The message enum variants for this contract will be
+// generated here. This allows you to have just the definitions
+// in a separate crate so that they can be imported in multiple
+// contracts without causing cyclical crate references.
+
 #[interface]
 pub trait Contract {
     #[init]
