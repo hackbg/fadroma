8778db20cd2cb433f585a6f537bda710a25eabf7
Adam A <adam@hack.bg>
2023-02-26 00:04:33 +0200

    fix(beta.39;connect@2.0.5;devnet@1.0.9): remove comma that was causing empty bind mount

--- packages/devnet/devnet-docker.ts
+++ packages/devnet/devnet-docker.ts
@@ -165,7 +165,6 @@ export class DockerDevnet extends Devnet implements DevnetHandle {
         HostConfig:   {
           NetworkMode: 'bridge',
           Binds: [
-            ,
             `${this.stateRoot.path}:/receipts/${this.chainId}:rw`
           ],
           PortBindings: {
