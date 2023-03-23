05803387bc9eb81e4911403f4925b690aba7a422
Adam Avramov <adam.b.avramov@gmail.com>
2023-03-23 07:08:43 +0100

    docs: lol actually getting somewhere with the homepage design (wip)

--- homepage/render.js
+++ homepage/render.js
@@ -1,12 +1,15 @@
 import { readFileSync, writeFileSync } from 'fs'
 import { compileFile } from 'pug'
 import { loadAll } from 'js-yaml'
+import markdownIt from 'markdown-it'
 
 // Homepage template
 const template = compileFile(`./homepage/index.pug`, { self: true })
 
 // Data for template
 const context = {}
+
+context.markdown = markdownIt()
 
 // Icons as inline svg paths, see https://icomoon.io/#docs/inline-svg
 context.icons = loadAll(readFileSync('./homepage/icons.yaml'))
