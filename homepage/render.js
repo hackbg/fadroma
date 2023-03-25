8f317fda208345f0afef0cf4b7ef14bb5bab41e6
Adam Avramov <adam.b.avramov@gmail.com>
2023-03-25 21:48:18 +0100

    wip: docs

--- homepage/render.js
+++ homepage/render.js
@@ -2,20 +2,16 @@ import { readFileSync, writeFileSync } from 'fs'
 import { compileFile } from 'pug'
 import { loadAll } from 'js-yaml'
 import markdownIt from 'markdown-it'
-
-// Homepage template
+// Compile the homepage template
 const template = compileFile(`./homepage/index.pug`, { self: true })
-
-// Data for template
-const context = {}
-
-context.markdown = markdownIt()
-
-// Icons as inline svg paths, see https://icomoon.io/#docs/inline-svg
-context.icons = loadAll(readFileSync('./homepage/icons.yaml'))
-
-// Feature descriptions
-context.features = loadAll(readFileSync('./homepage/features.yaml'))
-
-// Render template with data
-writeFileSync(`./homepage/index.html`, template(context))
+// Render the homepage
+writeFileSync(`./homepage/index.html`, template({
+  // When running in CI, debug info is not shown
+  CI:       process.env.CI,
+  // Markdown renderer
+  markdown: markdownIt(),
+  // Feature descriptions
+  features: loadAll(readFileSync('./homepage/features.yaml')),
+  // Icons as inline svg paths, see https://icomoon.io/#docs/inline-svg
+  icons:    loadAll(readFileSync('./homepage/icons.yaml')),
+}))
