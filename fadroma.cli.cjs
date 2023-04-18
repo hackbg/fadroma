77c08c3482537546a57ea6de07327c66a73ad7c7
Adam Avramov <adam.b.avramov@gmail.com>
2023-04-19 00:01:46 +0200

    fix(1.0.0-beta.56): lol

--- fadroma.cli.cjs
+++ fadroma.cli.cjs
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
-
-process.argv = [
-  process.argv[0],
-  require.resolve('@hackbg/cmds/cmds-ts.cli.cjs'),
-  require('path').resolve(__dirname),
-  ...process.argv.slice(2)
-]
-
+const { join, resolve } = require('path')
+const { readFileSync } = require('fs')
+const node = process.argv[0]
+const cmds = require.resolve('@hackbg/cmds/cmds-ts.cli.cjs')
+const main = resolve(
+  __dirname,
+  JSON.parse(readFileSync(resolve(__dirname, 'package.json')), 'utf8').main
+)
+process.argv = [ node, cmds, main, ...process.argv.slice(2) ]
 require('@hackbg/cmds/cmds-ts.cli.cjs')
