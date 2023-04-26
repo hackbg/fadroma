f457f06ef29b014a891a64440ffbcec1ab40f1f1
Adam Avramov <adam.b.avramov@gmail.com>
2023-04-26 11:24:38 +0200

    fix(scrt): bump upload/init gas to 2

--- platforms/scrt/ScrtChain.ts
+++ platforms/scrt/ScrtChain.ts
@@ -20,8 +20,8 @@ export default class Scrt extends Chain {
     new Fee(amount, this.defaultDenom)
   /** Set permissive fees by default. */
   static defaultFees: AgentFees = {
-    upload: this.gas(1000000),
-    init:   this.gas(1000000),
+    upload: this.gas(2000000),
+    init:   this.gas(2000000),
     exec:   this.gas(1000000),
     send:   this.gas(1000000),
   }
