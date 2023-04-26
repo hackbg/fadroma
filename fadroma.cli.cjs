850e531ad89513b56e68244c234e5fbadf4a759e
Adam Avramov <adam.b.avramov@gmail.com>
2023-04-26 18:00:38 +0200

    fix(beta.84): cli

--- fadroma.cli.cjs
+++ fadroma.cli.cjs
@@ -7,6 +7,9 @@ const node = process.argv[0]
 const cmds = require.resolve('@hackbg/cmds/cmds-ts.cli.cjs')
 const pkgj = JSON.parse(readFileSync(resolve(__dirname, 'package.json')), 'utf8')
 console.log(`@hackbg/fadroma ${pkgj.version}`)
-const main = process.env.FADROMA_OPS ?? resolve(process.cwd(), pkgj.main)
+const main = process.env.FADROMA_OPS
+  ? resolve(process.cwd(), process.env.FADROMA_OPS)
+  : resolve(__dirname, pkgj.main)
+console.log(main)
 process.argv = [ node, cmds, main, ...process.argv.slice(2) ]
 require('@hackbg/cmds/cmds-ts.cli.cjs')
