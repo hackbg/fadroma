a30aa91c10e66115f35ce2951b033a3f11668fe9
Adam Avramov <adam.b.avramov@gmail.com>
2023-05-12 09:21:58 +0200

    docs: update copyright notice and add a couple examples to fadroma.json

--- fadroma.ts
+++ fadroma.ts
@@ -1,7 +1,7 @@
 /**
 
   Fadroma
-  Copyright (C) 2022 Hack.bg
+  Copyright (C) 2023 Hack.bg
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU Affero General Public License as published by
