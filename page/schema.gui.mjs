87c8f299700a35559dcc1e4d14667524c679fb28
Adam Avramov <adam@hack.bg>
2023-07-21 00:40:52 +0300

    refactor(schema): wip, pt.1

--- page/schema.gui.mjs
+++ page/schema.gui.mjs
@@ -1,59 +1,117 @@
+// TODO: in null state, center logo and toolbar in the middle of the screen!
+
+import Schema from './schema.mjs'
+import Dexie from 'https://cdn.jsdelivr.net/npm/dexie@3.2.4/dist/dexie.mjs'
 import * as Marked from 'https://cdn.jsdelivr.net/npm/marked@5.1.1/lib/marked.esm.js'
-import { convertSchema } from './schema.mjs'
-import DOM from '../ensuite/toolbox/dome/dome.mjs'
 
-console.log({Marked})
+Marked.use({ mangle: false, headerIds: false })
 
-// https://docs.okp4.network/assets/files/okp4-cognitarium-38b87179f0c3c7c6f7b35f81306caf25.json
-// https://docs.okp4.network/assets/files/okp4-objectarium-eada0cda6e11102840a1c57adfa7132e.json
-// https://docs.okp4.network/assets/files/okp4-law-stone-f26aee4b82425895e7f93cb468b1b639.json
+export const DB = new Dexie("FadromaJSONSchemaTool")
+DB.version(1).stores({ schemas: 'url,timestamp' })
 
-class Modal {
+export default class App {
 
-  constructor (root) {
-    this.root = root
-    this.bg = root.querySelector('.schema-converter-modal-background')
-    this.bg.addEventListener('click', () => { this.toggle(false) })
-    this.cancel = document.querySelector('.schema-converter-modal-cancel')
-    this.cancel.addEventListener('click', () => { this.toggle(false) })
+  uploadModal  = new UploadModal(document.querySelector('.schema-converter-modal'))
+  uploadButton = document.querySelector('#upload-from-url')
+  clearButton  = document.querySelector('footer button[data-name="Clear"]')
+  schemaList   = document.querySelector('.schemas')
+
+  constructor () {
+    this.uploadButton.addEventListener('click', () => {
+      console.log('hey')
+      this.uploadModal.toggle(true)
+    })
+    this.clearButton.addEventListener('click', () => {
+      this.schemaList.innerText = ''
+      for (const key in globalThis.localStorage) delete localStorage[key]
+    })
+    this.loadUrl('https://docs.okp4.network/assets/files/okp4-cognitarium-38b87179f0c3c7c6f7b35f81306caf25.json')
+    this.loadUrl('https://docs.okp4.network/assets/files/okp4-objectarium-eada0cda6e11102840a1c57adfa7132e.json')
+    this.loadUrl('https://docs.okp4.network/assets/files/okp4-law-stone-f26aee4b82425895e7f93cb468b1b639.json')
   }
 
-  toggle (show = this.root.style.visibility === 'hidden') {
-    if (show) {
-      Object.assign(this.root.style, { display: '', visibility: 'visible' })
-    } else {
-      Object.assign(this.root.style, { display: 'none', visibility: 'hidden' })
-      this.root.querySelector('input').value = ''
-      this.root.querySelector('.schema-converter-modal-error').innerText = ''
-    }
+  loadUrl (url) {
+    return new UploadModal(null).upload(url) /** hack */
   }
 
 }
 
+class Toggle {
+  constructor (open, button, panel) {
+    Object.assign(this, { open, button, panel })
+    if (this.button) this.button.addEventListener('click', () => this.toggle())
+  }
+  toggle = (show = this.panel.style.visibility === 'hidden') => {
+    if (!this.panel) return
+    if (show) {
+      Object.assign(this.panel.style, { display: '', visibility: 'visible' })
+    } else {
+      Object.assign(this.panel.style, { display: 'none', visibility: 'hidden' })
+    }
+  }
+}
+
+// Base modal class that can be toggled and dims everything else
+class Modal extends Toggle {
+  constructor (root) {
+    super(false, null, root) // Closed toggle of modal root with no button
+    this.root = root
+    if (this.root) {
+      this.bg = root.querySelector('.schema-converter-modal-background')
+      this.bg.addEventListener('click', () => { this.toggle(false) })
+      this.cancel = document.querySelector('.schema-converter-modal-cancel')
+      this.cancel.addEventListener('click', () => { this.toggle(false) })
+    } else {
+      console.warn('warning: awakened the dreaded rootless modal')
+    }
+  }
+  toggle (show = this.root.style.visibility === 'hidden') {
+    super.toggle(show)
+    if (!show) { // Clean up
+      this.root.querySelector('input').value = ''
+      this.root.querySelector('.schema-converter-modal-error').innerText = ''
+    }
+  }
+}
+
+// Modal for fetching from URL
 class UploadModal extends Modal {
   constructor (root) {
     super(root)
-    this.input  = root.querySelector('.schema-converter-modal-input')
-    this.error  = root.querySelector('.schema-converter-modal-error')
-    this.button = root.querySelector('.schema-converter-modal-upload')
-    this.button.addEventListener('click', ()=>{
-      this.upload()
-    })
-    this.input.addEventListener('keydown', ({ key }) => {
-      if (key === 'Enter') this.upload()
-    })
+    if (this.root) {
+      this.input  = root.querySelector('.schema-converter-modal-input')
+      this.error  = root.querySelector('.schema-converter-modal-error')
+      this.button = root.querySelector('.schema-converter-modal-upload')
+      this.button.addEventListener('click', ()=>{
+        this.upload()
+      })
+      this.input.addEventListener('keydown', ({ key }) => {
+        if (key === 'Enter') this.upload()
+      })
+    }
   }
-  async upload () {
-    if (this.input.value.trim() === '') return
-    let url, resp, data
-    await attempt(() => url = new URL(this.input.value),
-      e => this.error.innerText = `Invalid URL`)
-    await attempt(async () => resp = await fetch(url),
-      e => this.error.innerText = `Failed to fetch: ${e.message}`)
-    await attempt(async () => data = await resp.json(),
-      e => this.error.innerText = `Failed to parse JSON: ${e.message}`)
-    const result = await attempt(() => new SchemaViewer(url, data),
-      e => this.error.innerText = `Failed to parse the schema: ${e.message}`)
+  async upload (url = this.input?.value||'') {
+    url = url.trim()
+    if (url === '') return
+
+    await attempt(
+      () => url = new URL(url),
+      e => { this.error && (this.error.innerText = `Invalid URL`) })
+
+    let resp
+    await attempt(
+      async () => resp = await fetch(url),
+      e => { this.error && (this.error.innerText = `Failed to fetch: ${e.message}`) })
+
+    let data
+    await attempt(
+      async () => data = await resp.json(),
+      e => { this.error && (this.error.innerText = `Failed to parse JSON: ${e.message}`) })
+
+    const result = await attempt(
+      () => new SchemaViewer(url, data),
+      e => { this.error && (this.error.innerText = `Failed to parse the schema: ${e.message}`) })
+
     if (result) this.toggle(false)
     return result
   }
@@ -70,73 +128,52 @@ async function attempt (cb, onfail) {
   return result
 }
 
+/** Displays a contract schema */
 class SchemaViewer {
+  definitionMap = new Map()
 
   constructor (source, schema) {
-
-    localStorage[source] = schema
-
-    const {
-      contract_name,
-      contract_version,
-      idl_version,
-      description,
-      instantiate,
-      execute,
-      query,
-      migrate,
-      sudo,
-      responses
-    } = schema
+    // Validate input by constructing the data object
+    // Run-time type checking can be implemented in `Schema`
+    schema = new Schema(source, schema)
+    // Create a schema viewer from the template in the document.
     const template = document.querySelector('.schema-template')
     this.root = template.content.cloneNode(true).firstChild
+    // Hydrate the template
     this.content = this.root.querySelector('.schema-content')
-    document.querySelector('.schemas').appendChild(this.root)
-
-    this.root.querySelector('[data-name="Name"] .value').innerText = contract_name
-    this.root.querySelector('[data-name="Version"] .value').innerText = contract_version
-    this.root.querySelector('[data-name="Source"]').innerText = source
-    this.root.querySelector('[data-name="Show"]').addEventListener('click', ()=>this.toggle(true))
-    this.root.querySelector('[data-name="Hide"]').addEventListener('click', ()=>this.toggle(false))
-    //document.querySelector('.schema-metadata [data-name="IDL version"] .value')
-      //.innerText = idl_version
-    //document.querySelector('.schema-metadata [data-name="Description"] .value')
-      //.innerText = `${description.length} chars`
-    this.definitionMap = new Map()
+    for (const [name, value] of schema.getOverview().entries()) {
+      const selector = `[data-name="${name}"] .value`
+      const element  = this.root.querySelector(selector)
+      if (element) {
+        element.innerText = value
+      } else {
+        console.warn(`missing element`, { selector, root: this.root })
+      }
+    }
+    // Add description
     this.body = Object.assign(this.root.querySelector('.schema-body'), { innerText: '' })
-    this.body.appendChild(Object.assign(document.createElement('div'), {
-      innerHTML: Marked.parse(description)
-    }))
+    const descriptionHTML = Marked.parse(schema.description)
+    this.body.appendChild(Object.assign(document.createElement('div'), { innerHTML: descriptionHTML }))
     this.body.appendChild(Object.assign(document.createElement('hr')))
-    this.populateSection(instantiate,
-      () => this.root.querySelector('[data-name="Instantiate"] .value'),
-      () => `✔️`,
-      this.populateInitMethod)
-    this.populateSection(execute,
-      () => this.root.querySelector('[data-name="Execute"] .value'),
-      (execute) => `${execute.oneOf.length} methods`,
-      this.populateMethodVariants)
-    this.populateSection(query,
-      () => this.root.querySelector('[data-name="Query"] .value'),
-      (query) => `${query.oneOf.length} methods`,
-      this.populateMethodVariants)
-    this.populateSection(migrate,
-      () => this.root.querySelector('[data-name="Migrate"] .value'),
-      (migrate) => `${migrate.oneOf.length} methods`,
-      this.populateMethodVariants)
-    this.populateSection(sudo,
-      () => this.root.querySelector('[data-name="Sudo"] .value'),
-      (sudo) => `${sudo.oneOf.length} methods`,
-      this.populateMethodVariants)
-    this.populateSection(responses,
-      () => this.root.querySelector('[data-name="Responses"] .value'),
-      (responses) => `${Object.keys(responses).length} types`)
-
-    const definitionList = document.querySelector('.schema-definitions')
+    // Add method lists
+    this.populateSection(schema.instantiate, this.populateInitMethod)
+    this.populateSection(schema.execute,     this.populateMethodVariants)
+    this.populateSection(schema.query,       this.populateMethodVariants)
+    this.populateSection(schema.migrate,     this.populateMethodVariants)
+    this.populateSection(schema.sudo,        this.populateMethodVariants)
+    this.populateSection(schema.responses)
+    const definitionList = this.root.querySelector('.schema-definitions')
     for (const name of [...this.definitionMap.keys()].sort()) {
       definitionList.appendChild(Object.assign(document.createElement('div'), {
         className: 'schema-definition', innerText: name
       }))}
+    // Append the hydrated template to the document
+    document.querySelector('.schemas').appendChild(this.root)
+    // Bind event handlers
+    this.root.querySelector('[data-name="Show"]')
+      .addEventListener('click', ()=>this.toggle(true))
+    this.root.querySelector('[data-name="Hide"]')
+      .addEventListener('click', ()=>this.toggle(false))
   }
 
   toggle (show = this.content.style.visibility === 'hidden') {
@@ -147,19 +184,10 @@ class SchemaViewer {
     }
   }
 
-  populateSection (
-    data,
-    counter = () => { throw new Error('unimplemented') },
-    count   = () => { throw new Error('unimplemented') },
-    more
-  ) {
-    if (data) {
-      counter().innerText = count(data)
-      if (more) more(data)
-    } else {
-      counter().innerText = `❌`
-    }
+  populateSection (data, render) {
+    if (data && render) render(data)
   }
+
   populateInitMethod = (variant) => {
     const { title, description, definitions, properties, oneOf } = variant
     if (definitions) for (const name of Object.keys(definitions).sort()) this.definitionMap.set(name)
@@ -168,7 +196,6 @@ class SchemaViewer {
       innerHTML: Marked.parse(description)
     }))
     for (const [key, value] of Object.entries(properties)) {
-      console.log(key, value)
       this.body.appendChild(Object.assign(document.createElement('h4'), { innerText: `Parameter: ${key}` }))
       this.body.appendChild(Object.assign(document.createElement('div'), {
         innerHTML: Marked.parse(description)
@@ -184,7 +211,6 @@ class SchemaViewer {
     }))
     for (const subvariant of oneOf) {
       const { type, title, description, properties, enum: _enum } = subvariant
-      console.log(title, type)
       this.body.appendChild(Object.assign(document.createElement('h3'), { innerText: `Variant: ${title}` }))
       this.body.appendChild(Object.assign(document.createElement('p'), {
         innerHTML: Marked.parse(description)
@@ -206,12 +232,3 @@ class SchemaViewer {
     }
   }
 }
-
-const modal = new UploadModal(document.querySelector('.schema-converter-modal'))
-
-console.log('loaded')
-
-document.querySelector('#upload-from-url').addEventListener('click', () => {
-  console.log('hey')
-  modal.toggle(true)
-})
--- page/schema.mjs
+++ page/schema.mjs
@@ -1,1 +1,55 @@
-export function convertSchema () {}
+export default class CosmWasmSchema {
+  constructor (source, {
+    contract_name, contract_version, idl_version, description,
+    instantiate, execute, query, migrate, sudo, responses
+  }) {
+    this.source = source
+    Object.assign(this, {
+      contract_name, contract_version, idl_version, description,
+      instantiate, execute, query, migrate, sudo, responses
+    })
+  }
+
+  getOverview () {
+    const props = new Map()
+    props.set('Source',
+      this.source || `❌`)
+    props.set('Name',
+      this.contract_name || `❌`)
+    props.set('Version',
+      this.contract_version || `❌`)
+    props.set('IDL version',
+      this.idl_version || `❌`)
+    props.set('Description',
+      this.description || `❌`)
+    props.set('Instantiate',         this.instantiate?.properties
+      ? `${Object.keys(this.instantiate.properties).length} parameter(s)` : `❌`)
+    props.set('Transaction methods', this.execute?.oneOf?.length
+      ? `${this.execute.oneOf.length} method(s)`   : `❌`)
+    props.set('Query methods',       this.query?.oneOf?.length
+      ? `${this.query?.oneOf?.length} method(s)`   : `❌`)
+    props.set('Migrate methods',     this.migrate?.oneOf?.length
+      ? `${this.migrate?.oneOf?.length} method(s)` : `❌`)
+    props.set('Sudo methods',        this.sudo?.oneOf?.length
+      ? `${this.sudo?.oneOf?.length} method(s)`    : `❌`)
+    props.set('Responses',           this.responses
+      ? `${Object.keys(this.responses).length}`    : `❌`)
+    return props
+  }
+
+  toMd () {
+  }
+
+  toHtml () {
+  }
+
+  toJsonCompact () {
+  }
+
+  toJsonPretty () {
+  }
+
+  toYaml () {
+  }
+
+}
