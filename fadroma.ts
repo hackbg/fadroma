9f42aa23ee53c5b05ae668cdb40a85cf31a77d8e
Adam Avramov <adam@hack.bg>
2023-08-29 14:20:02 +0300

    fix(1.5.6,connect@3.4.7): pin versions, use cw@0.2.8

--- fadroma.ts
+++ fadroma.ts
@@ -510,12 +510,12 @@ export function writeProject ({ name, templates, root, dirs, files, crates }: Pr
     type: "module",
     version: "0.1.0",
     dependencies: {
-      "@fadroma/agent": "^1.1.2",
-      "@fadroma/scrt": "^10.1.6",
+      "@fadroma/agent": "1.1.2",
+      "@fadroma/scrt": "10.1.6",
       "secretjs": "1.9.3"
     },
     devDependencies: {
-      "@hackbg/fadroma": `^1.4.9`,
+      "@hackbg/fadroma": `1.5.6`,
       "@hackbg/ganesha": "4.2.0",
       "typescript": "^5.1.6",
     },
