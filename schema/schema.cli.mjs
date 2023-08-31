7fd79a626571f57001f2d87c053ccebe9280b65b
Adam Avramov <adam@hack.bg>
2023-08-31 09:22:04 +0300

    feat(schema@1.1): add footer

--- schema/schema.cli.mjs
+++ schema/schema.cli.mjs
@@ -2,14 +2,26 @@
 import {argv, env, cwd} from 'node:process'
 import {dirname, basename, resolve, relative} from 'node:path'
 import {existsSync, realpathSync, readFileSync} from 'node:fs'
+import {fileURLToPath} from 'node:url'
+import {createHash} from 'node:crypto'
 import {SchemaToMarkdown} from './schema.mjs'
-
+const { name, version } = JSON.parse(
+  readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), 'package.json'))
+)
+const link = `[${name} ${version}](https://www.npmjs.com/package/${name})`
+process.stderr.write(`${name} ${version}\n`)
 if (argv.length > 2) {
+  const input = argv[2]
   process.stderr.write(`Converting schema: ${argv[2]}\n`)
-  const source = readFileSync(argv[2], 'utf8')
+  const source = readFileSync(input, 'utf8')
+  const hash = createHash('sha256').update(source, 'utf8').digest().toString('hex').slice(0, 16)
   const parsed = JSON.parse(source)
   const schema = new SchemaToMarkdown(argv[2], parsed)
   process.stdout.write(schema.toMd())
+  process.stdout.write(
+    `\n\n---\n\n*Rendered by [Fadroma](https://fadroma.tech) (${link}) ` +
+    `from \`${basename(input)}\` (\`${hash}\`)*`
+  )
 } else {
   // Shorten paths for usage
   const paths = (env.PATH||'').split(':')
