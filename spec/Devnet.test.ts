6c80ff95fc56c098f2ad24e77ba236529392ae7f
Adam Avramov <adam@hack.bg>
2023-10-12 20:40:37 +0300

    wip: test(devnet): can select tests
    - when running pnpm test:devnet you are now prompted to select a suite
    - when running pnpm test:devnet all, tests are now run in parallel
    - possible to run pnpm test:devnet okp4 - not passing yet

--- spec/Devnet.test.ts
+++ spec/Devnet.test.ts
@@ -1,134 +1,141 @@
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import { Devnet } from '@hackbg/fadroma'
+import type { DevnetPlatform } from '@hackbg/fadroma'
+
 import * as assert from 'node:assert'
 import { getuid, getgid } from 'node:process'
+
 import $, { TextFile } from '@hackbg/file'
 import { Image, Container } from '@hackbg/dock'
 
-import { resolve, dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
-const initScript = resolve(dirname(fileURLToPath(import.meta.url)), 'devnet', 'devnet.init.mjs')
+const tests: Record<string, Function> = {
+  'all':        testAll,
+  'docs':       testDevnetDocs,
+  'scrt':       ()=>testDevnetPlatform('scrt_1.9'),
+  'okp4':       ()=>testDevnetPlatform('okp4_5.0'),
+  'chain-id':   testDevnetChainId,
+  'state-file': testDevnetStateFile,
+  'url':        testDevnetUrl,
+  'container':  testDevnetContainer,
+}
 
-let devnet: any
+if (resolve(process.argv[2]) === resolve(fileURLToPath(import.meta.url))) {
+  await pickDevnetTest()
+} else {
+  await testAll()
+}
 
-;(async () => {
+export async function pickDevnetTest () {
+  const test = tests[process.argv[3]]
+  if (test) {
+    await test()
+  } else {
+    console.log('\nSpecify suite to run:')
+    for (const test of Object.keys(tests)) {
+      console.log(`  ${test}`)
+    }
+    console.log()
+    process.exit(1)
+  }
+}
 
-  await testDevnetChainId()
+export async function testAll () {
+  const runs = Object.values(tests).filter(fn=>fn!==testAll).map(test=>test())
+  await Promise.all(runs)
+}
 
-  await testDevnetStateFile()
-
-  await testDevnetUrl()
-
-  await testDevnetContainer()
-
-  await testDevnetHighLevel()
-
+export async function testDevnetDocs () {
   //@ts-ignore
   await import('./Devnet.spec.ts.md')
+}
 
-})()
-
-async function testDevnetChainId () {
-
+export async function testDevnetChainId () {
+  let devnet: Devnet
   assert.throws(
     () => { devnet = new Devnet({ chainId: false as any }) },
     "construct must fail if passed falsy chainId"
   )
-
   assert.ok(
     devnet = new Devnet(),
     "construct must work with no options"
   )
-
   assert.ok(
     typeof devnet.chainId === 'string',
     "chain id must be auto populated when not passed"
   )
-
   // TODO: can't delete
-
   assert.ok(
     await devnet.save(),
     "can save"
   )
-
   assert.ok(
     devnet = new Devnet({ chainId: devnet.chainId }),
     "can construct when passing chainId"
   )
-
   // TODO: devnet with same chainid points to same resource
-
   assert.ok(
     await devnet.delete(),
     "can delete"
   )
-
   assert.ok(
     devnet = new Devnet({ chainId: devnet.chainId }),
     "after deletion, can construct new devnet with same chainId"
   )
-
   assert.ok(
     await devnet.save(),
     "can save"
   )
-
 }
 
-async function testDevnetStateFile () {
-
+export async function testDevnetStateFile () {
+  let devnet = new Devnet()
   $(devnet.stateFile).as(TextFile).save("invalidjson")
   assert.throws(
     ()=>{ devnet = new Devnet({ chainId: devnet.chainId }) },
     "can't construct if state is invalid json"
   )
-
   $(devnet.stateFile).as(TextFile).save("null")
   assert.ok(
     devnet = new Devnet({ chainId: devnet.chainId }),
     "can construct if state is valid json but empty"
   )
-
   assert.ok(
     await devnet.delete(),
     "can delete if state is valid json but empty"
   )
-
 }
 
-async function testDevnetUrl () {
-
+export async function testDevnetUrl () {
+  let devnet: Devnet
   assert.ok(
-    devnet = new Devnet({ initScript }),
-    "can construct with no options"
+    devnet = new Devnet(),
+    "can construct"
   )
-
   assert.equal(
     devnet.url.toString(), `http://${devnet.host}:${devnet.port}/`,
     "devnet url generated from host and port properties"
   )
-
   assert.ok(
     (await devnet.image) instanceof Image,
     "devnet has @hackbg/dock image"
   )
-
 }
 
-async function testDevnetContainer () {
-
+export async function testDevnetContainer () {
+  let devnet: any
   assert.ok(
-    devnet = new Devnet({ initScript }),
-    "can construct with explicitly enabled init script"
+    devnet = new Devnet(),
+    "can construct"
   )
-
   assert.equal(
     devnet.initScriptMount, '/devnet.init.mjs',
     "devnet init script mounted at default location"
   )
-
   assert.deepEqual(
     devnet.spawnEnv, {
+      DAEMON:    'secretd',
       CHAIN_ID:  devnet.chainId,
       ACCOUNTS:  devnet.accounts.join(' '),
       STATE_UID: String(getuid!()),
@@ -137,89 +144,72 @@ async function testDevnetContainer () {
     },
     "devnet spawn environment"
   )
-
   assert.deepEqual(
     devnet.spawnOptions.env, devnet.spawnEnv,
     "devnet spawn environment is passed to container options"
   )
-
   const spawnPort = `${String(devnet.port)}/tcp`
   assert.deepEqual(
     devnet.spawnOptions.exposed, [ spawnPort ],
     "devnet port is exposed"
   )
-
   assert.deepEqual(
     devnet.spawnOptions.extra.HostConfig.PortBindings, {
       [spawnPort]: [ { HostPort: String(devnet.port) } ]
     },
     "devnet port binding is present"
   )
-
   assert.equal(
     await devnet.container, undefined,
     "devnet starts with no container"
   )
-
   assert.ok(
     await devnet.create(),
     "devnet creates container"
   )
-
   assert.ok(
     (await devnet.container) instanceof Container,
     "devnet container property is populated after container is created"
   )
-
   assert.ok(
     await devnet.assertPresence() || true,
     "devnet assert presence is ok after container is created"
   )
-
   assert.ok(
     await devnet.create(),
     "devnet creation is idempotent"
   )
-
   ;(await devnet.container).remove()
-
   assert.rejects(
     devnet.assertPresence(),
     "devnet assert presence rejects if container is removed"
   )
-
 }
 
-async function testDevnetHighLevel () {
-
+export async function testDevnetPlatform (platform: DevnetPlatform) {
+  let devnet: any
   assert.ok(
-    devnet = new Devnet(),
+    devnet = new Devnet({ platform }),
     "construct devnet"
   )
-
   assert.ok(
     await devnet.start(),
     "starting the devnet works"
   )
-
   assert.ok(
     await devnet.assertPresence() || true,
     "starting the devnet automatically created the container"
   )
-
   assert.ok(
     await devnet.pause(),
     "pausing the devnet works"
   )
-
   assert.ok(
     await devnet.export(),
     "exporting the devnet works"
   )
-
   assert.ok(
     await devnet.forceDelete() || true,
     "force deleting the devnet works"
   )
-
 }
