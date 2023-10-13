7474db8c76621625f11a7390b6003dc4c0a35f43
Adam Avramov <adam@hack.bg>
2023-10-13 18:07:14 +0300

    refactor(okp4): move to connect/cw/okp4

--- connect/cw/okp4/okp4.ts
+++ connect/cw/okp4/okp4.ts
@@ -1,14 +1,22 @@
-import { Console, Error, Config, Chain, Agent, Bundle } from './cw-base'
+import { Console, Error, Config, Chain, Agent, Bundle } from '../cw-base'
+import { Objectarium, objectariumCodeIds } from './okp4-objectarium'
+import { Cognitarium, cognitariumCodeIds } from './okp4-cognitarium'
+import { LawStone, lawStoneCodeIds } from './okp4-law-stone'
+
 import type { Environment } from '@hackbg/conf'
-import type { AgentClass, ClientClass, Uint128, Address, ChainId, AgentFees } from '@fadroma/agent'
+import type {
+  AgentClass, ClientClass, Uint128, Address, ChainId, AgentFees, CodeId
+} from '@fadroma/agent'
 import { Client, Fee, bindChainSupport } from '@fadroma/agent'
 import type { CosmWasmClient } from '@hackbg/cosmjs-esm'
 
+/** Configuration for OKP4 */
 class OKP4Config extends Config {
-  static defaultTestnetChainId: string =
-    'okp4-nemeton-1'
-  static defaultTestnetUrl: string =
-    'https://okp4-testnet-rpc.polkachu.com/'
+
+  static defaultTestnetChainId: string = 'okp4-nemeton-1'
+
+  static defaultTestnetUrl: string = 'https://okp4-testnet-rpc.polkachu.com/'
+                                    //'https://okp4-testnet-api.polkachu.com/'
 
   constructor (
     options: Partial<OKP4Config> = {},
@@ -18,31 +26,24 @@ class OKP4Config extends Config {
     this.override(options)
   }
 
-    //'https://okp4-testnet-api.polkachu.com/'
   testnetChainId: string = this.getString(
     'FADROMA_OKP4_TESTNET_CHAIN_ID',
     () => OKP4Config.defaultTestnetChainId)
+
   testnetUrl: string = this.getString(
     'FADROMA_OKP4_TESTNET_URL',
     () => OKP4Config.defaultTestnetUrl)
 }
 
-/** Code IDs for versions of Cognitarium contract. */
-export const cognitariumCodeIds = [7]
-
-/** Code IDs for versions of Objectarium contract. */
-export const objectariumCodeIds = [4]
-
-/** Code IDs for versions of Law Stone contract. */
-export const lawStoneCodeIds = [5]
-
 /** OKP4 chain. */
 class OKP4Chain extends Chain {
+
   /** Default denomination of gas token. */
   static defaultDenom = 'uknow'
+
   /** @returns Fee in uscrt */
-  static gas = (amount: Uint128|number) =>
-    new Fee(amount, this.defaultDenom)
+  static gas = (amount: Uint128|number) => new Fee(amount, this.defaultDenom)
+
   /** Set permissive fees by default. */
   static defaultFees: AgentFees = {
     upload: this.gas(1000000),
@@ -50,10 +51,13 @@ class OKP4Chain extends Chain {
     exec:   this.gas(1000000),
     send:   this.gas(1000000),
   }
+
   /** Default Agent class to use. */
   declare Agent: AgentClass<OKP4Agent>
+
   /** Logging handle. */
   log = new Console('OKP4Chain')
+
   /** Default denomination of gas token. */
   defaultDenom = OKP4Chain.defaultDenom
 
@@ -82,21 +86,24 @@ class OKP4Chain extends Chain {
     * keyed by address. */
   async cognitaria ({ map = true } = {}) {
     const { api } = await this.ready
-    return await getContractsById(this.id, api, Cognitarium, cognitariumCodeIds, map)
+    const ids = Object.values(cognitariumCodeIds)
+    return await getContractsById(this.id, api, Cognitarium, ids, map)
   }
 
   /** Get clients for all Objectarium instances,
     * keyed by address. */
   async objectaria ({ map = true } = {}) {
     const { api } = await this.ready
-    return await getContractsById(this.id, api, Objectarium, objectariumCodeIds, map)
+    const ids = Object.values(objectariumCodeIds)
+    return await getContractsById(this.id, api, Objectarium, ids, map)
   }
 
   /** Get clients for all Law Stone instances,
     * keyed by address. */
   async lawStones ({ map = true } = {}) {
     const { api } = await this.ready
-    return await getContractsById(this.id, api, LawStone, lawStoneCodeIds, map)
+    const ids = Object.values(lawStoneCodeIds)
+    return await getContractsById(this.id, api, LawStone, ids, map)
   }
 }
 
@@ -104,21 +111,24 @@ async function getContractsById <C extends Client> (
   chainId: ChainId,
   api: CosmWasmClient,
   $C: ClientClass<C>,
-  ids: number[],
+  ids: CodeId[],
   map = true
 ): Promise<
   typeof map extends true ? Map<Address, C> : Record<Address, C>
 > {
   const contracts = map ? new Map() : {}
-  for (const codeId of ids) {
-    const { checksum } = await api.getCodeDetails(codeId)
+
+  for (const id of ids) {
+
+    const codeId = Number(id)
+    if (isNaN(codeId)) throw new Error('non-number code ID encountered')
+
+    const { checksum: codeHash } = await api.getCodeDetails(codeId)
+
     const addresses = await api.getContracts(codeId)
+
     for (const address of addresses) {
-      const contract = new $C({
-        address,
-        codeHash: checksum,
-        codeId: String(codeId),
-      } as Partial<C>)
+      const contract = new $C({ address, codeHash, codeId: String(codeId), } as Partial<C>)
       contract.meta.chainId = chainId
       if (map) {
         (contracts as Map<Address, C>).set(address, contract)
@@ -126,7 +136,9 @@ async function getContractsById <C extends Client> (
         (contracts as Record<Address, C>)[address] = contract
       }
     }
+
   }
+
   return contracts
 }
 
@@ -199,75 +211,3 @@ export const testnet = OKP4Chain.testnet
 
 /** Connect to local OKP4 devnet. */
 export const devnet = OKP4Chain.devnet
-
-/** OKP4 triple store. */
-export class Cognitarium extends Client {
-  /** Create an init message for a cognitarium. */
-  static init = (limits?: CognitariumLimits) =>
-    ({ limits })
-  /** Add data to this cognitarium. */
-  insert = (format: CognitariumFormat, data: string) =>
-    this.execute({ insert_data: { format, data } })
-  /** Query data in this cognitarium. */
-  select = (
-    limit:    number,
-    prefixes: CognitariumPrefix[],
-    select:   CognitariumSelect[],
-    where:    CognitariumWhere[]
-  ) =>
-    this.query({ select: { query: { limit, prefixes, select, where } } })
-}
-
-export type CognitariumLimits = {
-  max_byte_size:                Uint128
-  max_insert_data_byte_size:    Uint128
-  max_insert_data_triple_count: Uint128
-  max_query_limit:              number
-  max_query_variable_count:     number
-  max_triple_byte_size:         Uint128
-  max_triple_count:             Uint128
-}
-
-export type CognitariumFormat = 'turtle'|'rdf_xml'|'n_triples'|'n_quads'
-
-export type CognitariumPrefix = { prefix: string, namespace: string }
-
-export type CognitariumSelect = { variable: string }
-
-export type CognitariumWhere = {
-  simple: {
-    triple_pattern: {
-      subject: { variable: string }
-      predicate: { node: { named_node: string } }
-      object: { variable: string }
-    }
-  }
-}
-
-/** OKP4 object store. */
-export class Objectarium extends Client {
-  /** Create an init message for an objectarium. */
-  static init = (bucket: string) =>
-    ({ bucket })
-  store = (pin: boolean, data: string) =>
-    this.execute({ store_object: { data, pin } })
-  pin = (id: string) =>
-    this.execute({ pin_object: { id } })
-  unpin = (id: string) =>
-    this.execute({ unpin_object: { id } })
-  forget = (id: string) =>
-    this.execute({ forget_object: { id } })
-}
-
-/** OKP4 rule engine. */
-export class LawStone extends Client {
-  /** Create an init message for a law stone. */
-  static init = (storage_address: Address, program: string) =>
-    ({ storage_address, program })
-  /** Make a query against this law stone's program. */
-  ask = (query: string) =>
-    this.query({ ask: { query } })
-  /** Permanently prevent this law stone from answering further queries. */
-  break = () =>
-    this.execute("break_stone")
-}
