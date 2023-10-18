eca83a261c98ddbe3ce1d035b8d7bdc7eb7aa4f1
Adam Avramov <adam@hack.bg>
2023-10-18 15:12:58 +0300

    wip: docs: move TS stuff to test suite

--- fadroma.test.ts
+++ fadroma.test.ts
@@ -76,9 +76,112 @@ export async function testProject () {
   await project.exportDeployment('state')
 }
 
-export async function testDeploy () {
-import { Deployment, Template, Contract, Client } from '@fadroma/agent'
-let deployment: Deployment
-let template:   Template
-let contract:   Contract
+export async function testDeployment () {
+assert.deepEqual(
+  Object.keys(deployment.snapshot.contracts),
+  ['kv1', 'kv2']
+)
+// in your project's api.ts:
+
+import { Deployment } from '@fadroma/agent'
+
+// you would load snapshots as JSON, e.g.:
+// const testnet = await (await fetch('./testnet_v4.json')).json()
+export const mainnet = deployment.snapshot
+export const testnet = deployment.snapshot
+
+// and create instances of your deployment with preloaded
+// "address books" of contracts. for example here we restore
+// a different snapshot depending on whether we're passed a
+// mainnet or testnet connection.
+class DeploymentC extends Deployment {
+  kv1 = this.contract({ crate: 'examples/kv', name: 'kv1', initMsg: {} })
+  kv2 = this.contract({ crate: 'examples/kv', name: 'kv2', initMsg: {} })
+
+  static connect = (agent: Agent) => {
+    if (agent?.chain?.isMainnet) return new this({ ...mainnet, agent })
+    if (agent?.chain?.isTestnet) return new this({ ...testnet, agent })
+    return new this({ agent })
+  }
+}
+const mainnetAgent = { chain: { isMainnet: true } } // mock
+const testnetAgent = { chain: { isTestnet: true } } // mock
+
+const onMainnet = DeploymentC.connect(mainnetAgent)
+
+const onTestnet = DeploymentC.connect(testnetAgent)
+
+assert(onMainnet.isMainnet)
+assert(onTestnet.isTestnet)
+assert.deepEqual(Object.keys(onMainnet.contracts), ['kv1', 'kv2'])
+assert.deepEqual(Object.keys(onTestnet.contracts), ['kv1', 'kv2'])
+
+const kv1 = DeploymentC.connect(mainnetAgent).kv1.expect()
+assert(kv1 instanceof Client)
+
+const kv2 = DeploymentC.connect(testnetAgent).kv2.expect()
+assert(kv2 instanceof Client)
+// in your project's api.ts:
+
+import { Deployment } from '@fadroma/agent'
+
+class DeploymentD extends DeploymentC {
+  kv3 = this.contract({ crate: 'examples/kv', name: 'kv3', initMsg: {} })
+
+  // simplest client-side migration is to just instantiate
+  // a new deployment with the data from the old deployment.
+  static upgrade = (previous: DeploymentC) =>
+    new this({ ...previous })
+}
+
+// simplest chain-side migration is to just call default deploy,
+// which should reuse kv1 and kv2 and only deploy kv3.
+deployment = await DeploymentD.upgrade(deployment).deploy()
+class Deployment4 extends Deployment {
+
+  t = this.template({ crate: 'examples/kv' })
+
+  a = this.t.instance({ name: 'a', initMsg: {} })
+
+  b = this.t.instances([
+    {name:'b1',initMsg:{}},
+    {name:'b2',initMsg:{}},
+    {name:'b3',initMsg:{}}
+  ])
+
+  c = this.t.instances({
+    c1:{name:'c1',initMsg:{}},
+    c2:{name:'c2',initMsg:{}},
+    c3:{name:'c3',initMsg:{}}
+  })
+
+}
+deployment = await getDeployment(Deployment4).deploy()
+assert(deployment.t instanceof Template)
+
+assert([
+  deployment.a,
+  ...Object.values(deployment.b)
+  ...Object.values(deployment.c)
+].every(
+  c=>(c instanceof Contract) && (c.expect() instanceof Client)
+))
+}
+
+export async function testBuild () {
+import { Builder } from '@fadroma/agent'
+assert(deployment.t.builder instanceof Builder)
+assert.equal(deployment.t.builder, deployment.builder)
+await deployment.t.built
+// -or-
+await deployment.t.build()
+}
+
+export async function testUpload () {
+import { Uploader } from '@fadroma/agent'
+assert(deployment.t.uploader instanceof Uploader)
+assert.equal(deployment.t.uploader, deployment.uploader)
+await deployment.t.uploaded
+// -or-
+await deployment.t.upload()
 }
