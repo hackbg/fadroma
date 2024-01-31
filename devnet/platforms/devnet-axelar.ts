8b16b9da2d1634ce54fd2db56645601aaac9a9ce
Adam A <adam@hack.bg>
2024-01-31 18:41:37 +0200

    fix(devnet): axelarnetwork->axelarnet

--- devnet/platforms/devnet-axelar.ts
+++ devnet/platforms/devnet-axelar.ts
@@ -10,7 +10,7 @@ export type Version = `0.34.3`
 export const versions: Record<Version, ReturnType<typeof version>> = {
   '0.34.3': version(
     '0.34.3',
-    'axelarnetwork/axelar-core:v0.34.3',
+    'axelarnet/axelar-core:v0.34.3',
     '6e99f8913054bbd81b8fe248c8c6ade736bc751f822ae6f9556cc0b8fe3a998d'
   ),
 }
