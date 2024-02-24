ca73de76b6f3becdee54666543f370db9956ed55
Adam A <adam@hack.bg>
2024-02-24 03:16:08 +0200

    wip: feat(cw): querying namada proposals

---
+++ packages/cw/namada/namada-address.ts
@@ -0,0 +1,4 @@
+export default {
+  Governance: "tnam1q5qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqrw33g6"
+  //Governance: "/shell/value/#tnam1q5qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqrw33g6/counter"
+}
--- packages/cw/namada/namada.ts
+++ packages/cw/namada/namada.ts
@@ -5,9 +5,12 @@ import { CWConnection, CWBatch } from '../cw-connection'
 import { CWMnemonicIdentity } from '../cw-identity'
 import { brailleDump } from '@hackbg/dump'
 import * as Borsh from 'borsh'
+import Borshes from './namada-borsh'
+import Addresses from './namada-address'
 
 /** Namada CLI commands. */
 class NamadaCLI extends CLI {
+
   validators = this.command({
     name: 'validators',
     info: 'query validators for a RPC endpoint',
@@ -29,6 +32,7 @@ class NamadaCLI extends CLI {
     }
     this.log.br().info('Total validators:', Core.bold(String(validators.length)))
   })
+
   validator = this.command({
     name: 'validator',
     info: 'query info about a validator',
@@ -39,7 +43,8 @@ class NamadaCLI extends CLI {
       process.exit(1)
     }
     const connection = new NamadaConnection({ url })
-    const { metadata } = await connection.getValidatorMetadata(address)
+    const { metadata, state } = await connection.getValidatorMetadata(address)
+    console.log({state})
     this.log.br()
       .log('Address:     ', Core.bold(address))
       .log('Email:       ', Core.bold(metadata.email))
@@ -57,32 +62,106 @@ class NamadaCLI extends CLI {
     }
     process.exit(0)
   })
+
+  proposalCount = this.command({
+    name: 'proposal-count',
+    info: 'get number of last proposal',
+    args: 'RPC_URL'
+  }, async (url: string) => {
+    if (!url) {
+      this.log.error(Core.bold('Pass a RPC URL to query proposal counter.'))
+      process.exit(1)
+    }
+    const connection = new NamadaConnection({ url })
+    const counter = await connection.getProposalCount()
+    this.log
+      .log('Proposal count:', Core.bold(String(counter)))
+      .log('Last proposal: ', Core.bold(String(counter-1n)))
+      .info('Use the', Core.bold('proposal'), 'command to query proposal details.')
+    process.exit(0)
+  })
+
+  proposal = this.command({
+    name: 'proposal',
+    info: 'get info about a proposal by number',
+    args: 'RPC_URL NUMBER'
+  }, async (url: string, number: string) => {
+    if (!url || !number || isNaN(Number(number))) {
+      this.log.error(Core.bold('Pass a RPC URL and proposal number to query proposal info.'))
+      process.exit(1)
+    }
+    const connection = new NamadaConnection({ url })
+    const proposal = await connection.getProposalInfo(Number(number))
+    this.log
+      .log('Proposal:', Core.bold(number))
+      .log(proposal)
+    process.exit(0)
+  })
+
 }
 
 type ValidatorMetaData = {
-  email: string,
-  description: string|null,
-  website: string|null,
-  discord_handle: string|null,
-  avatar: string|null
+  email:          string
+  description:    string|null
+  website:        string|null
+  discord_handle: string|null
+  avatar:         string|null
+}
+
+type Proposal = {
+  id:                 string,
+  proposal_type:      "pgf_steward" | "pgf_payment" | "default";
+  author:             string,
+  start_epoch:        bigint,
+  end_epoch:          bigint,
+  grace_epoch:        bigint,
+  content_json:       string,
+  status:             "ongoing" | "finished" | "upcoming"
+  result:             "passed" | "rejected"
+  total_voting_power: string,
+  total_yay_power:    string,
+  total_nay_power:    string,
+}
+
+async function abciQuery (client, path, params = new Uint8Array) {
+  const { value } = await client.queryAbci(path, params)
+  return value
 }
 
 class NamadaConnection extends CWConnection {
 
   async getValidatorMetadata (address: Address): Promise<{
     metadata: ValidatorMetaData
+    state: any
   }> {
     const client = await this.qClient
-    const [metadata, /*commission*/] = await Promise.all([
+    const [
+      metadata,
+      /*commission,*/
+      state,
+    ] = await Promise.all([
       `/vp/pos/validator/metadata/${address}`,
       //`/vp/pos/validator/commission/${address}`, // TODO
-    ].map(
-      async path => (await client.queryAbci(path, new Uint8Array())).value
-    ))
+      `/vp/pos/validator/state/${address}`,
+    ].map(path => abciQuery(client, path)))
     return {
       metadata: Borsh.deserialize(Borshes.ValidatorMetaData, metadata) as ValidatorMetaData,
+      state: Borsh.deserialize(Borshes.ValidatorState, state) as any
       //commission: Borsh.deserialize(Borshes.CommissionPair, commission),
     }
+  }
+
+  async getProposalCount () {
+    const client = await this.qClient
+    const counter = await abciQuery(client, `/shell/value/#${Addresses.Governance}/counter`)
+    return Borsh.deserialize('u64', counter) as bigint
+  }
+
+  async getProposalInfo (number: Number) {
+    const client = await this.qClient
+    const info = await abciQuery(client, `/vp/governance/proposal/${number}`)
+    console.log({info})
+    return Borsh.deserialize(Borshes.Proposal, info)
   }
 
 }
@@ -120,27 +199,4 @@ export const testnet = (options: Partial<NamadaConnection> = {}): NamadaConnecti
   return new NamadaConnection({
     chainId: chainIds.testnet, url: Core.pickRandom(testnets), ...options||{}
   })
-}
-
-/** Borsh schema for values returned by ABCI. */
-export const Borshes = {
-  ValidatorMetaData: {
-    option: {
-      struct: {
-        email: 'string',
-        description: { option: 'string' },
-        website: { option: 'string' },
-        discord_handle: { option: 'string' },
-        avatar: { option: 'string' }
-      }
-    }
-  },
-  //CommissionPair: {
-    //option: {
-      //struct: {
-        //commission_rate: 'string',
-        //max_commission_change_per_epoch: 'string',
-      //}
-    //}
-  //}
 }
