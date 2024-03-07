9f38f5a6ac75cee9f62860074e6b541ab79daf99
Adam A <adam@hack.bg>
2024-03-07 09:13:00 +0200

    wip: feat(cw): decoding namada transactions, pt.4

--- packages/cw/namada/namada-tx.ts
+++ packages/cw/namada/namada-tx.ts
@@ -82,101 +82,149 @@ const bundleSchema = Schema.Struct({
   authorization:     Schema.Unit
 })
 
+const extendedPointSchema = Schema.Struct({
+  u:  Schema.Array(Schema.u64, 4),
+  v:  Schema.Array(Schema.u64, 4),
+  z:  Schema.Array(Schema.u64, 4),
+  t1: Schema.Array(Schema.u64, 4),
+  t2: Schema.Array(Schema.u64, 4),
+})
+
+const noteSchema = Schema.Struct({
+  asset_type:   Schema.Struct({
+    identifier: Schema.Array(Schema.u8, 32),
+  }),
+  value:        Schema.u64,
+  g_d:          extendedPointSchema,
+  pk_d:         extendedPointSchema,
+  rseed:        schemaEnum([
+    ['BeforeZip212', Schema.Array(Schema.u64, 4)],
+    ['AfterZip212',  Schema.Array(Schema.u8, 32)],
+  ]),
+})
+
+const merklePathSchema = Schema.Struct({
+  auth_path: Schema.Vec([Node, bool]),
+  position:  Schema.u64
+})
+
 const txSchema = Schema.Struct({
-  header:                    headerSchema,
-  sections:                  Schema.Vec(schemaEnum([
-    ['Data',                 Schema.Struct({
-      salt:                  Schema.Array(Schema.u8, 8),
-      data:                  Schema.Vec(Schema.u8),
+  header:                       headerSchema,
+  sections:                     Schema.Vec(schemaEnum([
+    ['Data',                    Schema.Struct({
+      salt:                     Schema.Array(Schema.u8, 8),
+      data:                     Schema.Vec(Schema.u8),
     })],
-    ['ExtraData',            codeSchema],
-    ['Code',                 codeSchema],
-    ['Signature',            Schema.Struct({
-      targets:               Schema.Vec(hashSchema),
-      signer:                schemaEnum([
-        ['Address',          addressSchema],
-        ['PubKeys',          Schema.Vec(publicKeySchema)],
+    ['ExtraData',               codeSchema],
+    ['Code',                    codeSchema],
+    ['Signature',               Schema.Struct({
+      targets:                  Schema.Vec(hashSchema),
+      signer:                   schemaEnum([
+        ['Address',             addressSchema],
+        ['PubKeys',             Schema.Vec(publicKeySchema)],
       ]),
-      signatures:            Schema.HashMap(Schema.u8, schemaEnum([
-        ['Ed25519',          Schema.Array(Schema.u8, 64)],
-        ['Secp256k1',        Schema.Array(Schema.u8, 65)]
+      signatures:               Schema.HashMap(Schema.u8, schemaEnum([
+        ['Ed25519',             Schema.Array(Schema.u8, 64)],
+        ['Secp256k1',           Schema.Array(Schema.u8, 65)]
       ]))
     })],
-    ['Ciphertext',           Schema.Struct({
-      opaque:                Schema.Vec(Schema.u8)
+    ['Ciphertext',              Schema.Struct({
+      opaque:                   Schema.Vec(Schema.u8)
     })],
-    ['MaspTx',               Schema.Struct({
-      txid:                  Schema.Array(Schema.u8, 32),
-      data:                  Schema.Struct({
-        version:             schemaEnum([
-          ['MASPv5',         Schema.Unit]
+    ['MaspTx',                  Schema.Struct({
+      txid:                     Schema.Array(Schema.u8, 32),
+      data:                     Schema.Struct({
+        version:                schemaEnum([
+          ['MASPv5',            Schema.Unit]
         ]),
-        consensus_branch_id: schemaEnum([
-          ['MASP',           Schema.Unit]
+        consensus_branch_id:    schemaEnum([
+          ['MASP',              Schema.Unit]
         ]),
-        lock_time:           Schema.u32,
-        expiry_height:       Schema.u32,
-        transparent_bundle:  Schema.Option(bundleSchema),
-        sapling_bundle:      Schema.Option(bundleSchema),
+        lock_time:              Schema.u32,
+        expiry_height:          Schema.u32,
+        transparent_bundle:     Schema.Option(bundleSchema),
+        sapling_bundle:         Schema.Option(bundleSchema),
       }),
     })],
-    ['MaspBuilder',          Schema.Struct({
-      hash:                  hashSchema,
-      asset_types:           Schema.HashSet(Schema.Struct({
-        token:               addressSchema,
-        denomination:        Schema.u8,
-        position:            schemaEnum([
-          ['Zero',           Schema.Unit],
-          ['One',            Schema.Unit],
-          ['Two',            Schema.Unit],
-          ['Three',          Schema.Unit],
+    ['MaspBuilder',             Schema.Struct({
+      hash:                     hashSchema,
+      asset_types:              Schema.HashSet(Schema.Struct({
+        token:                  addressSchema,
+        denomination:           Schema.u8,
+        position:               schemaEnum([
+          ['Zero',              Schema.Unit],
+          ['One',               Schema.Unit],
+          ['Two',               Schema.Unit],
+          ['Three',             Schema.Unit],
         ]),
-        epoch:               Schema.Option(Schema.u64)
+        epoch:                  Schema.Option(Schema.u64)
       })),
-      metadata:              Schema.Struct({
-        spend_indices:       Schema.Vec(Schema.u32),
-        convert_indices:     Schema.Vec(Schema.u32),
-        output_indices:      Schema.Vec(Schema.u32),
+      metadata:                 Schema.Struct({
+        spend_indices:          Schema.Vec(Schema.u32),
+        convert_indices:        Schema.Vec(Schema.u32),
+        output_indices:         Schema.Vec(Schema.u32),
       }),
-      builder:               Schema.Struct({
-        params:              Schema.Unit,
-        rng:                 Schema.Unit,
-        target_height:       Schema.u32,
-        expiry_height:       Schema.u32,
-        transparent_builder: Schema.Struct({
-          inputs:            Schema.Vec(Schema.Struct({
-            coin:            txOutSchema
+      builder:                  Schema.Struct({
+        params:                 Schema.Unit,
+        rng:                    Schema.Unit,
+        target_height:          Schema.u32,
+        expiry_height:          Schema.u32,
+        transparent_builder:    Schema.Struct({
+          inputs:               Schema.Vec(Schema.Struct({
+            coin:               txOutSchema
           })),
-          vout:              Schema.Vec(txOutSchema)
+          vout:                 Schema.Vec(txOutSchema)
         }),
-        sapling_builder:     Schema.Struct({
-          params:            Schema.Unit,
-          spend_anchor:      Schema.Option(Scalar),
-          target_height:     Schema.u32,
-          value_balance:     Schema.i128,
-          convert_anchor:    Schema.Option(Scalar),
-          spends:            Schema.Vec(Schema.Struct({
-            extsk:           Key,
-            diversifier:     Diversifier,
-            node:            Note,
-            alpha:           Fr,
-            merkle_path:     MerklePath
+        sapling_builder:        Schema.Struct({
+          params:               Schema.Unit,
+          spend_anchor:         Schema.Option(Schema.Array(Schema.u64, 4)),
+          target_height:        Schema.u32,
+          value_balance:        Schema.i128,
+          convert_anchor:       Schema.Option(Schema.Array(Schema.u64, 4)),
+          spends:               Schema.Vec(Schema.Struct({
+            extsk:              Schema.Struct({
+              depth:            Schema.u8,
+              parent_fvk_tag:   Schema.Array(Schema.u8, 4),
+              child_index:      schemaEnum([
+                ['NonHardened', Schema.u32],
+                ['Hardened',    Schema.u32],
+              ]),
+              chain_code:       Schema.Array(Schema.u8, 32),
+              fbk:              Schema.Struct({
+                vk:             Schema.Struct({
+                  ak:           extendedPointSchema,
+                  nk:           extendedPointSchema,
+                }),
+                ovk:            Schema.Array(Schema.u8, 32),
+              }),
+              dk:               Schema.Array(Schema.u8, 32),
+            }),
+            diversifier:        Schema.Array(Schema.u8, 11),
+            note:               noteSchema,
+            alpha:              Schema.Array(Schema.u64, 4),
+            merkle_path:        merklePathSchema
           })),
-          converts:          Schema.Vec(Schema.Struct({
-            allowed:         AllowedConversion,
-            value:           Schema.u64,
-            merkle_path:     MerklePath,
+          converts:             Schema.Vec(Schema.Struct({
+            allowed:            Schema.Struct({
+              assets:           Schema.i128,
+              generator:        extendedPointSchema
+            }),
+            value:              Schema.u64,
+            merkle_path:        merklePathSchema,
           })),
-          outputs:           Schema.Vec(Schema.Struct({
-            ovk:             Schema.Option(OutgoingViewingKey),
-            to:              PaymentAddress,
-            note:            Note,
-            memo:            MemoBytes
+          outputs:              Schema.Vec(Schema.Struct({
+            ovk:                Schema.Option(Schema.Array(Schema.u8, 32)),
+            to:                 Schema.Struct({
+              pk_d:             extendedPointSchema,
+              diversifier:      Schema.Array(Schema.u8, 11)
+            }),
+            note:               noteSchema,
+            memo:               Schema.Array(Schema.u8, 512)
           })),
         })
       })
     })],
-    ['Header',           headerSchema]
+    ['Header',                  headerSchema]
   ]))
 })
 
