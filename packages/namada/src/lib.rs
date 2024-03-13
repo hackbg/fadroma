9a206c10fa9a6e22491035002aa7a250e5bee109
Adam A <adam@hack.bg>
2024-03-13 17:03:58 +0200

    wip: feat(namada): decoding transactions from rust/wasm, pt.6

--- packages/namada/src/lib.rs
+++ packages/namada/src/lib.rs
@@ -1,21 +1,51 @@
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 use js_sys::{Uint8Array, JsString, Error, Object, Array, Reflect, BigInt, Set};
-
+use std::fmt::Write;
+use std::io::Cursor;
+use namada::{
+    account::{
+        InitAccount,
+        UpdateAccount,
+    },
+    address::Address,
+    core::borsh::BorshDeserialize,
+    governance::storage::proposal::{
+        InitProposalData,
+        VoteProposalData
+    },
+    key::common::PublicKey,
+    storage::KeySeg,
+    string_encoding::Format,
+    token::{
+        MaspDigitPos,
+        Transfer,
+    },
+    tx::{
+        Tx, Header, Section, Data, Code, Signature, Signer, MaspBuilder,
+        data::{
+            TxType,
+            pos::{
+                BecomeValidator,
+                Bond,
+                ClaimRewards,
+                CommissionChange,
+                ConsensusKeyChange,
+                MetaDataChange,
+                Unbond,
+                Withdraw
+            },
+            pgf::{
+                UpdateStewardCommission
+            },
+        }
+    },
+};
 //use masp_primitives::consensus::BranchId;
 //use masp_primitives::transaction::Transaction;
 //use masp_primitives::transaction::TxVersion;
-use namada::address::Address;
-use namada::core::borsh::BorshDeserialize;
 //use namada::core::masp_primitives::TxVersion;
 //use namada::governance::parameters::GovernanceParameters;
-use namada::storage::KeySeg;
-use namada::string_encoding::Format;
-use namada::tx::{Tx, Header, Section, Data, Code, Signature, Signer, MaspBuilder};
-use namada::tx::data::TxType;
-use namada::token::MaspDigitPos;
-use std::fmt::Write;
-use std::io::Cursor;
 
 #[wasm_bindgen]
 pub struct Decode;
@@ -35,7 +65,7 @@ impl Decode {
         let tx = Tx::try_from_slice(&to_bytes(&source))
             .map_err(|e|Error::new(&format!("{e}")))?;
         let header = tx.header();
-        Ok(object(&[
+        let result = object(&[
             ("chainId".into(),    header.chain_id.as_str().into()),
             ("expiration".into(), header.expiration.map(|t|t.to_rfc3339()).into()),
             ("timestamp".into(),  header.timestamp.to_rfc3339().into()),
@@ -55,7 +85,185 @@ impl Decode {
                 }
                 sections
             }.into())
-        ])?)
+        ])?;
+        if let TxType::Decrypted(_) = tx.header().tx_type {
+            Self::tx_content(tx, result)
+        } else {
+            Ok(result)
+        }
+    }
+
+    fn tx_content (tx: Tx, result: Object) -> Result<Object, Error> {
+        let mut tag: Option<String> = None;
+        for section in tx.sections.iter() {
+            if let Section::Code(code) = section {
+                tag = code.tag.clone();
+                if tag.is_some() {
+                    break
+                }
+            }
+        }
+        if tag.is_none() {
+            return Ok(result)
+        }
+        let mut binary: Option<&[u8]> = None;
+        for section in tx.sections.iter() {
+            if let Section::Data(data) = section {
+                binary = Some(&data.data);
+                break
+            }
+        }
+        if binary.is_none() {
+            return Ok(result)
+        }
+        let data = Object::new();
+        let binary = binary.unwrap();
+        let tag = tag.unwrap();
+        match tag.as_str() {
+            "tx_become_validator.wasm" => {
+                let inner = BecomeValidator::try_from_slice(&binary[..]);
+                populate(&data, &[
+                    ("address".into(),), // Address
+                    ("consensus_key".into(),), //PublicKey,
+                    ("eth_cold_key".into(),), //PublicKey,
+                    ("eth_hot_key".into(),), //PublicKey,
+                    ("protocol_key".into(),), //PublicKey,
+                    ("commission_rate".into(),), //Dec,
+                    ("max_commission_rate_change".into(),), //Dec,
+                    ("email".into(),), //String,
+                    ("description".into(),), //Option<String>,
+                    ("website".into(),), //Option<String>,
+                    ("discord_handle".into(),), //Option<String>,
+                    ("avatar".into(),), //Option<String>,
+                ])?;
+            },
+            "tx_bond.wasm" => {
+                let inner = Bond::try_from_slice(&binary[..]);
+                /*    pub validator: Address,
+    pub amount: Amount,
+    pub source: Option<Address>,*/
+            },
+            "tx_bridge_pool.wasm" => {
+                //let inner = BridgePool::try_from_slice(&binary[..]);
+            },
+            "tx_change_consensus_key.wasm" => {
+                let inner = ConsensusKeyChange::try_from_slice(&binary[..]);
+                /*   pub validator: Address,
+    pub consensus_key: PublicKey,*/
+            },
+            "tx_change_validator_commission.wasm" => {
+                let inner = CommissionChange::try_from_slice(&binary[..]);
+                /* pub validator: Address,
+    pub new_rate: Dec,*/
+            },
+            "tx_change_validator_metadata.wasm" => {
+                let inner = MetaDataChange::try_from_slice(&binary[..]);
+                /* pub validator: Address,
+    pub email: Option<String>,
+    pub description: Option<String>,
+    pub website: Option<String>,
+    pub discord_handle: Option<String>,
+    pub avatar: Option<String>,
+    pub commission_rate: Option<Dec>,*/
+            },
+            "tx_claim_rewards.wasm" => {
+                let inner = ClaimRewards::try_from_slice(&binary[..]);
+                /*    pub validator: Address,
+    pub source: Option<Address>,*/
+            },
+            "tx_deactivate_validator.wasm" => {
+                let inner = Address::try_from_slice(&binary[..]);
+            },
+            "tx_ibc.wasm" => {
+                //let inner = IBC::try_from_slice(&binary[..]);
+            },
+            "tx_init_account.wasm" => {
+                let inner = InitAccount::try_from_slice(&binary[..]);
+                /*   pub public_keys: Vec<PublicKey>,
+    pub vp_code_hash: Hash,
+    pub threshold: u8,*/
+            },
+            "tx_init_proposal.wasm" => {
+                let inner = InitProposalData::try_from_slice(&binary[..]);
+                /*    pub id: u64,
+    pub content: Hash,
+    pub author: Address,
+    pub type: ProposalType,
+    pub voting_start_epoch: Epoch,
+    pub voting_end_epoch: Epoch,
+    pub grace_epoch: Epoch,*/
+            },
+            "tx_reactivate_validator.wasm" => {
+                let inner = Address::try_from_slice(&binary[..]);
+            },
+            "tx_redelegate.wasm" => {
+                //let inner = Redelegate::try_from_slice(&binary[..]);
+            },
+            "tx_resign_steward.wasm" => {
+                let inner = Address::try_from_slice(&binary[..]);
+            },
+            "tx_reveal_pk.wasm" => {
+                let inner = PublicKey::try_from_slice(&binary[..]);
+            },
+            "tx_transfer.wasm" => {
+                let inner = Transfer::try_from_slice(&binary[..]);
+                /*    pub source: Address,
+    pub target: Address,
+    pub token: Address,
+    pub amount: DenominatedAmount,
+    pub key: Option<String>,
+    pub shielded: Option<Hash>,*/
+            },
+            "tx_unbond.wasm" => {
+                let inner = Unbond::try_from_slice(&binary[..]);
+                /*   pub validator: Address,
+    pub amount: Amount,
+    pub source: Option<Address>,*/
+            },
+            "tx_unjail_validator.wasm" => {
+                let inner = Address::try_from_slice(&binary[..]);
+            },
+            "tx_update_account.wasm" => {
+                let inner = UpdateAccount::try_from_slice(&binary[..]);
+                /*    pub addr: Address,
+    pub vp_code_hash: Option<Hash>,
+    pub public_keys: Vec<PublicKey>,
+    pub threshold: Option<u8>,*/
+            },
+            "tx_update_steward_commission.wasm" => {
+                let inner = UpdateStewardCommission::try_from_slice(&binary[..]);
+                /* pub addr: Address,
+    pub vp_code_hash: Option<Hash>,
+    pub public_keys: Vec<PublicKey>,
+    pub threshold: Option<u8>*/
+            },
+            "tx_vote_proposal.wasm" => {
+                let inner = VoteProposalData::try_from_slice(&binary[..]);
+                /* pub id: u64,
+    pub vote: ProposalVote,
+    pub voter: Address,
+    pub delegations: Vec<Address>,*/
+            },
+            "tx_withdraw.wasm" => {
+                let inner = Withdraw::try_from_slice(&binary[..]);
+                /*
+    pub validator: Address,
+    pub source: Option<Address>,*/
+            },
+            "vp_implicit.wasm" => {
+                //let inner = VPImplicit::try_from_slice(&binary[..]);
+            },
+            "vp_user.wasm" => {
+                //let inner = VPUser::try_from_slice(&binary[..]);
+            },
+            _ => {}
+        }
+        let content = object(&[
+            ("type".into(), tag.into()),
+            ("data".into(), data.into()),
+        ])?;
+        Reflect::set(&result, &"content".into(), &content.into())?;
+        Ok(result)
     }
 
     fn tx_section (section_data: &Section) -> Result<Object, Error> {
@@ -99,17 +307,23 @@ impl Decode {
                             let vin = Array::new();
                             for tx_data in bundle_data.vin.iter() {
                                 vin.push(&object(&[
-                                    ("assetType".into(), format!("{}", tx_data.asset_type).into()),
-                                    ("value".into(),     JsValue::from(BigInt::from(tx_data.value))),
-                                    ("address".into(),   hex::encode_upper(tx_data.address.0).into()),
+                                    ("assetType".into(),
+                                        format!("{}", tx_data.asset_type).into()),
+                                    ("value".into(),
+                                        JsValue::from(BigInt::from(tx_data.value))),
+                                    ("address".into(),
+                                        hex::encode_upper(tx_data.address.0).into()),
                                 ])?.into());
                             }
                             let vout = Array::new();
                             for tx_data in bundle_data.vout.iter() {
                                 vout.push(&object(&[
-                                    ("assetType".into(), format!("{}", tx_data.asset_type).into()),
-                                    ("value".into(),     JsValue::from(BigInt::from(tx_data.value))),
-                                    ("address".into(),   hex::encode_upper(tx_data.address.0).into()),
+                                    ("assetType".into(),
+                                        format!("{}", tx_data.asset_type).into()),
+                                    ("value".into(),
+                                        JsValue::from(BigInt::from(tx_data.value))),
+                                    ("address".into(),
+                                        hex::encode_upper(tx_data.address.0).into()),
                                 ])?.into());
                             }
                             object(&[
@@ -124,31 +338,45 @@ impl Decode {
                             let shielded_spends = Array::new();
                             for spend in bundle_data.shielded_spends.iter() {
                                 shielded_spends.push(&object(&[
-                                    ("cv".into(),           format!("{}", &spend.cv).into()),
-                                    ("anchor".into(),       hex::encode_upper(&spend.anchor.to_bytes()).into()),
-                                    ("nullifier".into(),    hex::encode_upper(&spend.nullifier).into()),
-                                    ("rk".into(),           format!("{}", &spend.rk.0).into()),
-                                    ("zkProof".into(),      hex::encode_upper(&spend.zkproof).into()),
+                                    ("cv".into(),
+                                        format!("{}", &spend.cv).into()),
+                                    ("anchor".into(),
+                                        hex::encode_upper(&spend.anchor.to_bytes()).into()),
+                                    ("nullifier".into(),
+                                        hex::encode_upper(&spend.nullifier).into()),
+                                    ("rk".into(),
+                                        format!("{}", &spend.rk.0).into()),
+                                    ("zkProof".into(),
+                                        hex::encode_upper(&spend.zkproof).into()),
                                     //("spendAuthSig".into(), to_hex(&spend.spend_auth_sig).into()),
                                 ])?.into());
                             }
                             let shielded_converts = Array::new();
                             for convert in bundle_data.shielded_converts.iter() {
                                 shielded_converts.push(&object(&[
-                                    ("cv".into(),      format!("{}", &convert.cv).into()),
-                                    ("anchor".into(),  hex::encode_upper(&convert.anchor.to_bytes()).into()),
-                                    ("zkProof".into(), hex::encode_upper(&convert.zkproof).into()),
+                                    ("cv".into(),
+                                        format!("{}", &convert.cv).into()),
+                                    ("anchor".into(),
+                                        hex::encode_upper(&convert.anchor.to_bytes()).into()),
+                                    ("zkProof".into(),
+                                        hex::encode_upper(&convert.zkproof).into()),
                                 ])?.into());
                             }
                             let shielded_outputs = Array::new();
                             for output in bundle_data.shielded_outputs.iter() {
                                 shielded_outputs.push(&object(&[
-                                    ("cv".into(),            format!("{}", &output.cv).into()),
-                                    ("cmu".into(),           hex::encode_upper(&output.cmu.to_bytes()).into()),
-                                    ("ephemeralKey".into(),  hex::encode_upper(&output.ephemeral_key.0).into()),
-                                    ("encCiphertext".into(), hex::encode_upper(&output.enc_ciphertext).into()),
-                                    ("outCiphertext".into(), hex::encode_upper(&output.out_ciphertext).into()),
-                                    ("zkProof".into(),       hex::encode_upper(&output.zkproof).into()),
+                                    ("cv".into(),
+                                        format!("{}", &output.cv).into()),
+                                    ("cmu".into(),
+                                        hex::encode_upper(&output.cmu.to_bytes()).into()),
+                                    ("ephemeralKey".into(),
+                                        hex::encode_upper(&output.ephemeral_key.0).into()),
+                                    ("encCiphertext".into(),
+                                        hex::encode_upper(&output.enc_ciphertext).into()),
+                                    ("outCiphertext".into(),
+                                        hex::encode_upper(&output.out_ciphertext).into()),
+                                    ("zkProof".into(),
+                                        hex::encode_upper(&output.zkproof).into()),
                                 ])?.into());
                             }
                             let value_balance = Object::new();
