62043c3420e705882300d36b5b682fc585bf556e
Adam A <adam@hack.bg>
2024-03-13 17:32:42 +0200

    wip: feat(namada): decoding transactions from rust/wasm, pt.7

--- packages/namada/src/lib.rs
+++ packages/namada/src/lib.rs
@@ -119,151 +119,322 @@ impl Decode {
         let data = Object::new();
         let binary = binary.unwrap();
         let tag = tag.unwrap();
-        match tag.as_str() {
-            "tx_become_validator.wasm" => {
-                let inner = BecomeValidator::try_from_slice(&binary[..]);
-                populate(&data, &[
-                    ("address".into(),), // Address
-                    ("consensus_key".into(),), //PublicKey,
-                    ("eth_cold_key".into(),), //PublicKey,
-                    ("eth_hot_key".into(),), //PublicKey,
-                    ("protocol_key".into(),), //PublicKey,
-                    ("commission_rate".into(),), //Dec,
-                    ("max_commission_rate_change".into(),), //Dec,
-                    ("email".into(),), //String,
-                    ("description".into(),), //Option<String>,
-                    ("website".into(),), //Option<String>,
-                    ("discord_handle".into(),), //Option<String>,
-                    ("avatar".into(),), //Option<String>,
-                ])?;
-            },
-            "tx_bond.wasm" => {
-                let inner = Bond::try_from_slice(&binary[..]);
-                /*    pub validator: Address,
-    pub amount: Amount,
-    pub source: Option<Address>,*/
-            },
-            "tx_bridge_pool.wasm" => {
-                //let inner = BridgePool::try_from_slice(&binary[..]);
-            },
-            "tx_change_consensus_key.wasm" => {
-                let inner = ConsensusKeyChange::try_from_slice(&binary[..]);
-                /*   pub validator: Address,
-    pub consensus_key: PublicKey,*/
-            },
-            "tx_change_validator_commission.wasm" => {
-                let inner = CommissionChange::try_from_slice(&binary[..]);
-                /* pub validator: Address,
-    pub new_rate: Dec,*/
-            },
-            "tx_change_validator_metadata.wasm" => {
-                let inner = MetaDataChange::try_from_slice(&binary[..]);
-                /* pub validator: Address,
-    pub email: Option<String>,
-    pub description: Option<String>,
-    pub website: Option<String>,
-    pub discord_handle: Option<String>,
-    pub avatar: Option<String>,
-    pub commission_rate: Option<Dec>,*/
-            },
-            "tx_claim_rewards.wasm" => {
-                let inner = ClaimRewards::try_from_slice(&binary[..]);
-                /*    pub validator: Address,
-    pub source: Option<Address>,*/
-            },
-            "tx_deactivate_validator.wasm" => {
-                let inner = Address::try_from_slice(&binary[..]);
-            },
-            "tx_ibc.wasm" => {
-                //let inner = IBC::try_from_slice(&binary[..]);
-            },
-            "tx_init_account.wasm" => {
-                let inner = InitAccount::try_from_slice(&binary[..]);
-                /*   pub public_keys: Vec<PublicKey>,
-    pub vp_code_hash: Hash,
-    pub threshold: u8,*/
-            },
-            "tx_init_proposal.wasm" => {
-                let inner = InitProposalData::try_from_slice(&binary[..]);
-                /*    pub id: u64,
-    pub content: Hash,
-    pub author: Address,
-    pub type: ProposalType,
-    pub voting_start_epoch: Epoch,
-    pub voting_end_epoch: Epoch,
-    pub grace_epoch: Epoch,*/
-            },
-            "tx_reactivate_validator.wasm" => {
-                let inner = Address::try_from_slice(&binary[..]);
-            },
-            "tx_redelegate.wasm" => {
-                //let inner = Redelegate::try_from_slice(&binary[..]);
-            },
-            "tx_resign_steward.wasm" => {
-                let inner = Address::try_from_slice(&binary[..]);
-            },
-            "tx_reveal_pk.wasm" => {
-                let inner = PublicKey::try_from_slice(&binary[..]);
-            },
-            "tx_transfer.wasm" => {
-                let inner = Transfer::try_from_slice(&binary[..]);
-                /*    pub source: Address,
-    pub target: Address,
-    pub token: Address,
-    pub amount: DenominatedAmount,
-    pub key: Option<String>,
-    pub shielded: Option<Hash>,*/
-            },
-            "tx_unbond.wasm" => {
-                let inner = Unbond::try_from_slice(&binary[..]);
-                /*   pub validator: Address,
-    pub amount: Amount,
-    pub source: Option<Address>,*/
-            },
-            "tx_unjail_validator.wasm" => {
-                let inner = Address::try_from_slice(&binary[..]);
-            },
-            "tx_update_account.wasm" => {
-                let inner = UpdateAccount::try_from_slice(&binary[..]);
-                /*    pub addr: Address,
-    pub vp_code_hash: Option<Hash>,
-    pub public_keys: Vec<PublicKey>,
-    pub threshold: Option<u8>,*/
-            },
-            "tx_update_steward_commission.wasm" => {
-                let inner = UpdateStewardCommission::try_from_slice(&binary[..]);
-                /* pub addr: Address,
-    pub vp_code_hash: Option<Hash>,
-    pub public_keys: Vec<PublicKey>,
-    pub threshold: Option<u8>*/
-            },
-            "tx_vote_proposal.wasm" => {
-                let inner = VoteProposalData::try_from_slice(&binary[..]);
-                /* pub id: u64,
-    pub vote: ProposalVote,
-    pub voter: Address,
-    pub delegations: Vec<Address>,*/
-            },
-            "tx_withdraw.wasm" => {
-                let inner = Withdraw::try_from_slice(&binary[..]);
-                /*
-    pub validator: Address,
-    pub source: Option<Address>,*/
-            },
-            "vp_implicit.wasm" => {
-                //let inner = VPImplicit::try_from_slice(&binary[..]);
-            },
-            "vp_user.wasm" => {
-                //let inner = VPUser::try_from_slice(&binary[..]);
-            },
+        let data = match tag.as_str() {
+            "tx_become_validator.wasm" =>
+                Self::tx_content_become_validator(binary),
+            "tx_bond.wasm" =>
+                Self::tx_content_bond(binary),
+            "tx_bridge_pool.wasm" =>
+                Ok(JsValue::NULL),
+            "tx_change_consensus_key.wasm" =>
+                Self::tx_content_change_consensus_key(binary),
+            "tx_change_validator_commission.wasm" =>
+                Self::tx_content_change_validator_commission(binary),
+            "tx_change_validator_metadata.wasm" =>
+                Self::tx_content_change_validator_metadata(binary),
+            "tx_claim_rewards.wasm" =>
+                Self::tx_content_claim_rewards(binary),
+            "tx_deactivate_validator.wasm" =>
+                Self::tx_content_deactivate_validator(binary),
+            "tx_ibc.wasm" =>
+                Ok(JsValue::NULL),
+            "tx_init_account.wasm" =>
+                Self::tx_content_init_account(binary),
+            "tx_init_proposal.wasm" =>
+                Self::tx_content_init_proposal(binary),
+            "tx_reactivate_validator.wasm" =>
+                Self::tx_content_reactivate_validator(binary),
+            "tx_redelegate.wasm" =>
+                Ok(JsValue::NULL),
+            "tx_resign_steward.wasm" =>
+                Self::tx_content_resign_steward(binary),
+            "tx_reveal_pk.wasm" =>
+                Self::tx_content_reveal_pk(binary),
+            "tx_transfer.wasm" =>
+                Self::tx_content_transfer(binary),
+            "tx_unbond.wasm" =>
+                Self::tx_content_unbond(binary),
+            "tx_unjail_validator.wasm" =>
+                Self::tx_content_unjail_validator(binary),
+            "tx_update_account.wasm" =>
+                Self::tx_content_update_account(binary),
+            "tx_update_steward_commission.wasm" =>
+                Self::tx_content_update_steward_commission(binary),
+            "tx_vote_proposal.wasm" =>
+                Self::tx_content_vote_proposal(binary),
+            "tx_withdraw.wasm" =>
+                Self::tx_content_withdraw(binary),
+            "vp_implicit.wasm" =>
+                Ok(JsValue::NULL),
+            "vp_user.wasm" =>
+                Ok(JsValue::NULL),
             _ => {}
-        }
+        };
         let content = object(&[
             ("type".into(), tag.into()),
             ("data".into(), data.into()),
         ])?;
         Reflect::set(&result, &"content".into(), &content.into())?;
         Ok(result)
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_become_validator (binary: &[u8]) -> Result<Object, Error> {
+        let inner = BecomeValidator::try_from_slice(&binary[..])?;
+        object(&[
+            ("address".into(),
+                inner.address), // Address
+            ("consensusKey".into(),
+                inner.consensus_key), //PublicKey,
+            ("ethColdKey".into(),
+                inner.eth_cold_key), //PublicKey,
+            ("ethHotKey".into(),
+                inner.eth_hot_key), //PublicKey,
+            ("protocolKey".into(),
+                inner.protocol_key), //PublicKey,
+            ("commissionRate".into(),
+                inner.commission_rate), //Dec,
+            ("maxCommissionRateChange".into(),
+                inner.max_commission_rate_change), //Dec,
+            ("email".into(),
+                inner.email), //String,
+            ("description".into(),
+                inner.description), //Option<String>,
+            ("website".into(),
+                inner.website), //Option<String>,
+            ("discord_handle".into(),
+                inner.discor_handle), //Option<String>,
+            ("avatar".into(),
+                inner.avatar), //Option<String>,
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_bond (binary: &[u8]) -> Result<Object, Error> {
+        let inner = Bond::try_from_slice(&binary[..])?;
+        object(&[
+            ("validator".into(),
+                inner.validator), //    pub validator: Address,
+            ("amount".into(),
+                inner.amount), // Amount
+            ("source".into(),
+                inner.source) //        pub source: Option<Address>,*/
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_change_consensus_key (binary: &[u8]) -> Result<Object, Error> {
+        let inner = ConsensusKeyChange::try_from_slice(&binary[..])?;
+        object(&[
+            ("validator".into(),
+                inner.validator), // Address,
+            ("consensusKey".into(),
+                inner.consensus_key), //   pub consensus_key: PublicKey,*/
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_change_validator_commission (binary: &[u8]) -> Result<Object, Error> {
+        let inner = CommissionChange::try_from_slice(&binary[..])?;
+        object(&[
+            ("validator",
+                inner.validator), // Address,
+            ("newRate",
+                inner.new_rate), // Dec,*/
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_change_validator_metadata (binary: &[u8]) -> Result<Object, Error> {
+        let inner = MetaDataChange::try_from_slice(&binary[..])?;
+        object(&[
+            ("validator",
+                inner.validator), // validator: Address,
+            ("email",
+                inner.email),//    pub email: Option<String>,
+            ("description",
+                inner.description),//    pub description: Option<String>,
+            ("website",
+                inner.website), //    pub website: Option<String>,
+            ("discordHandle",
+                inner.discord_handle),//    pub discord_handle: Option<String>,
+            ("avatar",
+                inner.avatar),//    pub avatar: Option<String>,
+            ("commissionRate",
+                inner.commission_rate),//   pub commission_rate: Option<Dec>,*/
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_claim_rewards (binary: &[u8]) -> Result<Object, Error> {
+        let inner = ClaimRewards::try_from_slice(&binary[..])?;
+        object(&[
+            ("validator",
+                inner.validator), //          /*    pub validator: Address,
+            ("source",
+                inner.source), //    pub source: Option<Address>,*/
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_deactivate_validator (binary: &[u8]) -> Result<Object, Error> {
+        let inner = Address::try_from_slice(&binary[..])?;
+        object(&[
+            ("address",
+                inner),
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_init_account (binary: &[u8]) -> Result<Object, Error> {
+        let inner = InitAccount::try_from_slice(&binary[..])?;
+        object(&[
+            ("publicKeys",
+                inner.public_keys),//               /*   pub public_keys: Vec<PublicKey>,
+            ("vpCodeHash",
+                inner.vp_code_hash), //    pub vp_code_hash: Hash,
+            ("threshold",
+                inner.threshold),//: u8,*/
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_init_proposal (binary: &[u8]) -> Result<Object, Error> {
+        let inner = InitProposalData::try_from_slice(&binary[..])?;
+        object(&[
+            ("id".into(),
+                inner.id), //               /*    pub id: u64,
+            ("content".into(),
+                inner.content),//: Hash,
+            ("author".into(),
+                inner.author), // Address,
+            ("type".into(),
+                inner.r#type),//    pub type: ProposalType,
+            ("votingStartEpoch",
+                inner.voting_start_epoch), //  pub voting_start_epoch: Epoch,
+            ("votingEndEpoch",
+                inner.voting_end_epoch),//    pub voting_end_epoch: Epoch,
+            ("graceEpoch",
+                inner.grace_epoch),//    pub grace_epoch: Epoch,*/
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_reactivate_validator (binary: &[u8]) -> Result<Object, Error> {
+        let inner = Address::try_from_slice(&binary[..])?;
+        object(&[
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_resign_steward (binary: &[u8]) -> Result<Object, Error> {
+        let inner = Address::try_from_slice(&binary[..])?;
+        object(&[
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_reveal_pk (binary: &[u8]) -> Result<Object, Error> {
+        let inner = PublicKey::try_from_slice(&binary[..])?;
+        object(&[
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_transfer (binary: &[u8]) -> Result<Object, Error> {
+        let inner = Transfer::try_from_slice(&binary[..])?;
+        object(&[
+            ("source".into(),
+                inner.source),//        /*    pub source: Address,
+            ("target".into(),
+                inner.target),//pub target: Address,
+            ("token".into(),
+                inner.token),//pub token: Address,
+            ("amount".into(),
+                inner.amount),//pub amount: DenominatedAmount,
+            ("key".into(),
+                inner.key),//pub key: Option<String>,
+            ("shielded".into(),
+                inner.shielded),//pub shielded: Option<Hash>,*/
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_unbond (binary: &[u8]) -> Result<Object, Error> {
+        let inner = Unbond::try_from_slice(&binary[..])?;
+        object(&[
+            ("validator".into(),
+                inner.validator),//        /*   pub validator: Address,
+            ("amount".into(),
+                inner.amount),//pub amount: Amount,
+            ("source".into(),
+                inner.source),//pub source: Option<Address>,*/
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_unjail_validator (binary: &[u8]) -> Result<Object, Error> {
+        let inner = Address::try_from_slice(&binary[..])?;
+        object(&[
+            ("address".into(),
+                inner),
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_update_account (binary: &[u8]) -> Result<Object, Error> {
+        let inner = UpdateAccount::try_from_slice(&binary[..])?;
+        object(&[
+            ("addr".into(),
+                inner.addr),//        /*    pub addr: Address,
+            ("vpCodeHash".into(),
+                inner.vp_code_hash),//pub vp_code_hash: Option<Hash>,
+            ("publicKeys".into(),
+                inner.public_keys),//ub public_keys: Vec<PublicKey>,
+            ("threshold".into(),
+                inner.threshold),//pub threshold: Option<u8>,*/
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_update_steward_commission (binary: &[u8]) -> Result<Object, Error> {
+        let inner = UpdateStewardCommission::try_from_slice(&binary[..])?;
+        object(&[
+            ("addr".into(),
+                inner.addr),//        /* pub addr: Address,
+            ("vpCodeHash".into(),
+                inner.vp_code_hash),//pub vp_code_hash: Option<Hash>,
+            ("publicKeys".into(),
+                inner.public_keys),//pub public_keys: Vec<PublicKey>,
+            ("threshold".into(),
+                inner.threshold),//pub threshold: Option<u8>*/
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_vote_proposal (binary: &[u8]) -> Result<Object, Error> {
+        let inner = VoteProposalData::try_from_slice(&binary[..])?;
+        object(&[
+            ("id".into(),
+                inner.id),//        /* pub id: u64,
+            ("vote".into(),
+                inner.vote),//pub vote: ProposalVote,
+            ("voter".into(),
+                inner.voter),//pub voter: Address,
+            ("delegations".into(),
+                inner.delegations),//pub delegations: Vec<Address>,*/
+        ])
+    }
+
+    #[wasm_bindgen]
+    pub fn tx_content_withdraw (binary: &[u8]) -> Result<Object, Error> {
+        let inner = Withdraw::try_from_slice(&binary[..])?;
+        object(&[
+            ("validator",
+                inner.validator),//        /*pub validator: Address,
+            ("source",
+                inner.source),//pub source: Option<Address>,*/
+        ])
     }
 
     fn tx_section (section_data: &Section) -> Result<Object, Error> {
