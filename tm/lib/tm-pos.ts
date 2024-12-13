6db781de34f661fa51108bca34dec98f19871329
Adam A <adam@hack.bg>
2024-12-13 13:20:42 +0100

    wip: feat(tm): remove unused modules

--- tm/lib/tm-pos.ts
+++
@@ -1,4 +0,0 @@
-export interface Validator {
-  readonly chain:   Chain
-  readonly address: Address
-}
--- tm/lib/tmTypes.ts
+++ tm/lib/tmTypes.ts
@@ -24,3 +24,22 @@ export interface Transaction extends Base.Transaction {
 export interface Block extends Base.Block {
   hash: unknown
 }
+
+export type ProposalId = bigint
+
+export type ProposalResult = 'Pass'|'Fail'
+
+export interface Proposal {
+  id:     ProposalId
+  votes:  Vote[]
+  result: ProposalResult
+}
+
+export type VoteValue = 'Yay'|'Nay'|'Abstain'
+
+export interface Vote {
+  proposal: ProposalId
+  voter:    Base.Address
+  power:    bigint
+  value:    VoteValue
+}
