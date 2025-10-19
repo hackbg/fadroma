c2f4ebe45e21568e30ef1bb628605567cb143c43
skunkworks <adam@hack.bg>
2025-10-20 01:38:55 +0300

    test(tester): respec

--- lib/tester/test.ts
+++ lib/tester/test.ts
@@ -1,12 +1,11 @@
 #!/usr/bin/env -S deno run --allow-env
 import { suite, expect } from './index.ts';
 export default suite(import.meta, 'Tester',
-  expect('Reporting',
-    expect('Categories'),
-    expect('Result set',
-      expect('Step counter'))),
-  expect('Execution',
-    expect('Suite'),
-    expect('Expect'),
-    expect('Forbid'),
-    expect('Matrix')));
+  expect('Category'),
+  expect('Context'),
+  expect('Counter'),
+  expect('Result'),
+  expect('Suite'),
+  expect('Expect'),
+  expect('Forbid'),
+  expect('Matrix'));
