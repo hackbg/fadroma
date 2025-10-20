24299237a5eddbee9edd7a2fea061e1ba1ebe3df
skunkworks <adam@hack.bg>
2025-10-20 21:59:09 +0300

    feat(error): add Oops

--- lib/@hackbg/fadroma/error/index.ts
+++ lib/@hackbg/fadroma/error/index.ts
@@ -1,4 +1,27 @@
 import { cwd } from '../deps.ts';
+
+export class Oops extends Error {
+  /** Define an error subclass. */
+  static define <T extends unknown[]> (
+    /** Name of error class. Prepended to parent. */
+    name: string,
+    /** How to generate the error message from the arguments passed to the constructor. */
+    getMessage: (string|((...args: T)=>string)) = (...args: T) => args.join(' '),
+    /** Whether there are any further construction steps such as assigning properties. */
+    construct?: (self: Error, ...args: T) => any
+  ) {
+    const fullName = `${this.name}_${name}`
+    class OopsError extends this {
+      name = fullName
+      constructor (...args: T) {
+        super((typeof getMessage === 'string') ? getMessage : getMessage(...args))
+        if (construct) construct(this, ...args)
+      }
+    }
+    return Object.defineProperty(OopsError, 'name', { value: fullName })
+  }
+}
+
 export const formatError = (e: Error, name?: Stringy) => {
   const [head, ...tail] = (e?.stack||'').split('\n')
   const stack = tail.map(x=>x
