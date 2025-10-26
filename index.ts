2e667eb972994d5d586bc3e401e0534227441e33
skunkworks <adam@hack.bg>
2025-10-26 20:16:40 +0200

    fix: workspace

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-export * as Tester from '@hackbg/tester';
-export * as Tm     from '@fadroma/tm';
-export * as Cw     from '@fadroma/cw';
-export * as Scrt   from '@fadroma/scrt';
-export * as Namada from '@fadroma/namada';
+export * from '@hackbg/fadroma';
+export * as Btc  from '@fadroma/btc';
+export * as Simf from '@fadroma/simf';
