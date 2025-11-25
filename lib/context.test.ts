4fc1239e47a4f3b6ac5e2982780df9d955120bbc
skunkworks <adam@hack.bg>
2025-11-25 18:21:29 +0200

    chore(bin): rename

--- lib/context.test.ts
+++ lib/context.test.ts
@@ -1,8 +1,7 @@
 #!/usr/bin/env -S deno run --coverage --allow-env --allow-net --allow-run --allow-import --allow-read --allow-write=/tmp/fadroma
 import { Test } from "./index.ts";
-import UI      from './context/ui.test.ts';
-import OS      from './context/os.test.ts';
-import Net     from './context/net.test.ts';
-import Project from './context/project.test.ts';
-import Tester  from './context/tester.test.ts';
-export default Test.suite(import.meta, 'Context', UI, OS, Net, Project, Tester);
+import UI from './context/ui.test.ts';
+import OS from './context/os.test.ts';
+import Net from './context/net.test.ts';
+import Tester from './context/tester.test.ts';
+export default Test.suite(import.meta, 'Context', UI, OS, Net, Tester);
