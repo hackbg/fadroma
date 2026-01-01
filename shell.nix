d7314cfef5da31c088e658bdf6cdfe38f8a9ef08
skunkworks <adam@hack.bg>
2026-01-01 02:03:47 +0200

    chore: cleanup shell.nix

--- shell.nix
+++ shell.nix
@@ -21,23 +21,33 @@
   # Override package attributes
   over = pkg: attrs: pkg.overrideAttrs (_: attrs);
 
-in sh "fadroma" [
-  pkgs.lld
-  pkgs.cloc
-  pkgs.bitcoind
-  #pkgs.emscripten
-  pkgs.binaryen
-  pkgs.elements
-  pkgs.python3
-  #pkgs.rustPlatform.bindgenHook
-  #(rs-gh "starkware-bitcoin" "simply" "3e1d0589"
-    #"sha256-EKfeEsr/sG/SorT2GK/ovMvI2QaoTMZ1wehbCcSjEmQ="
-    #"sha256-N2i5IJtKU1iPkpBaX90LgA7gw8B3n+K5hbByJOMRV3o=")
-  #(rs-gh "drager" "wasm-pack" "f28cf3e7"
-    #"sha256-Zv4WFv/lVySs6qfBC/hnZLe6T5Ako/HEDzvP/be4dgM="
-    #"sha256-Dw/Kz4YO/RMRTlUsW+5im3cVmqC80dMVjLc0+Ge536o=")
-] {
-  #CC     = "${pkgs.llvmPackages.clang-unwrapped}/bin/clang";
-  #CC_wasm32_unknown_unknown     = "${pkgs.llvmPackages.clang-unwrapped}/bin/clang";
-  #CFLAGS_wasm32_unknown_unknown = "-I ${pkgs.llvmPackages.libclang.lib}/lib/clang/include/";
-}
+  deps = [
+    pkgs.lld
+    pkgs.cloc
+    pkgs.bitcoind
+    pkgs.binaryen
+    pkgs.elements
+    pkgs.python3
+
+    # FIXME: provides correct clang without container?
+    # pkgs.rustPlatform.bindgenHook
+
+    # Not used.
+    # pkgs.emscripten
+
+    # Examples of loading third-party dependencies from GitHub:
+    # (rs-gh "starkware-bitcoin" "simply" "3e1d0589"
+    #   "sha256-EKfeEsr/sG/SorT2GK/ovMvI2QaoTMZ1wehbCcSjEmQ="
+    #   "sha256-N2i5IJtKU1iPkpBaX90LgA7gw8B3n+K5hbByJOMRV3o=")
+    # (rs-gh "drager" "wasm-pack" "f28cf3e7"
+    #   "sha256-Zv4WFv/lVySs6qfBC/hnZLe6T5Ako/HEDzvP/be4dgM="
+    #   "sha256-Dw/Kz4YO/RMRTlUsW+5im3cVmqC80dMVjLc0+Ge536o=")
+  ];
+
+  vars = {
+    #CC     = "${pkgs.llvmPackages.clang-unwrapped}/bin/clang";
+    #CC_wasm32_unknown_unknown     = "${pkgs.llvmPackages.clang-unwrapped}/bin/clang";
+    #CFLAGS_wasm32_unknown_unknown = "-I ${pkgs.llvmPackages.libclang.lib}/lib/clang/include/";
+  };
+
+in sh "fadroma" deps vars
