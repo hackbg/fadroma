4d9ed95f57d470ba80dc5a580c6b15a4283e8883
Hack.bg Admin <hello@hack.bg>
2026-01-14 12:10:11 +0000

    wip: test(simf): add source of example escrow program

--- lib/platform/simf.test.ts
+++ lib/platform/simf.test.ts
@@ -172,6 +172,54 @@ function Examples () {
       }`
     }
   }
+  function EscrowProgram ({
+    sender    = '0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798',
+    recipient = '0xc6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5',
+    escrow    = '0xf9308a019258c31049344f85f89d5229b531c845836f99b08601f113bce036f9',
+  } = {}) {
+    return `
+    /*
+     * https://github.com/BlockstreamResearch/SimplicityHL/blob/master/examples/escrow_with_delay.simf
+     * https://docs.ivylang.org/bitcoin/language/ExampleContracts.html#escrowwithdelay
+     */
+    fn main () {
+      match witness::TRANSFER_OR_TIMEOUT {
+        Left(maybe_sigs: [Option<Signature>; 3]) => transfer_spend(maybe_sigs),
+        Right(sender_sig: Signature) => timeout_spend(sender_sig),
+      }
+    }
+    fn transfer_spend (maybe_sigs: [Option<Signature>; 3]) {
+      let threshold: u8 = 2;
+      let [sig1, sig2, sig3]: [Option<Signature>; 3] = maybe_sigs;
+      let counter1: u8 = checksig_add(0,        ${sender},    sig1);
+      let counter2: u8 = checksig_add(counter1, ${recipient}, sig2);
+      let counter3: u8 = checksig_add(counter2, ${escrow},    sig3);
+      assert!(jet::eq_8(counter3, threshold));
+    }
+    fn checksig_add (counter: u8, pk: Pubkey, maybe_sig: Option<Signature>) -> u8 {
+      match maybe_sig {
+        Some(sig: Signature) => {
+          checksig(pk, sig);
+          let (carry, new_counter): (bool, u8) = jet::increment_8(counter);
+          assert!(not(carry));
+          new_counter
+        }
+        None => counter,
+      }
+    }
+    fn not (bit: bool) -> bool {
+      <u1>::into(jet::complement_1(<bool>::into(bit)))
+    }
+    fn timeout_spend (sender_sig: Signature) {
+      let sender_pk: Pubkey = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798; // 1 * G
+      checksig(sender_pk, sender_sig);
+      let timeout: Distance = 1000;
+      jet::check_lock_distance(timeout);
+    }
+    fn checksig (pk: Pubkey, sig: Signature) {
+      jet::bip_0340_verify((pk, jet::sig_all_hash()), sig);
+    }`
+  }
 }
 function daemonOptions (
   chain = 'elementsregtest'
