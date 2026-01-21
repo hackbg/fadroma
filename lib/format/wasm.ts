eeba3207f6e0f6b199ae799e7c481fd9ce20e7ca
Hack.bg Admin <hello@hack.bg>
2026-01-21 13:38:02 +0000

    fix(wasm): default to URLs in loader

--- lib/format/wasm.ts
+++ lib/format/wasm.ts
@@ -1,12 +1,15 @@
+import { readFile } from 'node:fs/promises';
 export function wasmLoader <T> (
-  wasmPath: string,
-  wrapPath: string,
+  wasmUrl: string|URL,
+  wrapUrl: string|URL,
 ): () => Promise<T> {
-  let ready: Promise<T>
+  let ready: Promise<T>;
+  wasmUrl = new URL(wasmUrl);
+  wrapUrl = new URL(wrapUrl);
   ready ??= new Promise(async (resolve, reject)=>{
     try {
-      const wrap = await import(wrapPath);
-      const wasm = await Deno.readFile(wasmPath);
+      const wrap = await import(wrapUrl);
+      const wasm = await readFile(wasmUrl);
       await wrap.default(wasm);
       resolve(wrap);
     } catch (e) {
