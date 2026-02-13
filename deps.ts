499daa922ed39c75f2ff55ec7af771a6706258da
Hack.bg Admin <hello@hack.bg>
2026-02-13 20:35:03 +0000

    chore(deps): bump, cleanup

--- deps.ts
+++ deps.ts
@@ -1,57 +1,47 @@
 // Node-style builtins:
-
+import process from 'node:process';
+import { execFile } from 'node:child_process';
+import { promisify } from 'node:util';
 export { ok, throws, rejects, deepStrictEqual as equal } from 'node:assert';
-export { setImmediate, } from 'node:timers';
+export { setImmediate } from 'node:timers';
 export { tmpdir } from 'node:os';
 export { fileURLToPath } from 'node:url';
 export { inspect } from 'node:util';
 export { spawn as spawnImpl } from 'node:child_process';
 export type { ChildProcess } from 'node:child_process';
 export type { Buffer } from 'node:buffer';
-
-export const webcrypto =
-  globalThis.crypto ?? (await import('node:crypto')).webcrypto;
-
-import process from 'node:process';
-export { process };
+export const webcrypto = globalThis.crypto ?? (await import('node:crypto')).webcrypto;
 export const { stdin, stdout, stderr, argv, env, cwd, exit } = process;
-
+export { process };
 export { realpathSync } from 'node:fs';
 export { mkdir, rm, mkdtemp, writeFile } from 'node:fs/promises';
+export const execImpl = promisify(execFile);
+export { execFile };
 export { dirname
        , join     as joinPath
        , resolve  as resolvePath
        , relative as relativePath } from 'node:path';
 
-import { execFile } from 'node:child_process';
-import { promisify } from 'node:util';
-export const execImpl = promisify(execFile);
-export { execFile };
-
 // Deno-style builtins:
-
-export const {
-  connect: denoConnect, listen: denoListen, watchFs
-} = globalThis.Deno || {};
-
 export type TcpConn = Deno.TcpConn;
+export const { connect: denoConnect, listen: denoListen, watchFs } = globalThis.Deno || {};
 
 // Third-party packages:
-
 export { base16, base64, bech32, bech32m } from 'npm:@scure/base'
 export { default as Case } from 'npm:case';
 //export { Buffer as StreamBuffer } from '@std/streams';
 
-export const getCreateLogUpdate = () =>
-  import('npm:log-update').then(c=>c.createLogUpdate)
 if (globalThis.Deno) {
   await import("https://deno.land/x/indexeddb@v1.1.0/polyfill_memory.ts");
 }
 
 export const getIndexd       = () => import('npm:indexd');
-export const getBrowserLevel = () => import('npm:browser-level');
 export const getBtcJs        = () => import('npm:bitcoinjs-lib');
 export const getPbVarint     = () => import('npm:protobuf-varint');
+export const getBrowserLevel = () => import('npm:browser-level');
+
+export const getCreateLogUpdate = () =>
+  import('npm:log-update').then(c=>c.createLogUpdate);
 
 export async function fetchText (href: string|URL): Promise<string> {
   if (('Deno' in globalThis) && ('readFile' in globalThis.Deno)) {
