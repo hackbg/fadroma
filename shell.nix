9cb8312fc1f2f1fd240263f741b596c9368c3195
Hack.bg Admin <hello@hack.bg>
2026-02-17 12:00:05 +0000

    chore(shell): update

--- shell.nix
+++ shell.nix
@@ -1,16 +1,30 @@
 #!/usr/bin/env nix-shell
 {pkgs?import<nixpkgs>{}}: let
-  # Define Nix shell.
-  sh = name: nativeBuildInputs: opts:
-    pkgs.mkShell ({ inherit name nativeBuildInputs; } // opts);
-  # Fetch source from GitHub.
-  gh = owner: repo: rev: sha256:
-    pkgs.fetchFromGitHub { inherit owner repo rev sha256; };
-  # Build package.
+
+  # Define a Nix shell.
+  sh = name: nativeBuildInputs: opts:  pkgs.mkShell ({ inherit name nativeBuildInputs; } // opts);
+
+  # Fetch a source from GitHub.
+  gh = owner: repo: rev: sha256: pkgs.fetchFromGitHub { inherit owner repo rev sha256; };
+
+  # Build a package.
   pkg = p: (pkgs.callPackage p {});
-  # Build Rust package.
+
+  # Build a Rust package.
   rs = p: (pkgs.rustPlatform.buildRustPackage p);
-  # Build Rust package from GitHub.
+
+  # Build a Rust package from GitHub.
+  #
+  # Examples:
+  #
+  #   (rs-gh "starkware-bitcoin" "simply" "3e1d0589"
+  #     "sha256-EKfeEsr/sG/SorT2GK/ovMvI2QaoTMZ1wehbCcSjEmQ="
+  #     "sha256-N2i5IJtKU1iPkpBaX90LgA7gw8B3n+K5hbByJOMRV3o=")
+  #
+  #   (rs-gh "drager" "wasm-pack" "f28cf3e7"
+  #     "sha256-Zv4WFv/lVySs6qfBC/hnZLe6T5Ako/HEDzvP/be4dgM="
+  #     "sha256-Dw/Kz4YO/RMRTlUsW+5im3cVmqC80dMVjLc0+Ge536o=")
+  #
   rs-gh = owner: pname: version: sha256: cargoHash: (rs rec {
     inherit pname version cargoHash;
     src = gh owner pname version sha256;
@@ -18,47 +32,44 @@
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
     doCheck = false;
   });
-  # Override package attributes
+
+  # Override an existing package's attributes
+  #
+  # Example:
+  #
+  #   (over pkgs.elements (let hash = "sha256-hqHKH9B6EITwZ4F+YdPJI4n3Z3EeXdPYbzRoNODlThY="; in {
+  #     doCheck    = false;
+  #     patches    = [];
+  #     src        = gh "ElementsProject" "elements" "elements-23.3.1" hash;
+  #     version    = "23.3.1";
+  #     withGui    = false;
+  #     withWallet = true;
+  #   }))
+  #
   over = pkg: attrs: pkg.overrideAttrs (_: attrs);
 
-  deps = [
-    pkgs.lld
-    pkgs.cloc
-    pkgs.deno
-    pkgs.nodejs_24
-    pkgs.bitcoind
-    pkgs.binaryen
-    pkgs.python3
-    pkgs.pnpm
-    (over pkgs.elements {
-      patches = [];
-      doCheck = false;
-      withWallet = true;
-      withGui = false;
-      version = "23.3.1";
-      src = gh "ElementsProject" "elements" "elements-23.3.1"
-        "sha256-hqHKH9B6EITwZ4F+YdPJI4n3Z3EeXdPYbzRoNODlThY=";
-    })
+in sh "fadroma" [
 
-    # FIXME: provides correct clang without container?
-    # pkgs.rustPlatform.bindgenHook
+  # JS/TS tooling
+  pkgs.deno pkgs.nodejs_24 pkgs.pnpm
 
-    # Not used.
-    # pkgs.emscripten
+  # WASM tooling
+  pkgs.lld pkgs.binaryen pkgs.python3
 
-    # Examples of loading third-party dependencies from GitHub:
-    # (rs-gh "starkware-bitcoin" "simply" "3e1d0589"
-    #   "sha256-EKfeEsr/sG/SorT2GK/ovMvI2QaoTMZ1wehbCcSjEmQ="
-    #   "sha256-N2i5IJtKU1iPkpBaX90LgA7gw8B3n+K5hbByJOMRV3o=")
-    # (rs-gh "drager" "wasm-pack" "f28cf3e7"
-    #   "sha256-Zv4WFv/lVySs6qfBC/hnZLe6T5Ako/HEDzvP/be4dgM="
-    #   "sha256-Dw/Kz4YO/RMRTlUsW+5im3cVmqC80dMVjLc0+Ge536o=")
-  ];
+  # Code analysis
+  pkgs.cloc
 
-  vars = {
-    #CC     = "${pkgs.llvmPackages.clang-unwrapped}/bin/clang";
-    #CC_wasm32_unknown_unknown     = "${pkgs.llvmPackages.clang-unwrapped}/bin/clang";
-    #CFLAGS_wasm32_unknown_unknown = "-I ${pkgs.llvmPackages.libclang.lib}/lib/clang/include/";
-  };
+  # Not used but may become useful in the future:
+  # pkgs.emscripten
+  # pkgs.bitcoind
+  # pkgs.rustPlatform.bindgenHook # Provides correct clang without container?
 
-in sh "fadroma" deps vars
+] {
+
+  # Any extra environment variables to set?
+  #
+  # CC     = "${pkgs.llvmPackages.clang-unwrapped}/bin/clang";
+  # CC_wasm32_unknown_unknown     = "${pkgs.llvmPackages.clang-unwrapped}/bin/clang";
+  # CFLAGS_wasm32_unknown_unknown = "-I ${pkgs.llvmPackages.libclang.lib}/lib/clang/include/";
+
+}
