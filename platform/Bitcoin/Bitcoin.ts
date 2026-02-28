5da2c7c3fcd6753d6fa38c991844021d72bf2ce1
Hack.bg Admin <hello@hack.bg>
2026-02-28 19:36:48 +0000

    fix(btc): esplora url

--- platform/Bitcoin/Bitcoin.ts
+++ platform/Bitcoin/Bitcoin.ts
@@ -157,7 +157,7 @@ namespace Bitcoin {
     export const RETURN_TEST  = 'tlq1qq2g07nju42l0nlx0erqa3wsel2l8prnq96rlnhml262mcj7pe8w6ndvvyg237japt83z24m8gu4v3yfhaqvrqxydadc9scsmw';
     export const RETURN_AMP   = 'vjU8JWGnZu6XavzMEbLZ3mGZ3nrPxpwoBNC3brPi7CFm12sb7bHSkB4gz4SGSV9LhBceZVGaF8nsevu6';
     export const GENESIS      = 'a771da8e52ee6ad581ed1e9a99825e5b3b7992225534eaa2ae23244fe26ab1c1'; // TODO autofetch from block 0
-    export const esplora      = Esplora('https://blockstream.info/liquidtestnet/api');
+    export const esplora      = Esplora({ url: 'https://blockstream.info/liquidtestnet/api' });
     export async function callFaucet (address: string) {
       const api= `https://liquidtestnet.com/api/faucet`;
       const url= `${api}?address=${encodeURIComponent(address)}&action=lbtc`;
