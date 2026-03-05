b6a75b5b19024a2468d76b153b2d9e225b7e7412
Hack.bg Admin <hello@hack.bg>
2026-03-05 09:20:46 +0000

    fix(test): test import

--- test/platform.test.ts
+++ test/platform.test.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S deno run --coverage --allow-env --allow-net --allow-run --allow-import --allow-read --allow-write=/tmp/fadroma
 import Test         from "../library/Test.ts";
 import Bitcoin      from "../platform/Bitcoin/Bitcoin.test.ts";
-import SimplicityHL from "../platform/SimplicityHL/SimplicityHL.test.ts";
+import SimplicityHL from "../platform/SimplicityHL/src/test.ts";
 export default Test(import.meta, 'Platform', Bitcoin, SimplicityHL);
